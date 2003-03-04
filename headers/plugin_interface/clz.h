ad9f991351d17c6890f50999a02d9307011324b6
James McCartney <asynth@users.sourceforge.net>
2003-03-04 08:39:01 +0000

    add bit population count functions
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@554 a380766d-ff14-0410-b294-a243070f3f08

--- headers/plugin_interface/clz.h
+++ headers/plugin_interface/clz.h
@@ -106,6 +106,24 @@ inline int32 LSBit(int32 x)
 	return x & -x;
 }
 
+// count number of one bits
+inline uint32 ONES(uint32 x) 
+{
+	uint32 t; 
+	x = x - ((x >> 1) & 0x55555555); 
+	t = ((x >> 2) & 0x33333333); 
+	x = (x & 0x33333333) + t; 
+	x = (x + (x >> 4)) & 0x0F0F0F0F; 
+	x = x + (x << 8); 
+	x = x + (x << 16); 
+	return x >> 24; 
+}
+
+// count number of zero bits
+inline uint32 ZEROES(uint32 x)
+{
+	return ONES(~x);
+}
 
 #endif
 
