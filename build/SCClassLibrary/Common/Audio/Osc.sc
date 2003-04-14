eca994d9fc1ceb2f9842690d7c0805b9b4456bec
James McCartney <asynth@users.sourceforge.net>
2003-04-14 08:06:46 +0000

    fix Select
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@948 a380766d-ff14-0410-b294-a243070f3f08

--- build/SCClassLibrary/Common/Audio/Osc.sc
+++ build/SCClassLibrary/Common/Audio/Osc.sc
@@ -189,5 +189,15 @@ Select : UGen {
 		arg which, array;
 		^this.multiNewList(['control', which] ++ array)
 	}
+ 	checkInputs {
+ 		if (rate == 'audio', {
+ 			for(1, inputs.size - 1, { arg i;
+ 				if (inputs.at(i).rate != 'audio', {
+ 					^("input was not audio rate: " + inputs.at(i));
+ 				});
+ 			});
+ 		});
+ 		^nil
+ 	}
 }
 
--- source/plugins/OscUGens.cpp
+++ source/plugins/OscUGens.cpp
@@ -498,7 +498,7 @@ void Select_Ctor(Select *unit)
 
 void Select_next_1(Select *unit, int inNumSamples)
 {
-	int32 maxindex = unit->mNumInputs;
+	int32 maxindex = unit->mNumInputs - 1;
 	int32 index = (int32)ZIN0(0) + 1;
 	index = sc_clip(index, 1, maxindex);
 	ZOUT0(0) = ZIN0(index);
@@ -507,7 +507,7 @@ void Select_next_1(Select *unit, int inNumSamples)
 
 void Select_next_k(Select *unit, int inNumSamples)
 {
-	int32 maxindex = unit->mNumInputs;
+	int32 maxindex = unit->mNumInputs - 1;
 	int32 index = (int32)ZIN0(0) + 1;
 	index = sc_clip(index, 1, maxindex);
 
@@ -520,13 +520,14 @@ void Select_next_k(Select *unit, int inNumSamples)
 
 void Select_next_a(Select *unit, int inNumSamples)
 {
-	int32 maxindex = unit->mNumInputs;
+	int32 maxindex = unit->mNumInputs - 1;
 
 	float *out = ZOUT(0);
+	float *in0 = ZIN(0);
 	float **in = unit->mInBuf;
 	
 	for (int i=0; i<inNumSamples; ++i) {
-		int32 index = (int32)ZXP(in) + 1;
+		int32 index = (int32)ZXP(in0) + 1;
 		index = sc_clip(index, 1, maxindex);
 		ZXP(out) = in[index][i];
 	}
