ff4440222e9541eb00c4c5eabe8aaa2f02f7e809
James McCartney <asynth@users.sourceforge.net>
2003-04-29 06:25:29 +0000

    fix MouseY
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@1069 a380766d-ff14-0410-b294-a243070f3f08

--- source/plugins/MacUGens.cpp
+++ source/plugins/MacUGens.cpp
@@ -225,7 +225,7 @@ void load(InterfaceTable *inTable)
 	pthread_create (&macThread, NULL, gstate_update_func, (void*)0);
 
 	DefineUnit("MouseX", sizeof(MacUGen), (UnitCtorFunc)&MouseX_Ctor, 0);
-	DefineUnit("MouseY", sizeof(MacUGen), (UnitCtorFunc)&MouseX_Ctor, 0);
+	DefineUnit("MouseY", sizeof(MacUGen), (UnitCtorFunc)&MouseY_Ctor, 0);
 	DefineUnit("MouseButton", sizeof(MacUGen), (UnitCtorFunc)&MouseButton_Ctor, 0);
 	DefineUnit("KeyState", sizeof(MacUGen), (UnitCtorFunc)&KeyState_Ctor, 0);
 }
