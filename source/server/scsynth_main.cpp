a9008d9a62acf3f1560c2196256872f29c3c8dc8
Ross Bencina <rossb@users.sourceforge.net>
2003-10-15 11:50:47 +0000

    added win32 initization code for winsock and pthreads, and implementation of setlinebuf
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@1964 a380766d-ff14-0410-b294-a243070f3f08

--- source/server/scsynth_main.cpp
+++ source/server/scsynth_main.cpp
@@ -25,10 +25,27 @@
 #include <stdarg.h>
 #include <stdlib.h>
 #include <math.h>
-
 #include "clz.h"
 #include <stdexcept>
+#ifdef SC_WIN32
+#include <pthread.h>
+#include <winsock2.h>
+#else
 #include <sys/wait.h>
+#endif
+
+
+#ifdef SC_WIN32
+
+// according to this page: http://www.mkssoftware.com/docs/man3/setlinebuf.3.asp
+// setlinebuf is equivalent to the setvbuf call below.
+inline int setlinebuf(FILE *stream)
+{
+    return setvbuf( stream, (char*)0, _IONBF, 0 );
+}
+
+#endif
+
 
 void Usage();
 void Usage()
@@ -86,10 +103,24 @@ void Usage()
 
 int main(int argc, char* argv[]);
 int main(int argc, char* argv[])
-{	
-	
+{
     setlinebuf(stdout);
-	
+
+#ifdef SC_WIN32
+#ifdef SC_WIN32_STATIC_PTHREADS
+    // initialize statically linked pthreads library
+    pthread_win32_process_attach_np();
+#endif
+
+    // initialize winsock
+    WSAData wsaData;
+	int nCode;
+    if ((nCode = WSAStartup(MAKEWORD(1, 1), &wsaData)) != 0) {
+		scprintf( "WSAStartup() failed with error code %d.\n", nCode );
+        return 1;
+    }
+#endif
+
 	int udpPortNum = -1;
 	int tcpPortNum = -1;
 
@@ -198,7 +229,18 @@ int main(int argc, char* argv[])
 	fflush(stdout);
 	
 	World_WaitForQuit(world);
-	    	    
+
+    
+#ifdef SC_WIN32
+    // clean up winsock
+    WSACleanup();
+
+#ifdef SC_WIN32_STATIC_PTHREADS
+    // clean up statically linked pthreads
+    pthread_win32_process_detach_np();
+#endif
+#endif
+
 	return 0;
 }
 
