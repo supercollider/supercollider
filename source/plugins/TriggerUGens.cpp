3a5b89634defc16807e95ed72a169771af24ec1e
Julian Rohrhuber <jrhb@users.sourceforge.net>
2004-01-19 18:21:07 +0000

    added Ball, TBall and Spring
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@2504 a380766d-ff14-0410-b294-a243070f3f08

--- source/plugins/TriggerUGens.cpp
+++ source/plugins/TriggerUGens.cpp
@@ -150,6 +150,28 @@ struct LastValue : public Unit
 	float mCurr;
 };
 
+struct Spring : public Unit
+{
+	float m_pos;
+	float m_vel;
+};
+
+struct Ball : public Unit
+{
+	float m_pos;
+	float m_vel;
+	float m_prev;
+};
+
+struct TBall : public Unit
+{
+	double m_pos;
+	float m_vel;
+	double m_prev;
+};
+
+
+
 struct Done : public Unit
 {
 	Unit *m_src;
@@ -269,6 +291,15 @@ void LeastChange_next_aa(LeastChange *unit, int inNumSamples);
 void LastValue_Ctor(LastValue *unit);
 void LastValue_next_ak(LastValue *unit, int inNumSamples);
 void LastValue_next_kk(LastValue *unit, int inNumSamples);
+
+void Spring_Ctor(Spring *unit);
+void Spring_next(Spring *unit, int inNumSamples);
+
+void Ball_Ctor(Ball *unit);
+void Ball_next(Ball *unit, int inNumSamples);
+
+void TBall_Ctor(TBall *unit);
+void TBall_next(TBall *unit, int inNumSamples);
 
 void Done_Ctor(Done *unit);
 void Done_next(Done *unit, int inNumSamples);
@@ -1628,6 +1659,200 @@ void LastValue_next_ak(LastValue *unit, int inNumSamples)
 }
 
 
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+void Spring_Ctor(Spring *unit)
+{
+	SETCALC(Spring_next);
+	unit->m_vel = 0.f;
+	unit->m_pos = 0.f;
+	Spring_next(unit, 1);
+	
+}
+
+// in, spring, damping
+
+void Spring_next(Spring *unit, int inNumSamples)
+{
+	float pos = unit->m_pos;
+	float vel = unit->m_vel;
+	float *out = ZOUT(0); 		// out force
+	float *in = ZIN(0); 		// in force
+	float spring = ZIN0(1);		// spring constant
+	float damping = 1 - ZIN0(2);// damping
+	float c = SAMPLEDUR;
+	float rc = SAMPLERATE;
+	spring = spring * c;
+	LOOP(inNumSamples,
+		float force = ZXP(in) * c - pos * spring; 
+		vel = (force + vel) * damping;
+		pos += vel;
+		ZXP(out) = force * rc;
+	);
+	
+	unit->m_pos = pos;
+	unit->m_vel = vel;
+	
+}
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+void Ball_Ctor(Ball *unit)
+{
+	SETCALC(Ball_next);
+	unit->m_vel = 0.f;
+	unit->m_pos = ZIN0(0);
+	unit->m_prev = ZIN0(0);
+	Ball_next(unit, 1);
+	
+}
+
+
+void Ball_next(Ball *unit, int inNumSamples)
+{
+
+	float *out = ZOUT(0);
+	float *in = ZIN(0); 			// floor position
+	float g_in = ZIN0(1); 			// gravity
+	float damping = 1 - ZIN0(2);	// damping
+	float k = ZIN0(3);				// friction
+	
+	float pos = unit->m_pos;
+	float vel = unit->m_vel;
+	float prev_floor = unit->m_prev;
+	float c = SAMPLEDUR;
+	float maxvel = c * 1000.f;
+	float minvel = 0.f - maxvel;
+	float inter = c * 1000.f;
+	RGen& rgen = *unit->mParent->mRGen;	
+	float g = c * g_in;
+	(double) k *= g_in; // stickyness proportional to gravity
+	
+	LOOP(inNumSamples,
+		
+		float floor = ZXP(in);
+		float floorvel;
+		float dither;
+		vel -= g;
+		pos += vel;
+		float dist = pos - floor;
+		floorvel = floor - prev_floor;
+		floorvel = sc_clip(floorvel, minvel, maxvel);
+		float vel_diff = floorvel - vel; 
+		if(sc_abs(dist) < k) { // sticky friction
+				
+				if(sc_abs(dist) < (k*0.005)) { 
+					vel = 0.f;
+					pos = floor + g;
+				} else {
+					vel = vel_diff * inter + vel;
+					pos = (floor - pos) * inter + pos;
+				}
+				
+		} else if(dist <= 0.f) {
+					
+					pos = floor - dist;
+					vel = vel_diff;
+					vel *= damping;
+					
+					dither = rgen.frand() * 0.00005f * g_in; // dither to reduce jitter
+					//if(sc_abs(dist) < 0.000001) { vel += dither; }
+					vel += dither;
+		}
+		prev_floor = floor;
+		ZXP(out) = pos;
+	);
+	
+	unit->m_pos = pos;
+	unit->m_vel = vel;
+	unit->m_prev = prev_floor;
+	
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+
+
+void TBall_Ctor(TBall *unit)
+{
+	SETCALC(TBall_next);
+	unit->m_vel = 0.f;
+	unit->m_pos = ZIN0(0);
+	unit->m_prev = ZIN0(0);
+	TBall_next(unit, 1);
+	
+}
+
+void TBall_next(TBall *unit, int inNumSamples)
+{
+
+	float *out = ZOUT(0);
+	float *in = ZIN(0); 		// floor position
+	float g_in = ZIN0(1); 		// gravity
+	float damping = 1 - ZIN0(2);// damping
+	float k = ZIN0(3); 			// friction
+	
+	double pos = unit->m_pos;
+	float vel = unit->m_vel;
+	double prev_floor = unit->m_prev;
+	float c = SAMPLEDUR;
+	float maxvel = c * 1000.f;
+	float minvel = 0.f - maxvel;
+	float inter = c * 10000.f;
+	RGen& rgen = *unit->mParent->mRGen;	
+	float g = c * g_in;
+	(double) k *= g_in; // stickyness proportional to gravity
+	
+	LOOP(inNumSamples,
+		
+		double floor = ZXP(in);
+		float floorvel;
+		float outval = 0.f;
+		float dither;
+		vel -= g;
+		pos += vel;
+		double dist = pos - floor;
+		floorvel = floor - prev_floor;
+		floorvel = sc_clip(floorvel, minvel, maxvel);
+		float vel_diff = floorvel - vel;
+		if(sc_abs(dist) < k) { // sticky friction
+					if(sc_abs(dist) < (k*0.005)) { 
+					vel = 0.f;
+					pos = floor + g;
+				} else {
+					vel = vel_diff * inter + vel;
+					pos = (floor - pos) * inter + pos;
+				}
+		} else if(dist <= 0.f) {
+					pos = floor - dist;
+					vel = floorvel - vel;
+					vel *= damping;
+					outval = vel;
+					dither = rgen.frand() * 0.001f * g_in; // dither to reduce sampling jitter
+					//if(sc_abs(dist) < 0.003) { vel += dither; }
+					vel += dither;
+					
+		}
+		prev_floor = floor;
+		ZXP(out) = outval;
+	);
+	
+	unit->m_pos = pos;
+	unit->m_vel = vel;
+	unit->m_prev = prev_floor;
+	
+}
+
+
+
+
 
 //////////////////////////////////////////////////////////////////////////////////////////
 
@@ -1826,6 +2051,9 @@ void load(InterfaceTable *inTable)
 	DefineSimpleUnit(MostChange);
 	DefineSimpleUnit(LeastChange);
 	DefineSimpleUnit(LastValue);
+	DefineSimpleUnit(Spring);
+	DefineSimpleUnit(Ball);
+	DefineSimpleUnit(TBall);
 	DefineSimpleUnit(Done);
 	DefineSimpleUnit(Pause);
 	DefineSimpleUnit(FreeSelf);
