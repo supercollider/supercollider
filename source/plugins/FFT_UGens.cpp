af68900dde1ea2976229f7cb9f5d1948e12e5378
Nick Collins <sicklincoln@users.sourceforge.net>
2004-01-28 23:21:26 +0000

    removed PV_ConformalMap to third party FFT files
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@2547 a380766d-ff14-0410-b294-a243070f3f08

--- source/plugins/FFT_UGens.cpp
+++ source/plugins/FFT_UGens.cpp
@@ -240,10 +240,7 @@ extern "C"
 	void PV_BinScramble_Ctor(PV_BinScramble *unit);
 	void PV_BinScramble_Dtor(PV_BinScramble *unit);
 	void PV_BinScramble_next(PV_BinScramble *unit, int inNumSamples);
-	
-	void PV_ConformalMap_Ctor(PV_Unit *unit);
-	void PV_ConformalMap_next(PV_Unit *unit, int inNumSamples);
-	
+
 	
 /* spectral feature extractors? :
 		bin freq
@@ -1470,47 +1467,6 @@ void PV_BinScramble_Dtor(PV_BinScramble* unit)
 	RTFree(unit->mWorld, unit->m_tempbuf);
 }
 
-	
-void PV_ConformalMap_Ctor(PV_Unit *unit)
-{
-	SETCALC(PV_ConformalMap_next);
-	ZOUT0(0) = ZIN0(0);
-}
-
-void PV_ConformalMap_next(PV_Unit *unit, int inNumSamples)
-{
-	PV_GET_BUF
-
-	SCComplexBuf *p = ToComplexApx(buf);
-	
-	float real2 = ZIN0(1);
-	float imag2 = ZIN0(2);
-	
-	for (int i=0; i<numbins; ++i) {
-		float real1 = p->bin[i].real;
-		float imag1 = p->bin[i].imag;
-	
-		//apply conformal map z-> z-a/(1-za*) where z is the existing complex number in the bin and a is defined by inputs 1 and 2
-		float numr= real1-real2;
-		float numi= imag1-imag2;
-		float denomr= 1- (real1*real2+imag1*imag2);
-		float denomi= (real1*imag2- real2*imag1);
-		
-		numr= numr*denomr+numi*denomi;
-		numi= numi*denomr-numr*denomi; 
-		
-		//squared modulus
-		denomr= denomr*denomr+denomi*denomi;
-		
-		//avoid possible divide by zero
-		if(denomr<0.001) denomr=0.001;
-		denomr=1.0/denomr;
-						
-		p->bin[i].real = numr*denomr;
-		p->bin[i].imag = numi*denomr;
-	}
-	
-}	
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -1602,8 +1558,7 @@ void load(InterfaceTable *inTable)
 	DefinePVUnit(PV_BrickWall);
 	DefinePVUnit(PV_BinWipe);
 	DefinePVUnit(PV_LocalMax);
-	DefinePVUnit(PV_ConformalMap);
-	
+
 	DefineDtorUnit(PV_BinScramble);
 	DefineDtorUnit(PV_MagSmear);
 	DefineDtorUnit(PV_MagShift);
