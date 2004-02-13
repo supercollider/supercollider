5c1f1398505e001ab719df4c27931455af532914
James McCartney <asynth@users.sourceforge.net>
2004-02-13 02:35:29 +0000

    b_gen env. doesn't do wavetables yet
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@2656 a380766d-ff14-0410-b294-a243070f3f08

--- source/plugins/LFUGens.cpp
+++ source/plugins/LFUGens.cpp
@@ -3177,6 +3177,148 @@ void ADSR_next_k(ADSR *unit, int inNumSamples)
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+void EnvFill(World *world, struct SndBuf *buf, struct sc_msg_iter *msg)
+{
+	if (buf->channels != 1) return;
+		
+	int size = buf->samples;
+	int byteSize = size * sizeof(float);
+	float *data = (float*)malloc(byteSize);
+
+	double level = msg->getf();
+	int numStages = msg->geti();
+	/*int releaseNode =*/ msg->geti(); // ignored
+	/*int loopNode =*/ msg->geti(); // ignored
+	
+	double pos = 0.;
+	int32 index = 0;
+	int32 remain = size;
+	
+	for (int j=0; j < numStages; ++j)
+	{
+		double endLevel = msg->getf();
+		double dur = msg->getf();
+		int shape = msg->geti(); 
+		double curve = msg->getf();
+
+		int32 ipos = (int32)pos;
+		double smpdur = dur * size;
+		int32 nsmps = (int32)smpdur - ipos;
+		nsmps = sc_min(nsmps, remain);
+
+		switch (shape) {
+			case shape_Step : {
+				level = endLevel;
+				for (int i=0; i<nsmps; ++i) {
+					data[index++] = level;
+				}
+			} break;
+			case shape_Linear : {
+				double grow = (endLevel - level) / nsmps;
+				for (int i=0; i<nsmps; ++i) {
+					data[index++] = level;
+					level += grow;
+				}
+			} break;
+			case shape_Exponential : {
+				double grow = pow(endLevel / level, 1.0 / nsmps);
+				for (int i=0; i<nsmps; ++i) {
+					data[index++] = level;
+					level *= grow;
+				}
+			} break;
+			case shape_Sine : {
+				double w = pi / nsmps;
+	
+				double a2 = (endLevel + level) * 0.5;
+				double b1 = 2. * cos(w);
+				double y1 = (endLevel - level) * 0.5;
+				double y2 = y1 * sin(pi * 0.5 - w);
+				level = a2 - y1;
+				for (int i=0; i<nsmps; ++i) {
+					data[index++] = level;
+					double y0 = b1 * y1 - y2; 
+					level = a2 - y0;
+					y2 = y1; 
+					y1 = y0;
+				}
+			} break;
+			case shape_Welch : {
+				double w = (pi * 0.5) / nsmps;
+				
+				double b1 = 2. * cos(w);
+				double a2, y1, y2;
+				if (endLevel >= level) {
+					a2 = level;
+					y1 = 0.;
+					y2 = -sin(w) * (endLevel - level);
+				} else {
+					a2 = endLevel;
+					y1 = level - endLevel;
+					y2 = cos(w) * (level - endLevel);
+				}
+				level = a2 + y1;
+				for (int i=0; i<nsmps; ++i) {
+					data[index++] = level;
+					double y0 = b1 * y1 - y2; 
+					level = a2 - y0;
+					y2 = y1; 
+					y1 = y0;
+				}
+			} break;
+			case shape_Curve : {
+				if (fabs(curve) < 0.001) {
+					double grow = (endLevel - level) / nsmps;
+					for (int i=0; i<nsmps; ++i) {
+						data[index++] = level;
+						level += grow;
+					}
+				} else {
+					double a1 = (endLevel - level) / (1.0 - exp(curve));	
+					double a2 = level + a1;
+					double b1 = a1; 
+					double grow = exp(curve / nsmps);
+					for (int i=0; i<nsmps; ++i) {
+						data[index++] = level;
+						b1 *= grow;
+						level = a2 - b1;
+					}
+				}
+			} break;
+			case shape_Squared : {
+				double y1 = sqrt(level); 
+				double y2 = sqrt(endLevel); 
+				double grow = (y2 - y1) / nsmps;
+				for (int i=0; i<nsmps; ++i) {
+					data[index++] = level;
+					y1 += grow;
+					level = y1*y1;
+				}
+			} break;
+			case shape_Cubed : {
+				double y1 = pow(level, 0.33333333); 
+				double y2 = pow(endLevel, 0.33333333); 
+				double grow = (y2 - y1) / nsmps;
+				for (int i=0; i<nsmps; ++i) {
+					data[index++] = level;
+					y1 += grow;
+					level = y1*y1*y1;
+				}
+			} break;
+		}
+
+		pos += smpdur;
+		level = endLevel;
+		remain -= nsmps;
+	}
+	memcpy(buf->data, data, byteSize);
+	free(data);
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
 void load(InterfaceTable *inTable)
 {
 	ft = inTable;
@@ -3206,4 +3348,6 @@ void load(InterfaceTable *inTable)
 	DefineSimpleUnit(EnvGen);
 	//DefineSimpleUnit(BufEnvGen);
 	DefineSimpleUnit(Linen);
+
+	DefineBufGen("env", EnvFill);
 }
