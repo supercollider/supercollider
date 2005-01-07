2f3d0804032f8fa81a7283356ecdec697bc3a90e
Julian Rohrhuber <jrhb@users.sourceforge.net>
2005-01-07 21:46:13 +0000

    added DemandEnvGen
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@4095 a380766d-ff14-0410-b294-a243070f3f08

--- source/plugins/DemandUGens.cpp
+++ source/plugins/DemandUGens.cpp
@@ -39,6 +39,16 @@ struct Duty : public Unit
 	float m_prevout;
 };
 
+struct DemandEnvGen : public Unit
+{
+	float m_phase;
+	float m_prevreset;
+	
+	double m_a1, m_a2, m_b1, m_y1, m_y2, m_grow, m_level, m_endLevel, m_curve;
+	int m_shape;
+	bool m_release, m_running;
+};
+
 struct TDuty : public Unit
 {
 	float m_count;
@@ -139,6 +149,12 @@ void Demand_next(Demand *unit, int inNumSamples);
 
 void Duty_Ctor(Duty *unit);
 void Duty_next(Duty *unit, int inNumSamples);
+
+void TDuty_Ctor(TDuty *unit);
+void TDuty_next(TDuty *unit, int inNumSamples);
+
+void DemandEnvGen_Ctor(DemandEnvGen *unit);
+void DemandEnvGen_next(DemandEnvGen *unit, int inNumSamples);
 
 void Dseries_Ctor(Dseries *unit);
 void Dseries_next(Dseries *unit, int inNumSamples);
@@ -509,6 +525,583 @@ void Duty_Ctor(Duty *unit)
 	OUT0(0) = 0.f;
 	
 }
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+enum {
+	d_env_level,
+	d_env_dur,
+	d_env_shape,
+	d_env_curve,
+	d_env_gate,
+	d_env_reset,
+	d_env_levelScale,
+	d_env_levelBias,
+	d_env_timeScale,
+	d_env_doneAction
+	
+};
+
+enum {
+	shape_Step,
+	shape_Linear,
+	shape_Exponential,
+	shape_Sine,
+	shape_Welch,
+	shape_Curve,
+	shape_Squared,
+	shape_Cubed,
+	shape_Sustain = 9999
+};
+
+
+
+void DemandEnvGen_next_k(DemandEnvGen *unit, int inNumSamples)
+{
+	
+	float zreset = ZIN0(d_env_reset);
+
+
+	float *out = ZOUT(0);
+	double level = unit->m_level;
+	float phase = unit->m_phase;
+	double curve = unit->m_curve;
+	bool release = unit->m_release;
+	bool running = unit->m_running;
+	int shape = unit->m_shape;
+	
+	// printf("phase %f level %f \n", phase, level);
+	
+	for (int i=0; i<inNumSamples; ++i) {
+		
+		
+		
+		if (zreset > 0.f && unit->m_prevreset <= 0.f) {
+			//printf("reset: %f %f \n", zreset, unit->m_prevreset);
+			RESETINPUT(d_env_level);
+			RESETINPUT(d_env_dur);
+			RESETINPUT(d_env_shape);
+			RESETINPUT(d_env_curve);
+			
+			if(zreset <= 1.f) { 
+				DEMANDINPUT(d_env_level); // remove first level
+			} else {
+				level = DEMANDINPUT(d_env_level); // jump to first level
+			}
+			
+			release = false;
+			running = true;
+			phase = 0.f;
+			
+		}
+		
+		
+		if (phase <= 0.f && running) {
+						
+			// was a release during last segment?
+			if(release) {
+				
+				running = false;
+				release = false;
+				// printf("release: %f %f \n", phase, level);
+				int doneAction = (int)ZIN0(d_env_doneAction);
+				DoneAction(doneAction, unit);
+				
+			} else {
+			
+
+				
+				// new time
+				
+				float dur = DEMANDINPUT(d_env_dur);
+				// printf("dur: %f \n", dur);
+				if(sc_isnan(dur)) { 
+					release = true;
+					running = false;
+					phase = MAXFLOAT;
+				} else {
+					phase = dur * ZIN0(d_env_timeScale) * SAMPLERATE + .5f + phase;
+				}
+				
+				
+				// new shape
+				
+				float count;
+				shape = (int)DEMANDINPUT(d_env_shape);
+				curve = DEMANDINPUT(d_env_curve);
+				
+				
+				if (sc_isnan(curve)) curve = unit->m_shape;
+				if (phase <= 1.f) {
+					shape = 1; // shape_Linear
+					count = 1.f;
+				} else {
+					count = phase;
+				}
+				if(dur * 0.5f < SAMPLEDUR) shape = 1;
+				
+				//printf("shape: %i, curve: %f, dur: %f \n", shape, curve, dur);
+				
+				
+				// new end level
+				
+				double endLevel = DEMANDINPUT(d_env_level);
+				// printf("levels: %f %f\n", level, endLevel);
+				if (sc_isnan(endLevel)) { 
+					endLevel = unit->m_endLevel;
+					release = true;
+					phase = 0.f;
+					shape = 0;
+				} else  { 
+					endLevel = endLevel * ZIN0(d_env_levelScale) + ZIN0(d_env_levelBias);
+					unit->m_endLevel = endLevel; 
+				}
+			
+				
+				// calculate shape parameters
+				
+				switch (shape) {
+					case shape_Step : {
+						level = endLevel;
+					} break;
+					case shape_Linear : {
+						unit->m_grow = (endLevel - level) / count;
+					} break;
+					case shape_Exponential : {
+						unit->m_grow = pow(endLevel / level, 1.0 / count);
+					} break;
+					case shape_Sine : {
+						double w = pi / count;
+			
+						unit->m_a2 = (endLevel + level) * 0.5;
+						unit->m_b1 = 2. * cos(w);
+						unit->m_y1 = (endLevel - level) * 0.5;
+						unit->m_y2 = unit->m_y1 * sin(pi * 0.5 - w);
+						level = unit->m_a2 - unit->m_y1;
+					} break;
+					case shape_Welch : {
+						double w = (pi * 0.5) / count;
+						
+						unit->m_b1 = 2. * cos(w);
+						
+						if (endLevel >= level) {
+							unit->m_a2 = level;
+							unit->m_y1 = 0.;
+							unit->m_y2 = -sin(w) * (endLevel - level);
+						} else {
+							unit->m_a2 = endLevel;
+							unit->m_y1 = level - endLevel;
+							unit->m_y2 = cos(w) * (level - endLevel);
+						}
+						level = unit->m_a2 + unit->m_y1;
+					} break;
+					case shape_Curve : {
+						if (fabs(curve) < 0.001) {
+							unit->m_shape = 1; // shape_Linear
+							unit->m_grow = (endLevel - level) / count;
+						} else {
+							double a1 = (endLevel - level) / (1.0 - exp(curve));	
+							unit->m_a2 = level + a1;
+							unit->m_b1 = a1; 
+							unit->m_grow = exp(curve / count);
+						}
+					} break;
+					case shape_Squared : {
+						unit->m_y1 = sqrt(level); 
+						unit->m_y2 = sqrt(endLevel); 
+						unit->m_grow = (unit->m_y2 - unit->m_y1) / count;
+					} break;
+					case shape_Cubed : {
+						unit->m_y1 = pow(level, 0.33333333); 
+						unit->m_y2 = pow(endLevel, 0.33333333); 
+						unit->m_grow = (unit->m_y2 - unit->m_y1) / count;
+					} break;
+				}
+			
+			}
+			}
+			
+			
+			if(running) {
+			
+			switch (shape) {
+				case shape_Step : {
+				} break;
+				case shape_Linear : {
+					double grow = unit->m_grow;
+							//Print("level %g\n", level);
+						level += grow;
+				} break;
+				case shape_Exponential : {
+					double grow = unit->m_grow;
+						level *= grow;
+				} break;
+				case shape_Sine : {
+					double a2 = unit->m_a2;
+					double b1 = unit->m_b1;
+					double y2 = unit->m_y2;
+					double y1 = unit->m_y1;
+						double y0 = b1 * y1 - y2; 
+						level = a2 - y0;
+						y2 = y1; 
+						y1 = y0;
+					unit->m_y1 = y1;
+					unit->m_y2 = y2;
+				} break;
+				case shape_Welch : {
+					double a2 = unit->m_a2;
+					double b1 = unit->m_b1;
+					double y2 = unit->m_y2;
+					double y1 = unit->m_y1;
+						double y0 = b1 * y1 - y2; 
+						level = a2 + y0;
+						y2 = y1; 
+						y1 = y0;
+					unit->m_y1 = y1;
+					unit->m_y2 = y2;
+				} break;
+				case shape_Curve : {
+					double a2 = unit->m_a2;
+					double b1 = unit->m_b1;
+					double grow = unit->m_grow;
+						b1 *= grow;
+						level = a2 - b1;
+					unit->m_b1 = b1;
+				} break;
+				case shape_Squared : {
+					double grow = unit->m_grow;
+					double y1 = unit->m_y1;
+						y1 += grow;
+						level = y1*y1;
+					unit->m_y1 = y1;
+				} break;
+				case shape_Cubed : {
+					double grow = unit->m_grow;
+					double y1 = unit->m_y1;
+						y1 += grow;
+						level = y1*y1*y1;
+					unit->m_y1 = y1;
+				} break;
+				case shape_Sustain : {
+				} break;
+			}
+			
+			phase --;
+			
+			}
+			
+			
+			ZXP(out) = level;		
+			
+	}
+			float zgate = ZIN0(d_env_gate);
+			if(zgate >= 0.5f) { 
+				unit->m_running = true; 
+			} else if (zgate > 0.f) {
+				unit->m_running = true;
+				release = true;  // release next time.
+			} else { 
+				unit->m_running = false; // sample and hold
+			}
+			
+			unit->m_level = level;
+			unit->m_curve = curve;
+			unit->m_shape = shape;	
+			unit->m_prevreset = zreset;
+			unit->m_release = release;
+			
+			unit->m_phase = phase;
+	
+
+}
+
+
+
+void DemandEnvGen_next_a(DemandEnvGen *unit, int inNumSamples)
+{
+	
+	float *reset = ZIN(d_env_reset);
+	float *gate = ZIN(d_env_gate);
+	
+	float *out = ZOUT(0);
+	
+	float prevreset = unit->m_prevreset;
+	double level = unit->m_level;
+	float phase = unit->m_phase;
+	double curve = unit->m_curve;
+	bool release = unit->m_release;
+	bool running = unit->m_running;
+	
+
+	int shape = unit->m_shape;
+	// printf("phase %f \n", phase);
+	
+	for (int i=0; i<inNumSamples; ++i) {
+		
+		float zreset = ZXP(reset);
+		if (zreset > 0.f && prevreset <= 0.f) {
+			// printf("reset: %f %f \n", zreset, unit->m_prevreset);
+			RESETINPUT(d_env_level);
+			if(zreset <= 1.f) { 
+				DEMANDINPUT(d_env_level); // remove first level
+			} else {
+				level = DEMANDINPUT(d_env_level); // jump to first level
+			}
+			
+			RESETINPUT(d_env_dur);
+			RESETINPUT(d_env_shape);
+			release = false;
+			running = true;
+			
+			phase = 0.f;
+			
+		}
+		
+		prevreset = zreset;
+		
+		
+		if (phase <= 0.f && running) {
+						
+			// was a release?
+			if(release) {
+				
+				running = false;
+				release = false;
+				// printf("release: %f %f \n", phase, level);
+				int doneAction = (int)ZIN0(d_env_doneAction);
+				DoneAction(doneAction, unit);
+				
+			} else {
+			
+
+				
+				// new time
+				
+				float dur = DEMANDINPUT(d_env_dur);
+				// printf("dur: %f \n", dur);
+				if(sc_isnan(dur)) { 
+					release = true; 
+					running = false;
+					phase = MAXFLOAT;
+				} else {
+					phase = dur * ZIN0(d_env_timeScale) * SAMPLERATE + .5f + phase;
+				}
+				
+				// new shape
+				float count;
+				curve = DEMANDINPUT(d_env_shape);
+				shape = (int)DEMANDINPUT(d_env_shape);
+				
+				// printf("shapes: %i \n", shape);
+				if (sc_isnan(curve)) curve = unit->m_shape;
+				if (sc_isnan(shape)) shape = unit->m_shape;
+				
+				if (phase <= 1.f) {
+					shape = 1; // shape_Linear
+					count = 1.f;
+				} else {
+					count = phase;
+				}
+				if(dur * 0.5f < SAMPLEDUR) shape = 1;
+				
+				
+				// new end level
+				
+				double endLevel = DEMANDINPUT(d_env_level);
+				// printf("levels: %f %f\n", level, endLevel);
+				if (sc_isnan(endLevel)) { 
+					endLevel = unit->m_endLevel;
+					release = true;
+					phase = 0.f;
+					shape = 0;
+				} else  {
+					endLevel = endLevel * ZIN0(d_env_levelScale) + ZIN0(d_env_levelBias);
+					unit->m_endLevel = endLevel; 
+				}
+			
+				
+				// calculate shape parameters
+				
+				switch (shape) {
+					case shape_Step : {
+						level = endLevel;
+					} break;
+					case shape_Linear : {
+						unit->m_grow = (endLevel - level) / count;
+					} break;
+					case shape_Exponential : {
+						unit->m_grow = pow(endLevel / level, 1.0 / count);
+					} break;
+					case shape_Sine : {
+						double w = pi / count;
+			
+						unit->m_a2 = (endLevel + level) * 0.5;
+						unit->m_b1 = 2. * cos(w);
+						unit->m_y1 = (endLevel - level) * 0.5;
+						unit->m_y2 = unit->m_y1 * sin(pi * 0.5 - w);
+						level = unit->m_a2 - unit->m_y1;
+					} break;
+					case shape_Welch : {
+						double w = (pi * 0.5) / count;
+						
+						unit->m_b1 = 2. * cos(w);
+						
+						if (endLevel >= level) {
+							unit->m_a2 = level;
+							unit->m_y1 = 0.;
+							unit->m_y2 = -sin(w) * (endLevel - level);
+						} else {
+							unit->m_a2 = endLevel;
+							unit->m_y1 = level - endLevel;
+							unit->m_y2 = cos(w) * (level - endLevel);
+						}
+						level = unit->m_a2 + unit->m_y1;
+					} break;
+					case shape_Curve : {
+						if (fabs(curve) < 0.001) {
+							unit->m_shape = 1; // shape_Linear
+							unit->m_grow = (endLevel - level) / count;
+						} else {
+							double a1 = (endLevel - level) / (1.0 - exp(curve));	
+							unit->m_a2 = level + a1;
+							unit->m_b1 = a1; 
+							unit->m_grow = exp(curve / count);
+						}
+					} break;
+					case shape_Squared : {
+						unit->m_y1 = sqrt(level); 
+						unit->m_y2 = sqrt(endLevel); 
+						unit->m_grow = (unit->m_y2 - unit->m_y1) / count;
+					} break;
+					case shape_Cubed : {
+						unit->m_y1 = pow(level, 0.33333333); 
+						unit->m_y2 = pow(endLevel, 0.33333333); 
+						unit->m_grow = (unit->m_y2 - unit->m_y1) / count;
+					} break;
+				}
+			
+			}
+			}
+			
+			
+			
+			if(running) {
+			
+			switch (shape) {
+				case shape_Step : {
+				} break;
+				case shape_Linear : {
+					double grow = unit->m_grow;
+							//Print("level %g\n", level);
+						level += grow;
+				} break;
+				case shape_Exponential : {
+					double grow = unit->m_grow;
+						level *= grow;
+				} break;
+				case shape_Sine : {
+					double a2 = unit->m_a2;
+					double b1 = unit->m_b1;
+					double y2 = unit->m_y2;
+					double y1 = unit->m_y1;
+						double y0 = b1 * y1 - y2; 
+						level = a2 - y0;
+						y2 = y1; 
+						y1 = y0;
+					unit->m_y1 = y1;
+					unit->m_y2 = y2;
+				} break;
+				case shape_Welch : {
+					double a2 = unit->m_a2;
+					double b1 = unit->m_b1;
+					double y2 = unit->m_y2;
+					double y1 = unit->m_y1;
+						double y0 = b1 * y1 - y2; 
+						level = a2 + y0;
+						y2 = y1; 
+						y1 = y0;
+					unit->m_y1 = y1;
+					unit->m_y2 = y2;
+				} break;
+				case shape_Curve : {
+					double a2 = unit->m_a2;
+					double b1 = unit->m_b1;
+					double grow = unit->m_grow;
+						b1 *= grow;
+						level = a2 - b1;
+					unit->m_b1 = b1;
+				} break;
+				case shape_Squared : {
+					double grow = unit->m_grow;
+					double y1 = unit->m_y1;
+						y1 += grow;
+						level = y1*y1;
+					unit->m_y1 = y1;
+				} break;
+				case shape_Cubed : {
+					double grow = unit->m_grow;
+					double y1 = unit->m_y1;
+						y1 += grow;
+						level = y1*y1*y1;
+					unit->m_y1 = y1;
+				} break;
+				case shape_Sustain : {
+				} break;
+			}
+			
+			phase--;
+			
+			}
+			
+			
+			ZXP(out) = level;
+			float zgate = ZXP(gate);
+			
+			if(zgate >= 0.5f) { 
+				unit->m_running = true; 
+			} else if (zgate > 0.f) {
+				unit->m_running = true;
+				release = true;  // release next time.
+			} else { 
+				unit->m_running = false; // sample and hold
+			}
+			
+			
+			
+	}
+			unit->m_level = level;
+			unit->m_curve = curve;
+			unit->m_shape = shape;	
+			unit->m_prevreset = prevreset;
+			unit->m_release = release;
+			unit->m_phase = phase;
+
+}
+
+
+void DemandEnvGen_Ctor(DemandEnvGen *unit)
+{
+	// derive the first level.
+	
+	unit->m_level = DEMANDINPUT(d_env_level);
+	if(sc_isnan(unit->m_level)) { unit->m_level = 0.f; }
+	unit->m_endLevel = unit->m_level;
+	unit->m_release = false;
+	unit->m_prevreset = 0.f;
+	unit->m_phase = 0.f;
+	unit->m_running = ZIN0(d_env_gate);
+	
+	if(INRATE(d_env_gate) == calc_FullRate) {
+			SETCALC(DemandEnvGen_next_a);
+	} else {
+			SETCALC(DemandEnvGen_next_k);
+	}
+	
+	DemandEnvGen_next_k(unit, 1);
+	
+}
+
 
 /////////////////////////////////////////////////////////////////////////////
 
@@ -1120,6 +1713,7 @@ void load(InterfaceTable *inTable)
 
 	DefineSimpleCantAliasUnit(Demand);
 	DefineSimpleCantAliasUnit(Duty);
+	DefineSimpleCantAliasUnit(DemandEnvGen);
 	DefineSimpleCantAliasUnit(TDuty);
 	DefineSimpleUnit(Dseries);
 	DefineSimpleUnit(Dgeom);
