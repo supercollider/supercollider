56dac89c6f38424c4cfc769cd98469cd5a514e3f
Christopher Frauenberger <cfrb@users.sourceforge.net>
2006-01-09 23:35:46 +0000

    added icon for exe
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@5217 a380766d-ff14-0410-b294-a243070f3f08

--- Psycollider/Psycollider/setup.py
+++ Psycollider/Psycollider/setup.py
@@ -1,3 +1,9 @@
+# setup.py for use with py2exe to create a Psycollider.exe
+#
+# created: 10.12.2005
+#
+# commandline is: python setup.py py2exe -b1 -O2 -c
+
 from distutils.core import setup
 import py2exe
 
@@ -5,7 +11,12 @@ setup(
     version = "0.1",
     description = "Psycollider, the SuperCollider3 Lang implementation on Windows",
     name = "Psycollider",
-
-    # targets to build
-    console = ["Psycollider.py"],
+    zipfile=None,
+    # needs to be console to prevent empty cmd-windows for scsynth...
+    console = [ 
+        { 
+            "script": "Psycollider.py", 
+            "icon_resources": [(1, "Psycollider.ico")] 
+        } 
+    ], 
     )
