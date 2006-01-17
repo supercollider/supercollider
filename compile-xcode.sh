589b647e32f9d81f834879074b8b222cd07a28cb
a pieper <argl@users.sourceforge.net>
2006-01-17 21:12:01 +0000

    use newer project files
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@5228 a380766d-ff14-0410-b294-a243070f3f08

--- compile-xcode.sh
+++ compile-xcode.sh
@@ -3,16 +3,16 @@
 # This script builds the server, the plugins and the application (sclang), one after another.
 # It uses XCode, run it on Panther+ only.
 
-# scsynth 
+# scsynth
 echo "building scsynth"
-xcodebuild -project xSC3synth.pbproj -target "All" -buildstyle "Deployment" build || exit
+xcodebuild -project xSC3synth.xcodeproj -target "All" -buildstyle "Deployment" build || exit
 
 # sc plugins
 echo "building plugins"
-xcodebuild -project xSC3plugins.pbproj -target "All" -buildstyle "Deployment" build || exit
+xcodebuild -project xSC3plugins.xcodeproj -target "All" -buildstyle "Deployment" build || exit
 
 #sclang
 echo "building sclang"
-xcodebuild -project xSC3lang.pbproj -target "All" -buildstyle "Deployment" build || exit
+xcodebuild -project xSC3lang.xcodeproj -target "All" -buildstyle "Deployment" build || exit
 
 echo "Done."
