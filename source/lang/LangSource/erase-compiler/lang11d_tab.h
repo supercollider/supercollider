f198ba1285da56f4f5e32ad6363de1361b3ce841
James McCartney <asynth@users.sourceforge.net>
2006-08-16 05:42:40 +0000

    make MyClass() legal syntax equivalent to MyClass.new
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@5533 a380766d-ff14-0410-b294-a243070f3f08

--- source/lang/LangSource/erase-compiler/lang11d_tab.h
+++ source/lang/LangSource/erase-compiler/lang11d_tab.h
@@ -3,13 +3,7 @@
 #endif
 #define	NAME	257
 #define	INTEGER	258
-
-#ifdef SC_WIN32
-# define FLOAT_COMPAT 259  
-#else
-# define	FLOAT	259
-#endif
-
+#define	FLOAT	259
 #define	ACCIDENTAL	260
 #define	SYMBOL	261
 #define	STRING	262
@@ -20,11 +14,7 @@
 #define	VAR	267
 #define	ARG	268
 #define	CLASSVAR	269
-#ifdef SC_WIN32
-# define CONST_COMPAT 270
-#else
-# define	CONST	270
-#endif
+#define	CONST	270
 #define	NILOBJ	271
 #define	TRUEOBJ	272
 #define	FALSEOBJ	273
