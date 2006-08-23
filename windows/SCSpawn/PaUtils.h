b62602d274615c1732ba5c2f0d5cfb53507d67ba
Christopher Frauenberger <cfrb@users.sourceforge.net>
2006-08-23 15:38:23 +0000

    startup is now similar to the mac version, no need for scspawn anymore
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@5558 a380766d-ff14-0410-b294-a243070f3f08

--- windows/SCSpawn/PaUtils.h
+++
@@ -1,7 +0,0 @@
-extern void PaUtils_CreatePaDevList( );
-extern CString PaUtils_GetPaInDevNameFromIndex( int index );
-extern CString PaUtils_GetPaOutDevNameFromIndex( int index );
-extern int PaUtils_GetIndexFromPaInDevName(const CString& inDevName);
-extern int PaUtils_GetIndexFromPaOutDevName(const CString& outDevName);
-extern int PaUtils_GetDefaultInputDeviceIndex( );
-extern int PaUtils_GetDefaultOutputDeviceIndex( );
