fba9c80098977c5c9dab96419f44b79e36a8b1b4
Ryan Brown <rbrown46@users.sourceforge.net>
2007-06-25 00:55:17 +0000

    Fix help file loading exceptions. A large rewrite was involved.
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@6134 a380766d-ff14-0410-b294-a243070f3f08

--- Source/app/NSAttributedStringAdditions.m
+++ Source/app/NSAttributedStringAdditions.m
@@ -1,10 +1,19 @@
+// This code has its origins in the Colloquy project, which is licensed under the GPLv2. It has been heavily modified from the original. -rb
+
 #import <WebKit/WebKit.h>
 #import "NSAttributedStringAdditions.h"
 
-// This code has its origins in the Colloquy project, which is licensed under the GPLv2. It has been heavily modified from the original. -rb
+//#define DEBUG
 
-static NSConditionLock *renderLock = nil;
-static WebView *renderWebView = nil;
+#ifdef DEBUG
+	#define Log(fmt, ...) NSLog(fmt, ##__VA_ARGS__)
+#else
+	#define Log(fmt, ...)
+#endif
+
+NSConditionLock *renderLock = nil;
+WebView *renderWebView = nil;
+int finishedRendering = 0;
 
 @implementation NSAttributedString (NSAttributedStringHTMLAdditions)
 + (id) attributedStringWithURL:(NSURL *)url {
@@ -15,27 +24,30 @@ static WebView *renderWebView = nil;
 	// renderLock is reused across calls
 	if(!renderLock)
 		renderLock = [[NSConditionLock alloc] initWithCondition:2];
-
-	if([renderLock lockWhenCondition:2 beforeDate:[NSDate dateWithTimeIntervalSinceNow:2.]]) { // wait until any other call to this method finishes; timeout after 2 seconds waiting
-		[renderLock unlockWithCondition:0];
-
-		[NSThread detachNewThreadSelector:@selector(renderHTMLFragment:) toTarget:self withObject:[NSDictionary dictionaryWithObjectsAndKeys:url, @"url", nil]];
-
-		if( [renderLock lockWhenCondition:1 beforeDate:[NSDate dateWithTimeIntervalSinceNow:3.]] ) { // wait until the rendering is done; timeout in 3 seconds
-			result = [[[(id <WebDocumentText>)[[[renderWebView mainFrame] frameView] documentView] attributedString] mutableCopy] autorelease];
-			[renderLock unlockWithCondition:2];
-		}
+	
+	[renderLock lockWhenCondition:2]; // wait until any other call to this method finishes
+	
+	finishedRendering = 0;
+	[NSThread detachNewThreadSelector:@selector(renderHTMLFragment:) toTarget:self withObject:[NSDictionary dictionaryWithObjectsAndKeys:url, @"url", nil]];
+	[renderLock lockWhenCondition:1]; // block until the rendering is done
+	
+	if(finishedRendering == 1) {
+		result = [[[(id <WebDocumentText>)[[[renderWebView mainFrame] frameView] documentView] attributedString] mutableCopy] autorelease];
+		[renderLock unlockWithCondition:2]; // set the lock for the next run
+		return [[[self alloc] initWithAttributedString:result] autorelease];
 	}
-
-	return [[[self alloc] initWithAttributedString:result] autorelease];
+	else {
+		[renderLock unlockWithCondition:2]; // set the lock for the next run
+		return nil;
+	}
 }
 
 + (void) renderHTMLFragment:(NSDictionary *) info {
 	extern WebView *renderWebView;
 	extern NSConditionLock *renderLock;
 	NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
-
-	[renderLock lockWhenCondition:0]; // start the rendering, make the parent thread block
+	NSRunLoop *runLoop = [NSRunLoop currentRunLoop];
+	int i =0;
 
 	[NSThread setThreadPriority:1.0];
 
@@ -46,18 +58,32 @@ static WebView *renderWebView = nil;
 	[renderWebView setFrameLoadDelegate:self];
 	[[renderWebView mainFrame] loadRequest:[NSURLRequest requestWithURL:url]];
 
-	[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.25]];
+	while(!finishedRendering) {
+		Log(@"ran loop");
+		[[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow:0.05]];
+	}
+	
+	[renderLock unlockWithCondition:1]; // rendering is complete
+	
+	Log(@"exiting loop");
 
-	[renderLock lockWhenCondition:2];
-	[renderLock unlockWithCondition:2];
-
+	[renderWebView setFrameLoadDelegate:nil];
 	[pool release];
 }
 
++ (void)webView:(WebView *)sender didFailLoadWithError:(NSError *)error forFrame:(WebFrame *)frame {
+	Log(@"error from from webkit");
+	finishedRendering = -1;
+}
+
++ (void)webView:(WebView *)sender didFailProvisionalLoadWithError:(NSError *)error forFrame:(WebFrame *)frame {
+	Log(@"error from from webkit");
+	finishedRendering = -1;
+}
+
 + (void) webView:(WebView *) sender didFinishLoadForFrame:(WebFrame *) frame {
-	extern NSConditionLock *renderLock;
-	[renderLock unlockWithCondition:1]; // rendering is complete
-	[sender setFrameLoadDelegate:nil];
+	Log(@"webkit finished rendering");
+	finishedRendering = 1;
 }
 
 @end
