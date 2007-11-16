b5f7e9939feb21cd63edde81b77e55b218fc2faa
Ryan Brown <rbrown46@users.sourceforge.net>
2007-11-16 09:53:15 +0000

    Dashes are better than underscores.
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@6635 a380766d-ff14-0410-b294-a243070f3f08

--- packagesource.sh
+++ packagesource.sh
@@ -2,11 +2,11 @@
 
 DATE=`date "+%Y-%m-%d"`
 
-if [ -d SuperCollider_Source ]; then
-	echo "Please remove the ./SuperCollider_Source directory before running this script."
+if [ -d SuperCollider-Source ]; then
+	echo "Please remove the ./SuperCollider-Source directory before running this script."
 	exit 1
 fi
 
-mkdir SuperCollider_Source
-svn export --force ./ SuperCollider_Source
-ditto -ck --sequesterRsrc --keepParent SuperCollider_Source "SuperCollider_Source_$DATE.zip"
+mkdir SuperCollider-Source
+svn export --force ./ SuperCollider-Source
+ditto -ck --sequesterRsrc --keepParent SuperCollider-Source "SuperCollider-Source-$DATE.zip"
