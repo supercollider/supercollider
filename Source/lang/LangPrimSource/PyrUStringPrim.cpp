d8fbb9c37764bb85b1fe7f827ec3f589c522cd9e
Jan Trutzschler v. Falkenstein <ctrl-t@users.sourceforge.net>
2007-11-26 21:30:00 +0000

    prString_FindRegexp
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@6679 a380766d-ff14-0410-b294-a243070f3f08

---
+++ Source/lang/LangPrimSource/PyrUStringPrim.cpp
@@ -0,0 +1,184 @@
+/*
+	SuperCollider real time audio synthesis system
+    Copyright (c) 2002 James McCartney. All rights reserved.
+	http://www.audiosynth.com
+
+	Primitives for regular expression search by Florian Schmidt, adapted to ICU by Jan Trutzschler
+	
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+*/
+#ifdef SC_DARWIN
+
+struct URegularExpression;
+/**
+* Structure represeting a compiled regular rexpression, plus the results
+ *    of a match operation.
+ * @draft ICU 3.0
+ */
+typedef struct URegularExpression URegularExpression;
+
+#define U_HIDE_DRAFT_API 1
+#define U_DISABLE_RENAMING 1
+#include "uregex.h"
+#include "ustring.h"
+#include "udraft.h"
+#include "PyrPrimitive.h"
+#include "PyrKernel.h"
+#include "GC.h"
+
+struct SCRegExRegion {
+    int start, end, group, matched;
+};
+typedef struct SCRegExRegion SCRegExRegion;
+
+int prString_FindRegexp(struct VMGlobals *g, int numArgsPushed);
+int prString_FindRegexp(struct VMGlobals *g, int numArgsPushed)
+{
+	int err;
+
+	PyrSlot *a = g->sp - 2; // source string
+	PyrSlot *b = g->sp - 1; // pattern
+	PyrSlot *c = g->sp;     // offset
+		
+	if (!isKindOfSlot(b, class_string) || (c->utag != tagInt)) return errWrongType;
+
+	int offset = c->ui;
+	int stringsize = a->uo->size + 1;
+	int patternsize =  b->uo->size + 1;
+	char *string = (char*)malloc(a->uo->size + 1);
+	err = slotStrVal(a, string, a->uo->size + 1);
+	if (err) return err;
+	
+	char *pattern = (char*)malloc(b->uo->size + 1);
+	err = slotStrVal(b, pattern, b->uo->size + 1);
+	if (err) return err;
+	UParseError uerr;
+	UErrorCode status = (UErrorCode)0;
+	UChar *regexStr;
+	UChar *ustring;
+	
+	regexStr =  (UChar*)malloc((patternsize)*sizeof(UChar));
+	u_charsToUChars (pattern, regexStr, patternsize);
+	
+	ustring =  (UChar*)malloc((stringsize)*sizeof(UChar));
+	u_charsToUChars (string+offset, ustring, stringsize-offset);
+
+	unsigned flags = UREGEX_MULTILINE;
+	int groupNumber = 0;
+	SCRegExRegion * what;
+	int indx = 0;
+	int size = 0;
+	int MAXREGEXFIND = 100;
+		
+	URegularExpression *expression = uregex_open(regexStr, -1, flags, &uerr, &status);
+	if(U_FAILURE(status)) goto nilout;
+	
+	 if(!U_FAILURE(status)) {
+		uregex_setText(expression, ustring, -1, &status);
+		what =  (SCRegExRegion*)malloc((MAXREGEXFIND)*sizeof(SCRegExRegion));
+		for(int i=0; i< MAXREGEXFIND; i++)
+		{
+			SCRegExRegion range;
+			range.matched = false;
+			what[i] = range;
+		}
+
+		int32_t groups = uregex_groupCount(expression, &status) + 1;
+		if(U_FAILURE(status)) goto nilout;
+		
+//		post("groups: %i\n", groups);
+		while (uregex_findNext(expression, &status))
+		{
+			if(U_FAILURE(status)) return errNone;
+
+			for(int i=0; i< groups; ++i){
+				what[size].group = i;
+				what[size].start = sc_clip(uregex_start(expression, i, &status), 0, stringsize) ;
+				if(U_FAILURE(status)) goto nilout;
+				what[size].end = sc_clip(uregex_end(expression, i, &status), 0, stringsize);
+				what[size].matched = true;
+//				post("index:%i, size:%i, start %i, end %i\n", i, size, what[i].start, what[i].end);
+				size = indx++ + 1;
+				if(U_FAILURE(status)) goto nilout;
+			}
+		}
+		
+		PyrObject *result_array = newPyrArray(g->gc, size, 0, true);
+		result_array->size = 0;
+
+		if (size>0) //(matched)
+		{
+			for (int i = 0; i < size; i++)
+			{
+				if (what[0].matched == false)
+				{
+					result_array->size++;
+					SetNil(result_array->slots+i);
+				}
+				else
+				{
+					result_array->size++;
+	
+					int match_start =  what[i].start;
+					int match_length = what[i].end -  what[i].start;
+//					post("for i:%i, start %i, end %i\n",  i, what[i].start,  what[i].end);
+					char *match = (char*)malloc(match_length);
+					strncpy(match, string + offset + match_start, match_length);
+					match[match_length] = 0;
+					PyrObject *array = newPyrArray(g->gc, 2, 0, true);
+					array->size = 2;
+					SetInt(array->slots, match_start + offset);
+	
+					PyrObject *matched_string = (PyrObject*)newPyrString(g->gc, match, 0, true);
+					SetObject(array->slots+1, matched_string);
+					g->gc->GCWrite(matched_string, array->slots + 1);
+	
+					SetObject(result_array->slots + i, array);
+					g->gc->GCWrite(array, result_array->slots + i);
+				}
+			}
+		}
+		else
+		{
+			SetNil(a);
+			return errNone;
+		}
+	
+		SetObject(a, result_array);
+		g->gc->GCWrite(result_array,a);
+		free(what);
+		//uregex_close(expression);
+		return errNone;	
+	}
+
+		nilout:
+			SetNil(a);
+			return errNone;
+}
+
+void initUStringPrimitives();
+void initUStringPrimitives()
+{
+	int base, index = 0;
+	base = nextPrimitiveIndex();
+	definePrimitive(base, index++, "_String_FindRegexp", prString_FindRegexp, 3, 0);	
+}
+
+#else // !SC_DARWIN
+void initHIDPrimitives()
+{
+	//other platforms? - icu should be running on linux too
+}
+#endif // SC_DARWIN
