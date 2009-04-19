daecc0054b105746a806d939f72063289de4eb36
Dan Stowell <danstowell@users.sourceforge.net>
2009-04-19 20:04:02 +0000

    Pass parameters through from compilation scripts to the xcode calls, therefore allowing users to specify that they don't want UB builds
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9078 a380766d-ff14-0410-b294-a243070f3f08

--- compile.sh
+++ compile.sh
@@ -2,16 +2,26 @@
 
 # This script builds the server, the plugins and the application (sclang), one after another.
 
+if [ $# == 0 ]; then
+	echo "--------------------------------------------------------------------------------
+Building SuperCollider as a UNIVERSAL BINARY (intel + ppc).
+For faster build, or if you don't have UB versions of the libs (e.g. libsndfile),
+you may specify your computer's architecture by adding ARCHS=i386 or ARCHS=ppc 
+as a parameter to this script.
+--------------------------------------------------------------------------------
+"
+fi
+
 # scsynth
 echo "Building scsynth..."
-xcodebuild -project Synth.xcodeproj -target "All" -configuration "Deployment" build || exit
+xcodebuild -project Synth.xcodeproj -target "All" -configuration "Deployment" $* build || exit
 
 # plugins
 echo "Building plugins..."
-xcodebuild -project Plugins.xcodeproj -target "All" -configuration "Deployment" build || exit
+xcodebuild -project Plugins.xcodeproj -target "All" -configuration "Deployment" $* build || exit
 
 #sclang
 echo "Building sclang..."
-xcodebuild -project Language.xcodeproj -target "All" -configuration "Deployment" build || exit
+xcodebuild -project Language.xcodeproj -target "All" -configuration "Deployment" $* build || exit
 
 echo "Done."
