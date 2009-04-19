221886840fc31ba9174679679916145aa4047723
Dan Stowell <danstowell@users.sourceforge.net>
2009-04-19 20:04:02 +0000

    Pass parameters through from compilation scripts to the xcode calls, therefore allowing users to specify that they don't want UB builds
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9078 a380766d-ff14-0410-b294-a243070f3f08

--- clean-compile.sh
+++ clean-compile.sh
@@ -2,28 +2,38 @@
 
 # This script cleans all targets, then builds the projects for the server, the plugins and the application (sclang) projects, one after another.
 
+if [ $# == 0 ]; then
+	echo "--------------------------------------------------------------------------------
+Building SuperCollider as a UNIVERSAL BINARY (intel + ppc).
+For faster build, or if you don't have UB versions of the libs (e.g. libsndfile),
+you may specify your computer's architecture by adding ARCHS=i386 or ARCHS=ppc 
+as a parameter to this script.
+--------------------------------------------------------------------------------
+"
+fi
+
 # clean
 echo "Cleaning scsynth..."
-xcodebuild -project Synth.xcodeproj -target "All" clean || exit
+xcodebuild -project Synth.xcodeproj -target "All" $* clean || exit
 
 echo "Cleaning plugins..."
-xcodebuild -project Plugins.xcodeproj -target "All" clean || exit
+xcodebuild -project Plugins.xcodeproj -target "All" $* clean || exit
 
 echo "Cleaning sclang..."
-xcodebuild -project Language.xcodeproj -target "All" clean || exit
+xcodebuild -project Language.xcodeproj -target "All" $* clean || exit
 
 echo "Clean Done."
 
 # scsynth
 echo "Building scsynth..."
-xcodebuild -project Synth.xcodeproj -target "All" -configuration "Deployment" build || exit
+xcodebuild -project Synth.xcodeproj -target "All" -configuration "Deployment" $* build || exit
 
 # plugins
 echo "Building plugins..."
-xcodebuild -project Plugins.xcodeproj -target "All" -configuration "Deployment" build || exit
+xcodebuild -project Plugins.xcodeproj -target "All" -configuration "Deployment" $* build || exit
 
 #sclang
 echo "Building sclang..."
-xcodebuild -project Language.xcodeproj -target "All" -configuration "Deployment" build || exit
+xcodebuild -project Language.xcodeproj -target "All" -configuration "Deployment" $* build || exit
 
 echo "Done."
