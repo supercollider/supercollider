c6a968ed4d3afb1640fab2e41ef20bff4d05643f
Dan Stowell <danstowell@users.sourceforge.net>
2009-04-19 21:59:49 +0000

    Update address of Free Software Foundation (debian source-checking tools produce warnings about this)
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9079 a380766d-ff14-0410-b294-a243070f3f08

--- Headers/common/SC_fftlib.h
+++ Headers/common/SC_fftlib.h
@@ -15,7 +15,7 @@ An interface to abstract over different FFT libraries, for SuperCollider 3.
 
     You should have received a copy of the GNU General Public License
     along with this program; if not, write to the Free Software
-    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
 NOTE:
 vDSP uses a "SplitBuf" as an intermediate representation of the data.
