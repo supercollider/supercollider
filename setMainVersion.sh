f5a8097cbadd70b8e4b8ce985ccfb34994778b18
Dan Stowell <danstowell@users.sourceforge.net>
2009-06-28 09:50:10 +0000

    Add a safety-check to setMainVersion.sh
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9276 a380766d-ff14-0410-b294-a243070f3f08

--- setMainVersion.sh
+++ setMainVersion.sh
@@ -1,5 +1,14 @@
-versionline=$( grep "classvar" VERSION )
 mainfile="build/SCClassLibrary/DefaultLibrary/Main.sc"
+
+# Double-check that there's exactly one in each!
+count1=$( grep -c "classvar" VERSION )
+count2=$( grep -c "classvar scVersion" $mainfile )
+if [ $count1 != 1 ] || [ $count2 != 1 ]; then
+	echo "ERROR in setMainVersion.sh: we require exactly one 'classvar' line to be detected. Check VERSION and Main.sc."
+	exit
+fi
+
+versionline=$( grep "classvar" VERSION )
 line=$( grep "classvar scVersion" $mainfile )
 
 echo $versionline
