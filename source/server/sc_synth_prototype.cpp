ce62c3b76ff876f19e025358f5da3c140e620b53
Tim Blechmann <tim@klingt.org>
2009-07-31 10:49:22 +0200

    server: prototyped synthdef loading
    Signed-off-by: Tim Blechmann <tim@klingt.org>

---
+++ source/server/sc_synth_prototype.cpp
@@ -0,0 +1,57 @@
+//  prototype of a supercollider-synthdef-based synth prototype, implementation
+//  Copyright (C) 2009 Tim Blechmann
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; see the file COPYING.  If not, write to
+//  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+//  Boston, MA 02111-1307, USA.
+
+#include <boost/filesystem/operations.hpp>
+
+#include "sc_synth_prototype.hpp"
+#include "synth_factory.hpp"
+
+namespace nova
+{
+
+using namespace std;
+
+void sc_read_synthdefs_dir(synth_factory & factory, path const & dir)
+{
+    using namespace boost::filesystem;
+
+    if (!exists(dir))
+        return;
+
+    directory_iterator end;
+    for (directory_iterator it(dir);
+         it != end; ++it)
+    {
+        if (is_directory(it->status()))
+            sc_read_synthdefs_dir(factory, it->path());
+        else
+        {
+            try {
+                auto_ptr<sc_synth_prototype> sp(new sc_synth_prototype(it->path()));
+                factory.register_prototype(sp.get());
+                sp.release();
+            }
+            catch(std::exception & e)
+            {
+                cout << "Exception when parsing synthdef: " << e.what() << endl;
+            }
+        }
+    }
+}
+
+} /* namespace nova */
---
+++ source/server/sc_synth_prototype.hpp
@@ -0,0 +1,54 @@
+//  prototype of a supercollider-synthdef-based synth prototype
+//  Copyright (C) 2009 Tim Blechmann
+//
+//  This program is free software; you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation; either version 2 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program; see the file COPYING.  If not, write to
+//  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+//  Boston, MA 02111-1307, USA.
+
+#ifndef SC_SYNTH_PROTOTYPE_HPP
+#define SC_SYNTH_PROTOTYPE_HPP
+
+#include <boost/filesystem/path.hpp>
+
+#include "synth_prototype.hpp"
+
+namespace nova
+{
+
+using boost::filesystem::path;
+
+/* read synthdefs from path pattern */
+void sc_read_synthdefs_dir(class synth_factory & factory, path const & p);
+
+class sc_synth_prototype:
+    public synth_prototype
+{
+public:
+    sc_synth_prototype(path const & p):
+        synth_prototype(p.filename())
+    {}
+
+private:
+    virtual synth * create_instance(int)
+    {
+        return 0;
+    }
+};
+
+
+
+} /* namespace nova */
+
+
+#endif /* SC_SYNTH_PROTOTYPE_HPP */
