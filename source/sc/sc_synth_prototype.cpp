023470a0d144c7be4ff897e0f9c32c557d2c4cdb
Tim Blechmann <tim@klingt.org>
2009-08-24 15:25:11 +0200

    sc plugin loader: add missing header
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- source/sc/sc_synth_prototype.cpp
+++ source/sc/sc_synth_prototype.cpp
@@ -16,6 +16,8 @@
 //  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 //  Boston, MA 02111-1307, USA.
 
+#include <iostream>
+
 #include <boost/filesystem/operations.hpp>
 
 #include "sc_synth.hpp"
