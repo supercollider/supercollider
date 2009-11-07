d576162a5bd51b352035cc80aad704527daa88cc
Tim Blechmann <timblech@users.sourceforge.net>
2009-11-07 17:35:01 +0000

    updated nova-simd to latest git snapshot
    Signed-off-by: Tim Blechmann <tim@klingt.org>
    
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9486 a380766d-ff14-0410-b294-a243070f3f08

--- nova-simd/simd_memory_fallback_double.hpp
+++ nova-simd/simd_memory_fallback_double.hpp
@@ -58,24 +58,6 @@ inline void copyvec_simd_mp(double * dest, const double * src)
     copyvec_simd(dest, src, n);
 }
 
-template <unsigned int n>
-inline void addvec_simd(double * dest, const double * in)
-{
-    addvec_simd(dest, in, n);
-}
-
-template <unsigned int n>
-inline void addvec_simd(double * dest, const double in)
-{
-    addvec_simd(dest, in, n);
-}
-
-template <unsigned int n>
-inline void addvec_simd(double * dest, const double in, const double slope)
-{
-    addvec_simd(dest, in, slope, n);
-}
-
 } /* namespace nova */
 
 #endif /* SIMD_MEMORY_FALLBACK_DOUBLE_HPP */
