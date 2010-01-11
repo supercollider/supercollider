8963a41943b268d3a42539d91d9c836ef03a404f
Axel Balley <axelballey@users.sourceforge.net>
2010-01-11 19:27:28 +0000

    fixed slot type in netAddrSend when using TCP this fixes SwingOSC compatiblity
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9674 a380766d-ff14-0410-b294-a243070f3f08

--- Source/lang/LangPrimSource/OSCData.cpp
+++ Source/lang/LangPrimSource/OSCData.cpp
@@ -299,8 +299,8 @@ int netAddrSend(PyrObject *netAddrObj, int msglen, char *bufptr, bool sendMsgLen
 {
 	int err, port, addr;
 
-	if (IsObj(netAddrObj->slots + ivxNetAddr_Socket)) {
-		SC_TcpClientPort* comPort = (SC_TcpClientPort*)slotRawObject(netAddrObj->slots + ivxNetAddr_Socket);
+	if (IsPtr(netAddrObj->slots + ivxNetAddr_Socket)) {
+		SC_TcpClientPort* comPort = (SC_TcpClientPort*)slotRawPtr(netAddrObj->slots + ivxNetAddr_Socket);
 
 		// send TCP
 		int tcpSocket = comPort->Socket();
