85e6c8d3c6703f62b0fd800d04a869b4351d68e0
Dan Stowell <danstowell@users.sourceforge.net>
2010-01-28 14:21:01 +0000

    rm duplication: clean-compile.sh invokes compile.sh rather than replicating its contents
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9712 a380766d-ff14-0410-b294-a243070f3f08

--- clean-compile.sh
+++ clean-compile.sh
@@ -1,16 +1,6 @@
 #!/bin/sh
 
-# This script cleans all targets, then builds the projects for the server, the plugins and the application (sclang) projects, one after another.
-
-if [ $# == 0 ]; then
-	echo "--------------------------------------------------------------------------------
-Building SuperCollider as a UNIVERSAL BINARY (intel + ppc).
-For faster build, or if you don't have UB versions of the libs (e.g. libsndfile),
-you may specify your computer's architecture by adding ARCHS=i386 or ARCHS=ppc 
-as a parameter to this script.
---------------------------------------------------------------------------------
-"
-fi
+# This script cleans all targets, then invokes compile.sh
 
 # clean
 echo "Cleaning scsynth..."
@@ -24,20 +14,5 @@ xcodebuild -project Language.xcodeproj -target "All" $* clean || exit
 
 echo "Clean Done."
 
-# scsynth
-echo "Building scsynth..."
-xcodebuild -project Synth.xcodeproj -target "AllExceptAU" -configuration "Deployment" $* build || exit
-
-# plugins
-echo "Building plugins..."
-xcodebuild -project Plugins.xcodeproj -target "All" -configuration "Deployment" $* build || exit
-
-#sclang
-echo "Building sclang..."
-xcodebuild -project Language.xcodeproj -target "All" -configuration "Deployment" $* build || exit
-
-# scau
-echo "Building SuperColliderAU..."
-xcodebuild -project Synth.xcodeproj -target "SuperColliderAU" -configuration "Deployment" $* build || exit
-
-echo "Done."
+# Now let compile.sh do its job:
+./compile.sh $*
