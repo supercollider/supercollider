75265921cd50abdfadf96a185afd0169bc88f879
Dan Stowell <danstowell@users.sourceforge.net>
2010-02-09 21:45:13 +0000

    rearranging furniture: updates to many build scripts - not yet complete
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9823 a380766d-ff14-0410-b294-a243070f3f08

--- mac/clean-compile.sh
+++ mac/clean-compile.sh
@@ -2,6 +2,18 @@
 
 # This script cleans all targets, then invokes compile.sh
 
+# Replicate warning from compile.sh, because it's user-friendly for it to be the first output:
+if [ $# == 0 ]; then
+	echo "--------------------------------------------------------------------------------
+Building SuperCollider as a UNIVERSAL BINARY (intel + ppc).
+For faster build, or if you don't have UB versions of the libs (e.g. libsndfile),
+you may specify your computer's architecture by adding ARCHS=i386 or ARCHS=ppc 
+as a parameter to this script.
+--------------------------------------------------------------------------------
+"
+fi
+
+
 # clean
 echo "Cleaning scsynth..."
 xcodebuild -project Synth.xcodeproj -target "All" $* clean || exit
