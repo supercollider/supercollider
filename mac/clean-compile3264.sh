2fb5cf5bccf9db744895fe05a95faf8ba8bf9b9c
Joshua Parmenter <joshpar@users.sourceforge.net>
2010-02-13 06:07:31 +0000

    compile scripts for 32 / 64 bit 10.6 builds
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9845 a380766d-ff14-0410-b294-a243070f3f08

---
+++ mac/clean-compile3264.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# This script cleans all targets, then invokes compile.sh
+
+# Replicate warning from compile.sh, because it's user-friendly for it to be the first output:
+if [ $# == 0 ]; then
+	echo "--------------------------------------------------------------------------------
+Building SuperCollider as a UNIVERSAL BINARY (intel + x86_64 + ppc).
+For faster build, or if you don't have UB versions of the libs (e.g. libsndfile),
+you may specify your computer's architecture by adding ARCHS=i386, 
+ARCHS=x86_64 or ARCHS=ppc as a parameter to this script.
+--------------------------------------------------------------------------------
+"
+fi
+
+
+# clean
+echo "Cleaning scsynth..."
+xcodebuild -project Synth.xcodeproj -target "All" $* clean || exit
+
+echo "Cleaning plugins..."
+xcodebuild -project Plugins.xcodeproj -target "All" $* clean || exit
+
+echo "Cleaning sclang..."
+xcodebuild -project Language.xcodeproj -target "All" $* clean || exit
+
+echo "Clean Done."
+
+# Now let compile.sh do its job:
+./compile3264.sh $*
