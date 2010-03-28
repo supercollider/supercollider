a69b9a546f50bf1a203aad8e1764bcc0b32ddc3f
Tim Blechmann <timblech@users.sourceforge.net>
2010-03-28 17:16:09 +0000

    PyrPrimitives: use SlotEq helper function
    Signed-off-by: Tim Blechmann <tim@klingt.org>
    
    git-svn-id: https://supercollider.svn.sourceforge.net/svnroot/supercollider/trunk@9996 a380766d-ff14-0410-b294-a243070f3f08

--- common/Source/lang/LangPrimSource/PyrPrimitive.cpp
+++ common/Source/lang/LangPrimSource/PyrPrimitive.cpp
@@ -2452,7 +2452,7 @@ int prObjectPointsTo(struct VMGlobals *g, int numArgsPushed)
 		obj = slotRawObject(a);
 		for (i=0; i<obj->size; ++i) {
 			getIndexedSlot(obj, &temp, i);
-			if (GetTag(&temp) == GetTag(b) && slotRawInt(&temp) == slotRawInt(b)) {
+			if (SlotEq(&temp, b)) {
 				slotCopy(a,&o_true);
 				return errNone;
 			}
@@ -2948,7 +2948,7 @@ void initPyrThread(VMGlobals *g, PyrThread *thread, PyrSlot *func, int stacksize
 	PyrSlot* currentEnvironmentSlot = &g->classvars->slots[1];
 	slotCopy(&thread->environment,currentEnvironmentSlot);
 	gc->GCWrite(thread, currentEnvironmentSlot);
-	
+
 	if(g->process) { // check we're not just starting up
 		PyrSlot* executingPath = &g->process->nowExecutingPath;
 		slotCopy(&thread->executingPath,&g->process->nowExecutingPath);
@@ -3175,7 +3175,7 @@ int prRoutineResume(struct VMGlobals *g, int numArgsPushed)
 	thread = slotRawThread(a);
 	state = slotRawInt(&thread->state);
 	//postfl("->prRoutineResume %d\n", state);
-	
+
 	slotCopy(&thread->oldExecutingPath,&g->process->nowExecutingPath);
 	slotCopy(&g->process->nowExecutingPath, &thread->executingPath);
 	if (state == tInit) {
@@ -3186,7 +3186,7 @@ int prRoutineResume(struct VMGlobals *g, int numArgsPushed)
 		//post("thread %08X\n", thread);
 		SetObject(&thread->parent, g->thread);
 		g->gc->GCWrite(thread, g->thread);
-		
+
 		SetRaw(&thread->beats, slotRawFloat(&g->thread->beats));
 		SetRaw(&thread->seconds, slotRawFloat(&g->thread->seconds));
 		slotCopy(&thread->clock, &g->thread->clock);
@@ -3347,7 +3347,7 @@ int prRoutineYieldAndReset(struct VMGlobals *g, int numArgsPushed)
 	switchToThread(g, parent, state, &numArgsPushed);
 	// on the other side of the looking glass, put the yielded value on the stack as the result..
 	slotCopy((g->sp - numArgsPushed + 1),&value);
-	
+
 	//slotCopy(&g->process->nowExecutingPath, &g->thread->oldExecutingPath);
 
 	//post("<-prRoutineYieldAndReset\n");
