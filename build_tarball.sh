addc6b8405836cf7a1dfa3d0ed5afcfa2a438f8c
Tim Blechmann <tim@klingt.org>
2010-04-25 18:06:26 +0200

    tarball build script: we ship a subset of the sc3-plugins
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- build_tarball.sh
+++ build_tarball.sh
@@ -7,7 +7,6 @@ bash ./git-archive-all.sh --prefix $VERSION/ $TARBALL
 
 # we only need one instance of boost
 tar -f $TARBALL --delete $VERSION/libs/nova-tt/boost_lockfree/
-tar -f $TARBALL --delete $VERSION/source/sc/sc3-plugins
 
 bzip2 -k --best $TARBALL &
 lzma  -k --best $TARBALL &
