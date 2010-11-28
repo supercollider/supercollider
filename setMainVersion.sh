e6b75c50eb48bb3966eccff937063aa27850a555
Tim Blechmann <tim@klingt.org>
2010-11-28 22:36:34 +0100

    reorganize cmake build system
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- setMainVersion.sh
+++ setMainVersion.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-mainfile="build/SCClassLibrary/DefaultLibrary/Main.sc"
+mainfile="SCClassLibrary/DefaultLibrary/Main.sc"
 
 # Double-check that there's exactly one in each!
-count1=$( grep -c "classvar" ../VERSION )
+count1=$( grep -c "classvar" VERSION )
 count2=$( grep -c "classvar scVersion" $mainfile )
 if [ $count1 != 1 ] || [ $count2 != 1 ]; then
 	echo "ERROR in setMainVersion.sh: we require exactly one 'classvar' line to be detected. Check ../VERSION and Main.sc."
 	exit
 fi
 
-versionline=$( grep "classvar" ../VERSION )
+versionline=$( grep "classvar" VERSION )
 line=$( grep "classvar scVersion" $mainfile )
 
 echo $versionline
