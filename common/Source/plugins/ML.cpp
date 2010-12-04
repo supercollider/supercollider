5a3a6a1f515f4150f0199a874413c7aed3f5bae4
Tim Blechmann <tim@klingt.org>
2010-12-04 11:28:07 +0100

    move FFT_UGens.h to the public API
    define ToPolarApx and ToComplexApx as static inline to make the file
    header-only
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- common/Source/plugins/ML.cpp
+++ common/Source/plugins/ML.cpp
@@ -25,34 +25,6 @@
 
 InterfaceTable *ft;
 
-SCPolarBuf* ToPolarApx(SndBuf *buf)
-{
-	if (buf->coord == coord_Complex) {
-		SCComplexBuf* p = (SCComplexBuf*)buf->data;
-		int numbins = buf->samples - 2 >> 1;
-		for (int i=0; i<numbins; ++i) {
-			p->bin[i].ToPolarApxInPlace();
-		}
-		buf->coord = coord_Polar;
-	}
-	return (SCPolarBuf*)buf->data;
-}
-
-SCComplexBuf* ToComplexApx(SndBuf *buf)
-{
-	if (buf->coord == coord_Polar) {
-		SCPolarBuf* p = (SCPolarBuf*)buf->data;
-		int numbins = buf->samples - 2 >> 1;
-		for (int i=0; i<numbins; ++i) {
-			p->bin[i].ToComplexApxInPlace();
-		}
-		buf->coord = coord_Complex;
-	}
-	return (SCComplexBuf*)buf->data;
-}
-
-
-
 extern "C" void load(InterfaceTable *inTable)
 {
 	ft = inTable;
