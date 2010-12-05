9b047404c3045e931635cffb2e93216c885a0fe0
Tim Blechmann <tim@klingt.org>
2010-12-05 14:09:08 +0100

    UnpackFFTUGens: fix compilation for new sc complex class
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- common/Source/plugins/UnpackFFTUGens.cpp
+++ common/Source/plugins/UnpackFFTUGens.cpp
@@ -63,23 +63,7 @@ extern "C"
 	void PackFFT_next(PackFFT *unit, int inNumSamples);
 }
 
-SCComplexBuf* ToComplexApx(SndBuf *buf)
-{
-	if (buf->coord == coord_Polar) {
-		SCPolarBuf* p = (SCPolarBuf*)buf->data;
-		int numbins = buf->samples - 2 >> 1;
-		for (int i=0; i<numbins; ++i) {
-			p->bin[i].ToComplexApxInPlace();
-		}
-		buf->coord = coord_Complex;
-	}
-	return (SCComplexBuf*)buf->data;
-}
-
 InterfaceTable *ft;
-
-void init_SCComplex(InterfaceTable *inTable);
-
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
 
