2c3ed5e35925bb14d53cc2e6af8f824888fbf605
Tim Blechmann <tim@klingt.org>
2010-12-07 18:33:37 +0100

    fix build_tarball script
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- build_tarball.sh
+++ build_tarball.sh
@@ -6,10 +6,9 @@ TARBALL=$VERSION.tar
 bash ./git-archive-all.sh --prefix $VERSION/ $TARBALL
 
 # we only need one instance of boost
-tar -f $TARBALL --delete $VERSION/libs/nova-tt/boost_lockfree/
+tar -f $TARBALL --delete $VERSION/external_libraries/nova-tt/boost_lockfree/
 
 bzip2 -k --best $TARBALL &
-lzma  -k --best $TARBALL &
 
 wait
 rm $TARBALL
