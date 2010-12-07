642d5dc6aec3528ed54e0ecf21f796f5e7a71f8b
Tim Blechmann <tim@klingt.org>
2010-12-08 00:57:05 +0100

    libs: import missing boost libraries & update extract_boost script
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- external_libraries/extract_boost.sh
+++ external_libraries/extract_boost.sh
@@ -6,10 +6,13 @@
 # root of the boost source tree
 BOOST_ROOT=/home/tim/t/boost_1_45_0/
 
-bcp --boost=${BOOST_ROOT} --scan server/supernova/*/*pp external_libraries/boost
-
-# we don't use regex
-rm -rf external_libraries/boost/*/regex
+bcp --boost=${BOOST_ROOT} \
+	--scan server/supernova/*/*pp \
+	--scan external_libraries/boost_*/*/*pp \
+	--scan external_libraries/boost_*/*/*/*pp \
+	--scan external_libraries/boost_*/*/*/*/*pp \
+	--scan testsuite/supernova/*pp \
+	external_libraries/boost
 
 # nor do we need tests and the bjam build files
 rm -rf external_libraries/boost/Jamroot
