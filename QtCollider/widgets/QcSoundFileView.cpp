b97c26330c3d9474178b1b009a22fc374648c63e
Jakob Leben <jakob.leben@gmail.com>
2010-12-12 03:42:46 +0100

    QcSoundFileView: further work

--- QtCollider/widgets/QcSoundFileView.cpp
+++ QtCollider/widgets/QcSoundFileView.cpp
@@ -26,9 +26,9 @@ QcSoundFileView::QcSoundFileView() :
 {
   waveform = new QcWaveform();
 
-  timeScrollBar = new QScrollBar();
+  timeScrollBar = new QScrollBar( Qt::Horizontal );
 
-  zoomScrollBar = new QScrollBar();
+  zoomScrollBar = new QSlider();
   zoomScrollBar->setRange( 0, 1000 );
 
   progressBar = new QProgressBar();
@@ -36,9 +36,9 @@ QcSoundFileView::QcSoundFileView() :
 
   QGridLayout *l = new QGridLayout();
   l->addWidget( waveform , 0, 0 );
-  l->addWidget( timeScrollBar, 0, 1 );
-  l->addWidget( zoomScrollBar, 0, 2 );
-  l->addWidget( progressBar, 1, 0, 1, 3 );
+  l->addWidget( zoomScrollBar, 0, 1 );
+  l->addWidget( timeScrollBar, 1, 0, 1, 2 );
+  l->addWidget( progressBar, 1, 0, 1, 2 );
 
   progressBar->hide();
 
@@ -89,7 +89,11 @@ void QcSoundFileView::updateTimeScrollBar()
     hScrollMultiplier = 1.0;
   }
   timeScrollBar->setMaximum( max );
-  //timeScrollBar->setSliderPosition( waveform->viewStart() / hScrollMultiplier );
+  //printf("before: %Li\n", waveform->viewStart() );
+  timeScrollBar->blockSignals( true );
+  timeScrollBar->setSliderPosition( waveform->viewStart() / hScrollMultiplier );
+  timeScrollBar->blockSignals( false );
+  //printf("after: %Li\n", waveform->viewStart() );
   timeScrollBar->setEnabled( max > 0 );
 }
 
@@ -101,6 +105,7 @@ void QcSoundFileView::updateZoomScrollBar()
 
 QcWaveform::QcWaveform( QWidget * parent ) : QWidget( parent ),
   sf(0),
+  _cache(0),
   peakCache(0),
   _beg(0.0),
   _dur(0.0),
@@ -113,7 +118,6 @@ QcWaveform::QcWaveform( QWidget * parent ) : QWidget( parent ),
 QcWaveform::~QcWaveform()
 {
   if( sf ) sf_close( sf );
-  delete [] peakCache;
   delete pixmap;
 }
 
@@ -134,7 +138,7 @@ void QcWaveform::load( const QString& filename )
   _beg = 0;
   _dur = sfInfo.frames;
 
-  rebuildCache( 128 );
+  rebuildCache( 128, 10000 );
 
   redraw();
 }
@@ -185,57 +189,22 @@ void QcWaveform::paintEvent( QPaintEvent *ev )
   p.drawPixmap( ev->rect(), *pixmap, ev->rect() );
 }
 
-void QcWaveform::rebuildCache ( int resolution )
+void QcWaveform::rebuildCache ( int maxFPU, int maxRawFrames )
 {
-  delete [] peakCache;
+  delete _cache;
+  _cache = 0;
   peakCache = 0;
 
-  _cacheSize = (sfInfo.frames / resolution);
-  // recalculate frames-per-cache after cache size rounded down
-  _fpcache = (double) sfInfo.frames / _cacheSize;
+  SoundCacheStream *cache = new SoundCacheStream( sf, sfInfo, maxFPU, maxRawFrames );
 
-  PeakCache *cache = new PeakCache [sfInfo.channels];
-  int ch;
-  for( ch = 0; ch < sfInfo.channels; ++ch ) {
-    cache[ch].min = new short [_cacheSize];
-    cache[ch].max = new short [_cacheSize];
-  }
+  _cache = cache;
 
-  double f_pos = 0;
-  int i;
-
-  for( i = 0; i < _cacheSize; ++i ) {
-
-    // there has to be one frame of overlap with the previous pixel's frames,
-    // to avoid discontinuity
-    int sf_pos = floor(f_pos);
-
-    f_pos += _fpcache;
-    if( f_pos > sfInfo.frames ) f_pos = sfInfo.frames;
-
-    int frame_count = ceil(f_pos) - sf_pos;
-
-    short buf[frame_count * sfInfo.channels];
-    sf_seek( sf, sf_pos, SEEK_SET );
-    int read_count = sf_readf_short( sf, buf, frame_count );
-
-    short *data = buf;
-    for( ch = 0; ch < sfInfo.channels; ++ch, ++data ) {
-      short min,max;
-      integrate( data, data, sfInfo.channels, read_count, min, max );
-      cache[ch].min[i] = min;
-      cache[ch].max[i] = max;
-    }
-
-    Q_EMIT( loadProgress( i * 100 / _cacheSize ) );
-    QApplication::processEvents();
-  }
-
-  peakCache = cache;
+  peakCache = cache->_caches;
+  _fpcache = cache->_fpu;
+  _cacheSize = cache->_cacheSize;
 
   Q_EMIT( loadingDone() );
 }
-
 
 void QcWaveform::draw( QPixmap *pix, int x, int width, double f_beg, double f_dur )
 
@@ -243,146 +212,87 @@ void QcWaveform::draw( QPixmap *pix, int x, int width, double f_beg, double f_du
   QPainter p( pix );
   p.fillRect( pix->rect(), QColor( 255,255,255 ) );
 
-  if( !sf || !peakCache ) return;
+  if( !sf || !_cache || !peakCache ) return;
 
   // Check for sane situation:
   if( f_beg < 0 || f_beg + f_dur > sfInfo.frames ) return;
 
   double fpp = f_dur / width;
   float chHeight = (float) pix->height() / sfInfo.channels;
+  SoundFileStream sfStream;
 
-  p.scale( 1.f, chHeight / 65535.f ); 
+  p.scale( 1.f, chHeight / 65535.f );
+  p.translate( (float) x, 32767.f );
 
-  if( fpp >= _fpcache ) {
+  if( fpp > 1.0 ) {
+    SoundStream *soundStream;
 
-    // use cached peaks
-    printf("peaks from cache\n");
-
-    p.translate( (float)x, 32767.f );
-
-    double cachePerPixel = fpp / _fpcache;
+    if( fpp >= _cache->fpu() ) {
+      soundStream = _cache;
+    }
+    else {
+      sfStream.load( sf, sfInfo, f_beg, f_dur );
+      soundStream = &sfStream;
+    }
+    
+    short minBuffer[width];
+    short maxBuffer[width];
 
     int ch;
-    for( ch = 0; ch < sfInfo.channels; ++ch ) {
-
-      double cache_pos = f_beg / _fpcache;
-      int i_beg, i_count;
+    for( ch = 0; ch < soundStream->channels(); ++ch ) {
+      bool ok = soundStream->integrate( ch, f_beg, f_dur, minBuffer, maxBuffer, width );
+//    printf("integrated ok: %i\n", ok);
+      Q_ASSERT( ok );
 
       int i;
       for( i = 0; i < width; ++i ) {
-        int i_cache_pos = ceil(cache_pos);
-        cache_pos += cachePerPixel;
-        // Due to possibility of floating point operation failures. See same issue below.
-        if( cache_pos > _cacheSize ) cache_pos = _cacheSize;
-        int frame_count = ceil(cache_pos) - i_cache_pos ;
-
-        short *minData = peakCache[ch].min + i_cache_pos;
-        short *maxData = peakCache[ch].max + i_cache_pos;
-
-        short min, max;
-        integrate( minData, maxData, 1, frame_count, min, max );
-
-        p.drawLine( x + i, min, x + i, max );
+        short min = minBuffer[i];
+        short max = maxBuffer[i];
+        if( max != min ) {
+          p.drawLine( x + i, min, x + i, max );
+        }
+        else {
+          p.drawPoint( x + i, min );
+        }
       }
 
       p.translate( 0.f, 65535.f );
     }
   }
   else {
-
-    // we have to read from file
+    // we are drawing lines between actual values
+    //printf("straight data from file\n");
 
     QPainterPath paths[sfInfo.channels];
 
+    int i_beg = f_beg;
+    int i_count = (int)(f_beg + f_dur) - i_beg + 1 ;
+
+    short buf[ i_count * sfInfo.channels];
+    sf_seek( sf, i_beg, SEEK_SET);
+    int i_read = sf_readf_short( sf, buf, i_count );
+
+    float ppf = 1 / fpp;
+    float dx = ((int) f_beg - f_beg) * ppf;
+
     int ch;
-
-    if( fpp > 1.0 ) {
-
-      // we are drawing peaks
-      printf("peaks from file\n");
-
-      quint64 buf_offset = floor(f_beg);
-      int buf_size = ceil(f_beg + f_dur) - buf_offset;
-
-      short buf[buf_size * sfInfo.channels];
-      sf_seek( sf, buf_offset, SEEK_SET);
-      int i_read = sf_readf_short( sf, buf, buf_size );
-
-      double f_pos = 0;
-      int i;
-
-      for( i = 0; i < width; ++i ) {
-
-        // there has to be one frame of overlap with the previous pixel's frames,
-        // to avoid discontinuity
-        //quint64 buf_pos = floor(f_pos) - buf_offset;
-        int buf_pos = floor(f_pos);
-
-        // increment position
-        // slower, but error-prone:
-        // f_pos = (double)(i+1) / width() * f_dur + f_beg;
-        // the following is a faster variant, but floating point operations are fallible,
-        // so we need to make sure we stay within the constraints of f_dur;
-        f_pos += fpp;
-        if( f_pos > f_dur ) f_pos = f_dur;
-
-        int frame_count = ceil(f_pos) - buf_pos;
-
-        Q_ASSERT( buf_pos + frame_count <= buf_size );
-
-        short *data = buf + (buf_pos * sfInfo.channels);
-
-        for( ch = 0; ch < sfInfo.channels; ++ch, ++data ) {
-          short min,max;
-          integrate( data, data, sfInfo.channels, frame_count, min, max );
-
-          // QPainter doesn't wanto to draw 0 length lines!
-          if( max != min ) {
-            p.drawLine( i + x, min + ( (ch + 0.5f ) * 65535.f ),
-                        i + x, max + ( (ch + 0.5f ) * 65535.f ) );
-          }
-          else {
-            p.drawPoint( i + x, min + ( (ch + 0.5f ) * 65535.f ) );
-          }
-        }
+    for( ch = 0; ch < sfInfo.channels; ++ch ) {
+      int f;
+      for( f = 0; f < i_read; ++f ) {
+        short s = buf[ f*sfInfo.channels + ch ];
+        float x = f * ppf + dx;
+        QPointF pt( x, (float) s );
+        if( f == 0 ) paths[ch].moveTo( pt );
+        else paths[ch].lineTo( pt );
       }
     }
 
-    else {
-
-      // we are drawing lines between actual values
-      printf("straight data from file\n");
-
-      int i_beg = f_beg;
-      int i_count = (int)(f_beg + f_dur) - i_beg + 1 ;
-
-      short buf[ i_count * sfInfo.channels];
-      sf_seek( sf, i_beg, SEEK_SET);
-      int i_read = sf_readf_short( sf, buf, i_count );
-
-      float ppf = 1 / fpp;
-      float dx = ((int) f_beg - f_beg) * ppf;
-
-      for( ch = 0; ch < sfInfo.channels; ++ch ) {
-        int f;
-        for( f = 0; f < i_read; ++f ) {
-          short s = buf[ f*sfInfo.channels + ch ];
-          float x = f * ppf + dx;
-          QPointF pt( x, (float) s );
-          if( f == 0 ) paths[ch].moveTo( pt );
-          else paths[ch].lineTo( pt );
-        }
-      }
-
-      p.translate( (float) x, 32767.f );
-
-      for( ch = 0; ch < sfInfo.channels; ++ch ) {
-        p.drawPath( paths[ch] );
-        p.translate( 0.f, 65535.f );
-      }
+    for( ch = 0; ch < sfInfo.channels; ++ch ) {
+      p.drawPath( paths[ch] );
+      p.translate( 0.f, 65535.f );
     }
-
   }
+
 }
 
 
@@ -399,4 +309,185 @@ inline void QcWaveform::integrate
     if( srcMin < min ) min = srcMin;
     if( srcMax > max ) max = srcMax;
   }
+}
+
+SoundFileStream::SoundFileStream() : _data(0), _dataSize(0), _dataOffset(0)
+{}
+
+SoundFileStream::SoundFileStream( SNDFILE *sf, const SF_INFO &info, double b, double d )
+: _data(0)
+{
+  load( sf, info, b, d );
+}
+
+SoundFileStream::~SoundFileStream()
+{
+  delete[] _data;
+}
+
+void SoundFileStream::load( SNDFILE *sf, const SF_INFO &info, double b, double d )
+{
+  delete[] _data;
+
+  ch = info.channels;
+  beg = b;
+  dur = d;
+
+  _dataOffset = floor(b);
+  _dataSize = ceil(b + d) - _dataOffset;
+
+  _data = new short [_dataSize * channels()];
+  sf_seek( sf, _dataOffset, SEEK_SET);
+  _dataSize = sf_readf_short( sf, _data, _dataSize );
+}
+
+bool SoundFileStream::integrate
+( int ch, double off, double dur, short *minBuffer, short *maxBuffer, int bufferSize )
+{
+  bool ok = _data != 0
+            && ch < channels()
+            && ( off >= beginning() )
+            && ( off + dur <= beginning() + duration() );
+  if( !ok ) return false;
+
+  double fpu = dur / bufferSize;
+  double f_pos = off - beginning();
+
+  int i;
+  for( i = 0; i < bufferSize; ++i ) {
+
+    // there has to be one frame of overlap with the previous unit's frames,
+    // to avoid discontinuity
+
+    //quint64 data_pos = floor(f_pos) - _dataOffset;
+    int data_pos = floor(f_pos);
+
+    // increment position
+
+    // slower, but error-prone:
+    // f_pos = (double)(i+1) / width() * f_dur + f_beg;
+
+    // the following is a faster variant, but floating point operations are fallible,
+    // so we need to make sure we stay within the constraints of f_dur;
+    f_pos += fpu;
+    if( f_pos > dur ) f_pos = dur;
+
+    int frame_count = ceil(f_pos) - data_pos;
+
+    Q_ASSERT( data_pos + frame_count <= _dataSize );
+
+    short *samples = _data + (data_pos * channels()) + ch;
+
+    short min = SHRT_MAX;
+    short max = SHRT_MIN;
+
+    int f; // frame
+    for( f = 0; f < frame_count; ++f, samples += channels() ){
+      short sample = *samples;
+      if( sample < min ) min = sample;
+      if( sample > max ) max = sample;
+    }
+
+    //printf("_dataSize %i, data_pos %i, frame_count %i, min %i, max %i\n",
+    //       _dataSize, data_pos, frame_count, min, max );
+    minBuffer[i] = min;
+    maxBuffer[i] = max;
+  }
+
+  return true;
+}
+
+SoundCacheStream::SoundCacheStream
+( SNDFILE *sf, const SF_INFO &info, int maxFramesPerUnit, int maxRawFrames )
+: SoundStream ( info.channels, 0.0, (double) info.frames ),
+  _caches( 0 )
+{
+  /*if( sfInfo.frames <= maxRawFrames ) {
+    _cacheSize = sfInfo.frames;
+    _fpu = 1.0;
+  }
+  else {
+    _cacheSize = maxRawFrames;
+    _fpu = (double) sfInfo.frames / _cacheSize;
+    if( _fpu > maxFramesPerUnit ) {
+      _cacheSize = sfInfo.frames / maxFramesPerUnit;
+      _fpu = (double) sfInfo.frames / _cacheSize;
+    }
+  }*/
+
+  _cacheSize = (info.frames / maxFramesPerUnit);
+  // recalculate frames-per-cache after cache size rounded down
+  _fpu = (double) info.frames / _cacheSize;
+
+
+  _caches = new PeakCache [info.channels];
+  int ch;
+  for( ch = 0; ch < info.channels; ++ch ) {
+    _caches[ch].min = new short [_cacheSize];
+    _caches[ch].max = new short [_cacheSize];
+  }
+
+
+  int i = 0;
+  while( i < _cacheSize ) {
+    int chunkSize = qMin( 100, _cacheSize - i );
+
+    SoundFileStream sfStream( sf, info, i * _fpu, chunkSize * _fpu );
+
+    for( ch = 0; ch < channels(); ++ch ) {
+      sfStream.integrateAll( ch, _caches[ch].min + i, _caches[ch].max + i, chunkSize );
+    }
+
+    i += chunkSize;
+
+    Q_EMIT( loadProgress( i * 100 / _cacheSize ) );
+    QApplication::processEvents();
+  }
+
+  Q_EMIT( loadingDone() );
+}
+
+SoundCacheStream::~SoundCacheStream()
+{
+  delete [] _caches;
+}
+
+bool SoundCacheStream::integrate
+( int ch, double off, double dur, short *minBuffer, short *maxBuffer, int bufferSize )
+{
+  // we assume that beginning() == 0
+
+  bool ok = ch < channels()
+            && ( off >= beginning() )
+            && ( off + dur <= beginning() + duration() )
+            && bufferSize <= dur * _fpu;
+  if( !ok ) return false;
+
+  double ratio = dur / _fpu / bufferSize;
+  double cache_pos = off / _fpu;
+  int i_beg, i_count;
+
+  int i;
+  for( i = 0; i < bufferSize; ++i ) {
+    int i_cache_pos = ceil(cache_pos);
+    cache_pos += ratio;
+    // Due to possibility of floating point operation failures.
+    if( cache_pos > _cacheSize ) cache_pos = _cacheSize;
+    int frame_count = ceil(cache_pos) - i_cache_pos ;
+
+    short min = SHRT_MAX;
+    short max = SHRT_MIN;
+    short *p_srcMin = _caches[ch].min + i_cache_pos;
+    short *p_srcMax = _caches[ch].max + i_cache_pos;
+    int f; // frame
+    for( f = 0; f < frame_count; ++f, ++p_srcMin, ++p_srcMax ) {
+      if( *p_srcMin < min ) min = *p_srcMin;
+      if( *p_srcMax > max ) max = *p_srcMax;
+    }
+
+    minBuffer[i] = min;
+    maxBuffer[i] = max;
+  }
+
+  return true;
 }
