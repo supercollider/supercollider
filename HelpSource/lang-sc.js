c2dd5c6b180860b8d015d745f5b5df4261184bf4
Jonatan Liljedahl <lijon@kymatica.com>
2011-03-04 11:05:41 +0100

    More syntax coloring fixes and css tweaks

--- HelpSource/lang-sc.js
+++ HelpSource/lang-sc.js
@@ -1,39 +1,35 @@
-/*
-SC.app has: class, text, stringColor, commentColor, symbolColor, numberColor
-
-        'PR_COMMENT': PR_COMMENT,
-        'PR_DECLARATION': PR_DECLARATION,
-        'PR_KEYWORD': PR_KEYWORD,
-        'PR_LITERAL': PR_LITERAL,
-        'PR_NOCODE': PR_NOCODE,
-        'PR_PLAIN': PR_PLAIN,
-        'PR_PUNCTUATION': PR_PUNCTUATION,
-        'PR_SOURCE': PR_SOURCE,
-        'PR_STRING': PR_STRING,
-        'PR_TAG': PR_TAG,
-        'PR_TYPE': PR_TYPE
-
-*/
 PR.registerLangHandler(
     PR.createSimpleLexer(
-        [
-         [PR.PR_PLAIN,       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
-         [PR.PR_STRING,     /^"(?:[^\\"]|\\.)*(?:"|$)/, null, '"'],
-        ],
-        [
-        [PR.PR_LITERAL,      /^\$./],
-         [PR.PR_LITERAL,     /^\\\w+/],
-         
-        [PR.PR_TAG, /^(?:super|thisFunctionDef|thisFunction|thisMethod|thisProcess|thisThread|this)\b/],
-      
-         [PR.PR_KEYWORD,     /^(?:true|false|nil|inf)\b/],
-         [PR.PR_DECLARATION, /^(?:var|classvar|const|arg)\b/],
-          [PR.PR_DECLARATION, /^\|/],
-                  
-         [PR.PR_LITERAL,     /^(?:(?:0(?:[0-7]+|X[0-9A-F]+))L?|(?:(?:0|[1-9][0-9]*)(?:(?:\.[0-9]+)?(?:E[+\-]?[0-9]+)?F?|L?))|\\.[0-9]+(?:E[+\-]?[0-9]+)?F?)/i],
-/*         [PR.PR_TYPE,        /^\b([A-Z][A-Za-z_0-9]*){2,}\b/, null], //not working as it should.. does not match Bag and Pan2*/
-         [PR.PR_TYPE,        /^\b([A-Z][A-Za-z_0-9]+)\b/],
-         [PR.PR_COMMENT,     /^\/(?:\/.*|\*(?:\/|\**[^*/])*(?:\*+\/?)?)/],
-         [PR.PR_PUNCTUATION, /^[-.,;!#$/+*<>=@()\[\]{}]/]
-        ]),
+    [
+        /* whitespace */
+        [PR.PR_PLAIN,       /^[\t\n\r \xA0]+/, null, '\t\n\r \xA0'],
+        /* strings */
+        [PR.PR_STRING,      /^"(?:[^\\"]|\\.)*(?:"|$)/, null, '"'],
+    ],
+    [
+        /* char literal */
+        [PR.PR_LITERAL,     /^\$(\\)?./],
+        /* symbols */
+        [PR.PR_ATTRIB_NAME, /^\\\w+/],
+        [PR.PR_ATTRIB_NAME, /^'[^']+'/],
+        /* special variables */
+        [PR.PR_TAG,         /^(?:super|thisFunctionDef|thisFunction|thisMethod|thisProcess|thisThread|this)\b/],
+        /* special values */
+        [PR.PR_KEYWORD,     /^(?:true|false|nil|inf)\b/],
+        /* variable declarations */
+        [PR.PR_DECLARATION, /^(?:var|classvar|const|arg)\b/],
+//        [PR.PR_DECLARATION, /^\|/],
+        /* class names */
+        [PR.PR_TYPE,        /^\b([A-Z][A-Za-z_0-9]+)\b/],
+        [PR.PR_COMMENT,     /^\/(?:\/.*|\*(?:\/|\**[^*/])*(?:\*+\/?)?)/], //fixme: nested comments
+        /* numbers */
+        [PR.PR_LITERAL,     /^-?\d+r[\da-zA-Z]+(\.[\da-zA-Z]+)?/],
+//        [PR.PR_LITERAL,     /^-?(?:(?:\d+(?:\.\d*)?)(?:e[+\-]?\d+)?)(pi)?|pi/],
+        [PR.PR_LITERAL,     /^-?(?:(?:\d+(\.\d+)?)(?:e[+\-]?\d+)?)(pi)?|pi/],
+        /* other stuff */
+        [PR.PR_PLAIN, /^[a-z_]\w*/i],
+//        [PR.PR_PUNCTUATION, /^[-.,;!#$%&/+*<>=@()\[\]{}]/]
+        [PR.PR_PUNCTUATION, /^[-.,;!?#$%&\|/+*<>=@()\[\]{}]/]
+    ]),
     ['sc']);
+
