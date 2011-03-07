fadeba1b1ec04043601dfdf7f1a00590624549c6
Tim Blechmann <tim@klingt.org>
2011-03-07 23:12:51 +0100

    server plugins: don't emit flatten attribute for clang
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/plugins/TriggerUGens.cpp
+++ server/plugins/TriggerUGens.cpp
@@ -28,7 +28,7 @@
 #ifdef NOVA_SIMD
 #include "simd_memory.hpp"
 
-#ifdef __GNUC__
+#ifdef __GNUC__ && !defined(__clang__)
 #define inline_functions __attribute__ ((flatten))
 #else
 #define inline_functions
