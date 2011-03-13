09aaa1c9f4fcf213272dfdc0444f2ee4ce90a5bd
Tim Blechmann <tim@klingt.org>
2011-03-13 11:14:58 +0100

    sclang: string/symbol primitives - use locale-independent sc_strtod
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- lang/LangPrimSource/PyrStringPrim.cpp
+++ lang/LangPrimSource/PyrStringPrim.cpp
@@ -75,6 +75,8 @@ int prString_AsInteger(struct VMGlobals *g, int numArgsPushed)
 	return errNone;
 }
 
+extern "C" double sc_strtod(const char *nptr, char **endptr);
+
 int prString_AsFloat(struct VMGlobals *g, int numArgsPushed);
 int prString_AsFloat(struct VMGlobals *g, int numArgsPushed)
 {
@@ -84,7 +86,7 @@ int prString_AsFloat(struct VMGlobals *g, int numArgsPushed)
 	int err = slotStrVal(a, str, 255);
 	if (err) return err;
 
-	SetFloat(a, atof(str));
+	SetFloat(a, sc_strtod(str, NULL));
 
 	return errNone;
 }
