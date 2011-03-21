a609cdd2623f453cfc5f77785ddcfd4d29c49fb6
Jonatan Liljedahl <lijon@kymatica.com>
2011-03-21 16:49:44 +0100

    Revert "standardizePath did not work correctly on OSX, use realpath() also on OSX" The OSX behaviour is the original designed behaviour, so the linux implementation should probably be changed instead.
    This reverts commit 0a1b7a77d0543cd186fb3357361b92171651a36c.

--- common/SC_DirUtils.cpp
+++ common/SC_DirUtils.cpp
@@ -172,18 +172,42 @@ bool sc_SkipDirectory(const char *name)
 
 int sc_ResolveIfAlias(const char *path, char *returnPath, bool &isAlias, int length)
 {
-#if defined(_WIN32)
 	isAlias = false;
-	strcpy(returnPath, path);
-	return 0;
-#else
+#if defined(__APPLE__) && !defined(SC_IPHONE)
+	FSRef dirRef;
+	OSStatus osStatusErr = FSPathMakeRef ((const UInt8 *) path, &dirRef, NULL);
+	if ( !osStatusErr ) {
+		Boolean isFolder;
+		Boolean wasAliased;
+		OSErr err = FSResolveAliasFile (&dirRef, true, &isFolder, &wasAliased);
+		if (err)
+		{
+			return -1;
+		}
+		isAlias = wasAliased;
+		if (wasAliased)
+		{
+			UInt8 resolvedPath[PATH_MAX];
+			osStatusErr = FSRefMakePath (&dirRef, resolvedPath, length);
+			if (osStatusErr)
+			{
+				return -1;
+			}
+			strncpy(returnPath, (char *) resolvedPath, length);
+			return 0;
+		}
+	}
+#elif defined(__linux__) || defined(__FreeBSD__)
 	isAlias = sc_IsSymlink(path);
 	if (realpath(path, returnPath))
 	{
 		return 0;
 	}
+
 	return -1;
 #endif
+	strcpy(returnPath, path);
+	return 0;
 }
 
 // Support for Bundles
