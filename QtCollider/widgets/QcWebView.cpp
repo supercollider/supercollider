40899946d515683ff1153d6e5c346375846d9d03
Jakob Leben <jakob.leben@gmail.com>
2011-03-22 02:08:02 +0100

    QtCollider: new QWebView sc class

---
+++ QtCollider/widgets/QcWebView.cpp
@@ -0,0 +1,91 @@
+/************************************************************************
+*
+* Copyright 2011 Jakob Leben (jakob.leben@gmail.com)
+*
+* This file is part of SuperCollider Qt GUI.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+************************************************************************/
+
+#include "QcWebView.h"
+#include "../QcWidgetFactory.h"
+#include <QWebPage>
+#include <QWebFrame>
+
+static QcWidgetFactory<QtCollider::WebView> factory;
+
+namespace QtCollider {
+
+WebView::WebView( QWidget *parent ) : QWebView( parent )
+{
+  /// FIXME: for now we reset the base colors in order to render the help
+  ///        files correctly on a dark background
+  QPalette pal = palette();
+  pal.setColor(QPalette::Base, QColor(Qt::white));
+  pal.setColor(QPalette::Text, QColor(Qt::black));
+  setPalette(pal);
+
+  connect( this, SIGNAL(linkClicked(QUrl)), this, SLOT(onLinkClicked(QUrl)) );
+}
+
+QString WebView::url() const
+{
+  return QWebView::url().toString();
+}
+
+void WebView::setUrl( const QString & str )
+{
+  load( urlFromString(str) );
+}
+
+QString WebView::html () const
+{
+  return page()->mainFrame()->toHtml();
+}
+
+void WebView::setHtml ( const QString &html, const QString &baseUrl )
+{
+  QUrl url( baseUrl.isEmpty() ? QUrl() : urlFromString(baseUrl) );
+  QWebView::setHtml( html, url );
+}
+
+QString WebView::plainText () const
+{
+  return page()->mainFrame()->toPlainText();
+}
+
+QWebPage::LinkDelegationPolicy WebView::linkDelegationPolicy () const
+{
+  return page()->linkDelegationPolicy();
+}
+
+void WebView::setLinkDelegationPolicy ( QWebPage::LinkDelegationPolicy p )
+{
+  page()->setLinkDelegationPolicy( p );
+}
+
+void WebView::findText( const QString &searchText, bool reversed )
+{
+  QWebPage::FindFlags flags( QWebPage::FindWrapsAroundDocument );
+  if( reversed ) flags |= QWebPage::FindBackward;
+  QWebView::findText( searchText, flags );
+}
+
+void WebView::onLinkClicked( const QUrl &url )
+{
+  Q_EMIT( linkActivated( url.toString() ) );
+}
+
+} // namespace QtCollider
---
+++ SCClassLibrary/QtCollider/QWebView.sc
@@ -0,0 +1,62 @@
+QWebView : QView {
+
+  var <onLoadAction, <onLinkActivated;
+
+  *qtClass { ^'QtCollider::WebView'; }
+
+  url { ^this.getProperty( \url ); }
+
+  url_ { arg address; this.setProperty( \url, address ); }
+
+  // Get the displayed content in html form.
+  html { ^this.getProperty( \html ); }
+
+  // Set html content.
+  html_ { arg htmlString;
+    this.invokeMethod( \setHtml, htmlString );
+  }
+
+  selectedText { ^this.getProperty( \selectedText ); }
+
+  // Try to extract plain text from html content and return it.
+  plainText { ^this.getProperty( \plainText ); }
+
+  reload { this.invokeMethod( 'reload' ); }
+
+  back { this.invokeMethod( 'back' ); }
+
+  forward { this.invokeMethod( 'forward' ); }
+
+  // The given function will be evaluated when a page has finished loading -
+  // be it successfully or not.
+
+  // The arguments passed to the function are the calling QWebView object,
+  // and a boolean determining whether loading was successful.
+
+  onLoad_ { arg func;
+    this.manageFunctionConnection( onLoadAction, func, 'loadFinished(bool)' );
+    onLoadAction = func;
+  }
+
+  // After this method is called with a function as an argument, QWebView will not
+  // handle links in any way. Instead, the given function will be evaluated whenever
+  // the user activates (clicks) a link.
+
+  // The argument passed to the function is the calling QWebView object.
+
+  // If this method is called with nil argument, QWebView link handling will be
+  // restored again.
+
+  onLinkActivated_ { arg func;
+    this.manageFunctionConnection( onLinkActivated, func, 'linkActivated(QString)' );
+    if (func.notNil)
+      { this.setProperty( \linkDelegationPolicy, 2 ); }
+      { this.setProperty( \linkDelegationPolicy, 0 ); };
+    onLinkActivated = func;
+  }
+}
+
+/*
+perhaps also:
+- history  // returning a string list of urls.
+*/
