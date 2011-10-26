364f0ac053047bacb8022558ce475f7dd5d12718
tkatchev <tkatchev@gmail.com>
2011-10-26 13:47:39 +0400

    Fixes for the build system, to allow building scsynth/sclang in a bare-bones, minimal configuration.
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/scsynth/SC_World.cpp
+++ server/scsynth/SC_World.cpp
@@ -67,8 +67,14 @@ extern HashTable<struct BufGen, Malloc> *gBufGenLib;
 extern HashTable<PlugInCmd, Malloc> *gPlugInCmds;
 
 extern "C" {
+
+#ifdef NO_LIBSNDFILE
+struct SF_INFO {};
+#endif
+
 int sndfileFormatInfoFromStrings(struct SF_INFO *info,
 	const char *headerFormatString, const char *sampleFormatString);
+
 bool SendMsgToEngine(World *inWorld, FifoMsg& inMsg);
 bool SendMsgFromEngine(World *inWorld, FifoMsg& inMsg);
 }
@@ -1160,7 +1166,14 @@ int sndfileFormatInfoFromStrings(struct SF_INFO *info, const char *headerFormatS
 	info->format = (unsigned int)(headerFormat | sampleFormat);
 	return kSCErr_None;
 }
-#endif
+
+#else // NO_LIBSNDFILE
+
+int sndfileFormatInfoFromStrings(struct SF_INFO *info, const char *headerFormatString, const char *sampleFormatString) {
+	return kSCErr_Failed;
+}
+
+#endif // NO_LIBSNDFILE
 
 #include "scsynthsend.h"
 
