11a0c737a681091da913b1ad18d6247c73230716
Tim Blechmann <tim@klingt.org>
2011-11-22 22:58:24 +0100

    plugins: Dswitch - cleanup
    minor refactoring to make the function a bit more readable
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/plugins/DemandUGens.cpp
+++ server/plugins/DemandUGens.cpp
@@ -1974,26 +1974,23 @@ void Dswitch1_Ctor(Dswitch1 *unit)
 
 void Dswitch_next(Dswitch *unit, int inNumSamples)
 {
-	int index;
-	float ival;
 	if (inNumSamples) {
 		float val = DEMANDINPUT_A(unit->m_index, inNumSamples);
 		//printf("index: %i\n", (int) val);
 		if(sc_isnan(val)) {
-			ival = DEMANDINPUT_A(0, inNumSamples);
+			float ival = DEMANDINPUT_A(0, inNumSamples);
 
-			if(sc_isnan(ival)) {
-				OUT0(0) = ival;
-				return;
+			if(sc_isnan(ival))
+				val = ival;
+			else {
+				int index = (int32)floor(ival + 0.5f);
+				index = sc_wrap(index, 0, unit->mNumInputs - 2) + 1;
+				val = DEMANDINPUT_A(index, inNumSamples);
+
+				RESETINPUT(unit->m_index);
+				// printf("resetting index: %i\n", unit->m_index);
+				unit->m_index = index;
 			}
-
-			index = (int32)floor(ival + 0.5f);
-			index = sc_wrap(index, 0, unit->mNumInputs - 2) + 1;
-			val = DEMANDINPUT_A(index, inNumSamples);
-
-			RESETINPUT(unit->m_index);
-			// printf("resetting index: %i\n", unit->m_index);
-			unit->m_index = index;
 		}
 		OUT0(0) = val;
 
@@ -2002,7 +1999,7 @@ void Dswitch_next(Dswitch *unit, int inNumSamples)
 		for (int i=0; i<unit->mNumInputs; ++i) {
 			RESETINPUT(i);
 		}
-		index = (int32)floor(DEMANDINPUT(0) + 0.5f);
+		int index = (int32)floor(DEMANDINPUT(0) + 0.5f);
 		index = sc_wrap(index, 0, unit->mNumInputs - 1) + 1;
 		unit->m_index = index;
 	}
