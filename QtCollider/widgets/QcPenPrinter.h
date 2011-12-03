86c6fb6a0cbe093b78c476d59d3d192561ba4e97
Jonatan Liljedahl <lijon@kymatica.com>
2011-12-03 18:22:34 +0100

    Add QPen printing support (and PDF export) with new QPenPrinter class.
    QPenPrinter-showDialog(okFunc,cancelFunc) shows a print dialog to configure the printer
    and calls okFunc when user presses "print".
    
    QPenPrinter-print(drawFunc) calls drawFunc where QPen operates on the configured printer.
    All the normal Pen methods can be used inside drawFunc.

---
+++ QtCollider/widgets/QcPenPrinter.h
@@ -0,0 +1,97 @@
+/************************************************************************
+*
+* Copyright 2011 Jonatan Liljedahl <lijon@kymatica.com>
+*
+* This file is part of SuperCollider Qt GUI.
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+************************************************************************/
+
+#ifndef QC_PEN_PRINTER_H
+#define QC_PEN_PRINTER_H
+
+#include "../painting.h"
+#include <QPrintDialog>
+#include <QPrinter>
+#include <QPainter>
+
+#include <stdio.h>
+
+class QcPenPrinter : public QObject
+{
+  Q_OBJECT
+  Q_PROPERTY( QRect pageRect READ pageRect );
+  Q_PROPERTY( QRect paperRect READ paperRect );
+  Q_PROPERTY( int fromPage READ fromPage );
+  Q_PROPERTY( int toPage READ toPage );
+
+public:
+
+  QcPenPrinter() {
+  }
+
+  ~QcPenPrinter() {
+  }
+
+  QRect pageRect() const { return printer.pageRect(); }
+  QRect paperRect() const { return printer.paperRect(); }
+  int fromPage() const { return printer.fromPage(); }
+  int toPage() const { return printer.toPage(); }
+
+Q_SIGNALS:
+
+  void ok();
+  void cancel();
+  void printFunc();
+
+private Q_SLOTS:
+
+  void show() {
+    QPrintDialog *dialog = new QPrintDialog(&printer);
+
+    dialog->setWindowTitle( QString("Print Document") );
+    dialog->setOptions (
+      QAbstractPrintDialog::PrintToFile |
+      QAbstractPrintDialog::PrintPageRange |
+      QAbstractPrintDialog::PrintShowPageSize
+    );
+    if (dialog->exec() != QDialog::Accepted) {
+      Q_EMIT( cancel() );
+    } else {
+      Q_EMIT( ok() );
+    }
+    delete dialog;
+  }
+
+  void print() {
+    QPainter painter;
+    painter.begin(&printer);
+    QtCollider::beginPainting(&painter);
+    Q_EMIT ( printFunc() );
+    painter.end();
+    QtCollider::endPainting();
+  }
+
+  void newPage() {
+    printer.newPage();
+  }
+
+private:
+
+  QPrinter printer;
+
+};
+
+#endif // QC_PEN_PRINTER_H
---
+++ SCClassLibrary/QtCollider/QPenPrinter.sc
@@ -0,0 +1,45 @@
+QPenPrinter : QObject {
+  var printFunc, cancelFunc, okFunc;
+
+  *qtClass { ^'QcPenPrinter' }
+
+  *new {
+    ^super.new.init;
+  }
+
+  init {
+    this.connectFunction('printFunc()', synchronous:true) {
+      printFunc.value(this);
+      printFunc = nil;
+    };
+    this.connectFunction('ok()', synchronous:false) {
+      okFunc.value(this);
+      okFunc = nil;
+    };
+    this.connectFunction('cancel()', synchronous:false) {
+      cancelFunc.value(this);
+      cancelFunc = nil;
+    };
+  }
+
+  showDialog { |aOkFunc, aCancelFunc|
+    okFunc = aOkFunc;
+    cancelFunc = aCancelFunc;
+    this.invokeMethod(\show, synchronous:false);
+  }
+
+  print { |aPrintFunc|
+    printFunc = aPrintFunc;
+    this.invokeMethod(\print, synchronous:false);
+  }
+
+  newPage {
+    this.invokeMethod(\newPage, synchronous:true);
+  }
+
+  pageRect { ^this.getProperty(\pageRect) }
+  paperRect { ^this.getProperty(\paperRect) }
+  fromPage { ^this.getProperty(\fromPage) }
+  toPage { ^this.getProperty(\toPage) }
+  pageSize { ^this.pageRect.size }
+}
