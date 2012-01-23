f841ab26c57deda7f9185133a17713d625949f44
Tim Blechmann <tim@klingt.org>
2012-01-23 15:05:10 +0100

    Revert "Serial: avoid deadlock in destructor"
    it merely works around the bug but does not fix it.
    
    This reverts commit 66635893a6256954edb0541d9f8d8092a23c1510.
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- lang/LangPrimSource/PyrSerialPrim.cpp
+++ lang/LangPrimSource/PyrSerialPrim.cpp
@@ -349,7 +349,6 @@ SerialPort::~SerialPort()
 {
 	m_running = false;
 //	m_open = false;
-	pthread_mutex_unlock (&gLangMutex);
 	if ( m_open ){
 		tcflush(m_fd, TCIOFLUSH);
 		tcsetattr(m_fd, TCSANOW, &m_oldtermio);
@@ -357,7 +356,6 @@ SerialPort::~SerialPort()
 		m_open = false;
 	};
 	pthread_join(m_thread, 0);
-	pthread_mutex_lock (&gLangMutex);
 }
 
 void SerialPort::stop(){
@@ -406,10 +404,6 @@ void* SerialPort::threadFunc(void* self)
 void SerialPort::dataAvailable()
 {
 	pthread_mutex_lock (&gLangMutex);
-	if (!m_running) {
-		pthread_mutex_unlock (&gLangMutex);
-		return;
-	}
 	PyrSymbol *method = s_dataAvailable;
 	if (m_obj) {
 		VMGlobals *g = gMainVMGlobals;
