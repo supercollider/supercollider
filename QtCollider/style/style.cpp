a8fb8c3b9b8df68614e55a3efbdca3dcf16b5515
Jakob Leben <jakob.leben@gmail.com>
2012-01-29 20:59:49 +0100

    qtcollider: QButton styling and some general improvements
    Improve the 'drawRaised' routine that draws buttons, knobs, handles, etc.
    and extend it with optional focus indication

--- QtCollider/style/style.cpp
+++ QtCollider/style/style.cpp
@@ -1,4 +1,5 @@
 #include "style.hpp"
+#include "routines.hpp"
 
 #include "../widgets/QcSlider.h"
 
@@ -11,6 +12,38 @@ void StyleImpl::polish ( QPalette & p )
 
   _colors[Focus] = p.color(QPalette::Highlight);
   _colors[Groove] = p.color(QPalette::Window).darker(120);
+}
+
+void StyleImpl::drawControl ( ControlElement elem, const QStyleOption * option,
+                          QPainter * p, const QWidget * w ) const
+{
+  if( elem == CE_PushButton ) {
+
+    p->save();
+    p->setRenderHint( QPainter::Antialiasing, true );
+
+    const QStyleOptionButton *opt = static_cast<const QStyleOptionButton*>(option);
+    const QPalette &plt = opt->palette;
+    bool sunken = opt->state & State_Sunken || opt->state & State_On;
+    int radius = 2;
+
+    QColor color( sunken ? plt.color(QPalette::Button).darker(120) : plt.color(QPalette::Button) );
+    QColor focusColor( opt->state & State_HasFocus ? _colors[Focus] : QColor() );
+
+    RoundRect frame( opt->rect, radius );
+    if( sunken )
+      drawSunken( p, plt, frame, color, focusColor);
+    else
+      drawRaised( p, plt, frame, color, focusColor );
+
+    if( !opt->text.isEmpty() )
+      drawItemText( p, opt->rect, Qt::AlignCenter, plt, opt->state & State_Enabled, opt->text, QPalette::ButtonText );
+
+    p->restore();
+  }
+  else {
+    ProxyStyle::drawControl( elem, option, p, w );
+  }
 }
 
 void StyleImpl::drawComplexControl ( QStyle::ComplexControl cc, const QStyleOptionComplex * option,
