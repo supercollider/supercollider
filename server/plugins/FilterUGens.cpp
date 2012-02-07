5a285e6ea5e172adf3ff5da0adb2eeec15910588
Tim Blechmann <tim@klingt.org>
2012-02-07 23:58:14 +0100

    plugins: MoogFF - use precomputed sample duration
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/plugins/FilterUGens.cpp
+++ server/plugins/FilterUGens.cpp
@@ -305,7 +305,6 @@ struct MoogFF : public Unit
 	float m_freq, m_b0, m_a1; // Resonant freq and corresponding vals; stored because we need to compare against prev vals
 	double m_wcD;
 
-	double m_T; // 1/SAMPLEFREQ
 	float m_s1, m_s2, m_s3, m_s4; // 1st order filter states
 };
 
@@ -4799,7 +4798,6 @@ void MoogFF_Ctor(MoogFF* unit)
 
 	// initialize the unit generator state variables.
 	unit->m_freq = -10000.3f; // Force the freq to update on first run
-	unit->m_T = 1.0 / SAMPLERATE;
 	unit->m_s1 = 0.f;
 	unit->m_s2 = 0.f;
 	unit->m_s3 = 0.f;
@@ -4823,13 +4821,11 @@ void MoogFF_next(MoogFF *unit, int inNumSamples)
 	float s3 = unit->m_s3;
 	float s4 = unit->m_s4;
 	float freq = unit->m_freq;///
-	double T = unit->m_T;
 
 	// Reset filter state if requested
 	if(IN0(3)>0)
 		s1 = s2 = s3 = s4 = 0.f;
 
-	double wcD=unit->m_wcD;
 	float a1 = unit->m_a1, b0 = unit->m_b0; // Filter coefficient parameters
 	float o, u; // System's null response, loop input
 
@@ -4837,7 +4833,9 @@ void MoogFF_next(MoogFF *unit, int inNumSamples)
 	if(freq != IN0(1)) {
 		freq = IN0(1);
 		//Print("Updated freq to %g\n", freq);
-		wcD = 2.0 * tan(T * PI * freq) * SAMPLERATE;
+		double wcD=unit->m_wcD;
+		double T = SAMPLEDUR;
+		wcD = 2.0 * tan ( T * PI * freq ) * SAMPLERATE;
 		if(wcD<0)
 			wcD = 0; // Protect against negative cutoff freq
 		double TwcD = T*wcD;
