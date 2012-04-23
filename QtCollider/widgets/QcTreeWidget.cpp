e6ebf06760f857a27985332edce53b4b3247add3
Jakob Leben <jakob.leben@gmail.com>
2012-04-23 23:44:23 +0200

    qtcollider: improve key propagation in QListView and QTreeView
    Override Qt's default behavior so as to always accept arrow keys,
    Page Up, Page Down, Home, and End keys, thus preventing them from
    propagating to parent when the scroller meets top or bottom.

--- QtCollider/widgets/QcTreeWidget.cpp
+++ QtCollider/widgets/QcTreeWidget.cpp
@@ -24,6 +24,8 @@
 
 #include <PyrKernel.h>
 
+#include <QKeyEvent>
+
 class QcTreeWidgetFactory : public QcWidgetFactory<QcTreeWidget>
 {
   void initialize( QWidgetProxy *p, QcTreeWidget *w ) {
@@ -194,6 +196,28 @@ void QcTreeWidget::sort( int column, bool descending )
 {
   sortItems( column, descending ? Qt::DescendingOrder : Qt::AscendingOrder );
 }
+
+void QcTreeWidget::keyPressEvent( QKeyEvent *e )
+{
+  QTreeWidget::keyPressEvent( e );
+
+  switch (e->key())
+  {
+  case Qt::Key_Up:
+  case Qt::Key_Down:
+  case Qt::Key_Left:
+  case Qt::Key_Right:
+  case Qt::Key_PageUp:
+  case Qt::Key_PageDown:
+  case Qt::Key_Home:
+  case Qt::Key_End:
+    // Prevent propagating to parent when scroller reaches minimum or maximum:
+    e->accept();
+  default: break;
+  }
+}
+
+//////////////////////////////// Item //////////////////////////////////////
 
 QcTreeWidget::ItemPtr QcTreeWidget::Item::safePtr( QTreeWidgetItem * item )
 {
