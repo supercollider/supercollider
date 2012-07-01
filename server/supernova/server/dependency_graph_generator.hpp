1688870c59dfe91d66037baf32616e9635641419
Tim Blechmann <tim@klingt.org>
2012-07-01 12:43:06 +0200

    supernova: don't wake helper threads, unless we have parallel groups
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/supernova/server/dependency_graph_generator.hpp
+++ server/supernova/server/dependency_graph_generator.hpp
@@ -27,8 +27,7 @@
 #define MOVE(X) X
 #endif
 
-namespace nova
-{
+namespace nova {
 
 class dependency_graph_generator
 {
@@ -39,10 +38,12 @@ class dependency_graph_generator
 public:
     dsp_thread_queue * operator()(node_graph * graph)
     {
-        /* pessimize: reserve enough memory for the worst case */
-        q = new node_graph::dsp_thread_queue(graph->synth_count_);
+        bool has_parallelism = graph->root_group()->has_parallel_group_children();
 
-        fill_queue(*graph->root_group());
+        /* pessimize: reserve enough memory for the worst case */
+        q = new node_graph::dsp_thread_queue(graph->synth_count_, has_parallelism);
+
+        fill_queue(*graph->root_group()); // LATER: we could optimize the case when we do not have any paralleism
         return q;
     }
 
@@ -52,9 +53,7 @@ private:
     void fill_queue(group & root_group)
     {
         if (root_group.has_synth_children())
-        {
             fill_queue_recursive(root_group, successor_container(0), 0);
-        }
     }
 
     template <typename reverse_iterator>
@@ -62,8 +61,7 @@ private:
     {
         reverse_iterator prev = it;
 
-        for(;;)
-        {
+        for(;;) {
             ++prev;
             if (prev == end)
                 return previous_activation_limit; // we are the first item, so we use the previous activiation limit
@@ -104,9 +102,7 @@ private:
         sequential_child_list sequential_children;
         sequential_children.reserve(g.child_synth_count);
 
-        for (r_iterator it = g.child_nodes.rbegin();
-            it != g.child_nodes.rend(); ++it)
-        {
+        for (r_iterator it = g.child_nodes.rbegin(); it != g.child_nodes.rend(); ++it) {
             server_node & node = *it;
 
             if (node.is_synth()) {
@@ -154,8 +150,7 @@ private:
                 if (activation_limit == 0)
                     q->add_initially_runnable(q_item);
                 children -= node_count;
-            }
-            else {
+            } else {
                 abstract_group & grp = static_cast<abstract_group&>(node);
 
                 if (grp.has_synth_children())
@@ -177,9 +172,7 @@ private:
         std::vector<thread_queue_item*, rt_pool_allocator<void*> > collected_nodes;
         collected_nodes.reserve(g.child_synth_count + g.child_group_count * 16); // pessimize
 
-        for (server_node_list::iterator it = g.child_nodes.begin();
-            it != g.child_nodes.end(); ++it)
-        {
+        for (server_node_list::iterator it = g.child_nodes.begin(); it != g.child_nodes.end(); ++it) {
             server_node & node = *it;
 
             if (node.is_synth()) {
@@ -190,12 +183,10 @@ private:
                     q->add_initially_runnable(q_item);
 
                 collected_nodes.push_back(q_item);
-            }
-            else {
+            } else {
                 abstract_group & grp = static_cast<abstract_group&>(node);
 
-                if (grp.has_synth_children())
-                {
+                if (grp.has_synth_children()) {
                     successor_container group_successors = fill_queue_recursive(grp, successors_from_parent,
                                                                                 previous_activation_limit);
 
