40cf1301301123114971154d843d5e3a9bfe5fc2
Tim Blechmann <tim@klingt.org>
2012-07-02 16:31:51 +0200

    external libraries: fix extract_boost script and add missing boost library
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- external_libraries/extract_boost.sh
+++ external_libraries/extract_boost.sh
@@ -12,6 +12,7 @@ bcp --boost=${BOOST_ROOT} \
 	--scan lang/*/*pp \
 	--scan server/supernova/*/*pp \
 	--scan editors/sc-ide/*/*pp \
+	--scan editors/sc-ide/*/*/*pp \
 	--scan external_libraries/boost_*/*/*pp \
 	--scan external_libraries/boost_*/*/*/*pp \
 	--scan external_libraries/boost_*/*/*/*/*pp \
