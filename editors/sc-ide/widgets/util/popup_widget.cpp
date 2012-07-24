f80b56fd4d121d79ad926197ff099c95fa705e76
Jakob Leben <jakob.leben@gmail.com>
2012-07-24 19:09:29 +0200

    sc ide: introduce PopUpWidget and use it for opening method/class definitions

---
+++ editors/sc-ide/widgets/util/popup_widget.cpp
@@ -0,0 +1,81 @@
+/*
+    SuperCollider Qt IDE
+    Copyright (c) 2012 Jakob Leben & Tim Blechmann
+    http://www.audiosynth.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include "popup_widget.hpp"
+
+#include <QKeyEvent>
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QDebug>
+
+namespace ScIDE {
+
+int PopUpWidget::exec( const QPoint & pos )
+{
+    if (mEventLoop) {
+        qWarning("ScIDE::PopUpWidget: recursive exec() - suppressed!");
+        return Rejected;
+    }
+
+    QEventLoop eventLoop;
+    mEventLoop = &eventLoop;
+
+    move(pos);
+    show();
+
+    QPointer<QObject> self(this);
+    mEventLoop->exec();
+    if (self.isNull())
+        return Rejected;
+
+    return mResult;
+}
+
+void PopUpWidget::keyPressEvent( QKeyEvent *ke )
+{
+    switch(ke->key())
+    {
+    case Qt::Key_Escape:
+        reject();
+        break;
+    default:;
+    }
+}
+
+void PopUpWidget::showEvent( QShowEvent *e )
+{
+    QRect r = geometry();
+    QRect screen = QApplication::desktop()->availableGeometry(this);
+    if (!screen.contains(r))
+    {
+        if (r.right() > screen.right())
+            r.moveRight( screen.right() );
+        if (r.bottom() > screen.bottom())
+            r.moveBottom( screen.bottom() );
+        if (r.left() < screen.left())
+            r.moveLeft( screen.left() );
+        if (r.top() < screen.top())
+            r.moveTop(screen.top());
+    }
+
+    move(r.topLeft());
+}
+
+} // namespace ScIDE
---
+++ editors/sc-ide/widgets/util/popup_widget.hpp
@@ -0,0 +1,100 @@
+/*
+    SuperCollider Qt IDE
+    Copyright (c) 2012 Jakob Leben & Tim Blechmann
+    http://www.audiosynth.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#ifndef SCIDE_WIDGETS_UTIL_POPUP_WIDGET_HPP_INCLUDED
+#define SCIDE_WIDGETS_UTIL_POPUP_WIDGET_HPP_INCLUDED
+
+#include <QWidget>
+#include <QEventLoop>
+#include <QPointer>
+
+namespace ScIDE {
+
+class PopUpWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    enum Result {
+        Rejected = 0,
+        Accepted
+    };
+
+public:
+    PopUpWidget( QWidget * parent = 0 ):
+        QWidget( parent, Qt::Popup ),
+        mEventLoop(0),
+        mResult(0)
+    {}
+
+    ~PopUpWidget()
+    {
+        quit();
+    }
+
+    int exec( const QPoint & pos );
+
+public slots:
+
+    void accept()
+    {
+        done(Accepted);
+    }
+
+    void reject()
+    {
+        done(Rejected);
+    }
+
+    void done(int result)
+    {
+        mResult = result;
+        hide();
+        quit();
+    }
+
+private:
+    void quit()
+    {
+        if (mEventLoop) {
+            mEventLoop->exit();
+            mEventLoop = 0;
+        }
+    }
+
+protected:
+    virtual void hideEvent( QHideEvent *e )
+    {
+        quit();
+    }
+
+    virtual void keyPressEvent( QKeyEvent *ke );
+
+    virtual void showEvent( QShowEvent * );
+
+private:
+
+    QEventLoop *mEventLoop;
+    int mResult;
+};
+
+} // namespace ScIDE
+
+#endif
