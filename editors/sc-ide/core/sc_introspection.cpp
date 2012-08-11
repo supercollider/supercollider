4d69e8deb88fb3f2a2f17066d1c7ef6e91943b38
Jakob Leben <jakob.leben@gmail.com>
2012-08-11 15:04:57 +0200

    sc ide: cache all sc language introspection
    The class wraps introspection data, and offers a std::map<class name, class>
    and a std::multi_map<method name, method> for fast lookup of classes
    and methods.

---
+++ editors/sc-ide/core/sc_introspection.cpp
@@ -0,0 +1,160 @@
+/*
+    SuperCollider Qt IDE
+    Copyright (c) 2012 Jakob Leben & Tim Blechmann
+    http://www.audiosynth.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#include "sc_introspection.hpp"
+
+#include "yaml-cpp/node.h"
+#include "yaml-cpp/parser.h"
+
+#include <cassert>
+
+#include <QDebug>
+#include <QHash>
+
+namespace ScIDE {
+namespace ScLanguage {
+
+
+bool Introspection::parse(const QString & yamlString )
+{
+    using std::pair;
+
+    for (ClassMap::iterator it = mClassMap.begin(); it != mClassMap.end(); ++it)
+        delete it->second;
+
+    for (MethodMap::iterator it = mMethodMap.begin(); it != mMethodMap.end(); ++it)
+        delete it->second;
+
+    mClassMap.clear();
+    mMethodMap.clear();
+
+    //qDebug("parsing introspection...");
+
+    std::stringstream stream;
+    stream << yamlString.toStdString();
+    YAML::Parser parser(stream);
+
+    YAML::Node doc;
+    if(!parser.GetNextDocument(doc)) {
+        qWarning("no YAML document");
+        return false;
+    }
+
+    assert (doc.Type() == YAML::NodeType::Sequence);
+    for (YAML::Iterator it = doc.begin(); it != doc.end(); ++it)
+    {
+        assert(it->Type() == YAML::NodeType::Sequence);
+        QString name = (*it)[0].to<std::string>().c_str();
+        Class *klass = new Class;
+        klass->name = name;
+        mClassMap.insert(pair<QString, Class*>(klass->name, klass));
+    }
+
+    for (YAML::Iterator it = doc.begin(); it != doc.end(); ++it)
+    {
+        const YAML::Node & node = *it;
+        QString name = node[0].to<std::string>().c_str();
+        ClassMap::iterator it = mClassMap.find(name);
+        assert(it != mClassMap.end());
+        Class *klass = it->second;
+
+        //qDebug() << klass->name;
+
+        ClassMap::iterator class_it;
+
+        QString metaClassName = node[1].to<std::string>().c_str();
+        class_it = mClassMap.find(metaClassName);
+        assert(class_it != mClassMap.end());
+        klass->metaClass = class_it->second;
+
+        if (node[2].Read(YAML::Null))
+            klass->superClass = 0;
+        else {
+            QString superClassName = node[2].to<std::string>().c_str();
+            class_it = mClassMap.find(superClassName);
+            assert(class_it != mClassMap.end());
+            klass->superClass = class_it->second;
+        }
+
+        klass->definition.path = node[3].to<std::string>().c_str();
+        klass->definition.position = node[4].to<int>();
+
+        const YAML::Node &methodSeq = node[5];
+        if (methodSeq.Type() != YAML::NodeType::Sequence)
+            continue;
+
+        //assert(methodSeq.Type() == YAML::NodeType::Sequence);
+        for (YAML::Iterator mit = methodSeq.begin(); mit != methodSeq.end(); ++mit)
+        {
+            const YAML::Node &methodNode = *mit;
+            assert(methodNode.Type() == YAML::NodeType::Sequence);
+            assert(methodNode.size() >= 2);
+
+            assert(methodNode[0].Type() == YAML::NodeType::Scalar);
+            assert(methodNode[1].Type() == YAML::NodeType::Scalar);
+
+            Method *method = new Method;
+            method->ownerClass = klass;
+            method->name = methodNode[1].to<std::string>().c_str();
+            method->definition.path = methodNode[2].to<std::string>().c_str();
+            method->definition.position = methodNode[3].to<int>();
+
+            //qDebug() << "--" << method->name;
+
+            const YAML::Node &argNode = methodNode[4];
+            assert(argNode.Type() == YAML::NodeType::Sequence);
+            YAML::Iterator arg = argNode.begin();
+            while (arg != argNode.end())
+            {
+                Argument argument;
+
+                // get arg name
+                assert(arg->Type() == YAML::NodeType::Scalar);
+                argument.name = arg->to<std::string>().c_str();
+
+                //qDebug() << "---# " << argument.name;
+
+                // get arg default value
+                ++arg;
+                if (arg == node.end())
+                    break;
+                if(!arg->Read(YAML::Null)) {
+                    assert(arg->Type() == YAML::NodeType::Scalar);
+                    argument.defaultValue = arg->to<std::string>().c_str();
+                }
+
+                method->arguments.append(argument);
+
+                // next arg
+                ++arg;
+            }
+
+            klass->methods.append(method);
+            mMethodMap.insert(pair<QString, Method*>(method->name, method));
+        }
+    }
+
+    //qDebug("done parsing introspection.");
+    return true;
+}
+
+
+} // namespace ScLanguage
+} // namespace ScIDE
---
+++ editors/sc-ide/core/sc_introspection.hpp
@@ -0,0 +1,85 @@
+/*
+    SuperCollider Qt IDE
+    Copyright (c) 2012 Jakob Leben & Tim Blechmann
+    http://www.audiosynth.com
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+*/
+
+#ifndef SCIDE_CORE_SC_INTROSPECTION_HPP_INCLUDED
+#define SCIDE_CORE_SC_INTROSPECTION_HPP_INCLUDED
+
+#include <QString>
+#include <QVector>
+
+#include <map>
+
+namespace ScIDE {
+
+namespace ScLanguage {
+
+struct Class;
+struct Method;
+struct Argument;
+
+typedef std::map<QString, Class*> ClassMap;
+typedef std::multimap<QString, Method*> MethodMap;
+typedef QVector<Argument> ArgumentVector;
+typedef QVector<Method*> MethodVector;
+
+struct Argument {
+    QString name;
+    QString defaultValue;
+};
+
+struct Class {
+    QString name;
+    Class *metaClass;
+    Class *superClass;
+    MethodVector methods;
+    struct {
+        QString path;
+        int position;
+    } definition;
+};
+
+struct Method {
+    Class *ownerClass;
+    QString name;
+    ArgumentVector arguments;
+    struct {
+        QString path;
+        int position;
+    } definition;
+};
+
+class Introspection
+{
+public:
+    bool parse(const QString & yamlString );
+
+    const ClassMap & classMap() const { return mClassMap; }
+    const MethodMap & methodMap() const { return mMethodMap; }
+
+private:
+    ClassMap mClassMap;
+    MethodMap mMethodMap;
+};
+
+} // namespace ScLanguage
+
+} // namespace ScIDE
+
+#endif // SCIDE_CORE_SC_INTROSPECTION_HPP_INCLUDED
