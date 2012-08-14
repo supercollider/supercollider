5e9f8473e47232b8a1147617afdefb3f33f549a3
Tim Blechmann <tim@klingt.org>
2012-08-14 18:27:42 +0200

    cmake: osx - generate bundle with scide
    when SC_IDE is enabled, the app bundle will default to scide. otherwise
    it will generate a bundle with the cocoa application named
    SuperColliderCocoa.
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- cmake_modules/MacAppFolder.cmake
+++ cmake_modules/MacAppFolder.cmake
@@ -1,15 +1,19 @@
 if(APPLE)
 
 	###### The user may set the app name, causing a standalone build
-	######  cmake -D standalone:string=MyFabApp ../
+	######	cmake -D standalone:string=MyFabApp ../
 	if ("${standalone}" STREQUAL "")
-		set(scappbundlename ${PROJECT_NAME})
+		if (SC_IDE)
+			set(scappbundlename ${PROJECT_NAME})
+		else()
+			set(scappbundlename ${PROJECT_NAME}Cocoa)
+		endif()
 	else()
 		# We're building a standalone, change the app name.
 		set(scappbundlename ${standalone})
 		message(STATUS "Building sc in STANDALONE mode. App name: " ${standalone})
 	endif()
-	
+
 	set(scappauxresourcesdir "${scappbundlename}/${scappbundlename}.app/Contents/Resources")
 
 	###### Allow user to select a FHS-style install
@@ -17,5 +21,5 @@ if(APPLE)
 	option(INSTALL_FHS
 		"use FHS-style install (e.g. to /usr/local) rather than to a mac-style app folder"
 		FALSE)
-	
+
 endif(APPLE)
