5737ea307971cfc58ea8fd195d1afea6ad531894
Dan Stowell <danstowell@users.sourceforge.net>
2012-08-16 20:41:20 +0100

    Accelerator keys for Quit dialogue

--- editors/sc-ide/widgets/documents_dialog.cpp
+++ editors/sc-ide/widgets/documents_dialog.cpp
@@ -89,40 +89,40 @@ void DocumentsDialog::init( Mode mode, const QList<Document*> &docs )
     QPushButton *btn;
     QPushButton *defaultBtn;
     if (mode == ExternalChange) {
-        defaultBtn = btn = dialogBtnBox->addButton(tr("Reload"), QDialogButtonBox::ActionRole);
+        defaultBtn = btn = dialogBtnBox->addButton(tr("&Reload"), QDialogButtonBox::ActionRole);
         btn->setIcon( QIcon::fromTheme("view-refresh") );
         connect(btn, SIGNAL(clicked()), this, SLOT(reloadSelected()));
 
-        btn = dialogBtnBox->addButton(tr("Overwrite"), QDialogButtonBox::ActionRole);
+        btn = dialogBtnBox->addButton(tr("Over&write"), QDialogButtonBox::ActionRole);
         btn->setIcon( QIcon::fromTheme("document-save") );
         connect(btn, SIGNAL(clicked()), this, SLOT(saveSelected()));
 
-        btn = dialogBtnBox->addButton(tr("Ignore"), QDialogButtonBox::AcceptRole);
+        btn = dialogBtnBox->addButton(tr("&Ignore"), QDialogButtonBox::AcceptRole);
         btn->setIcon( QIcon::fromTheme("window-close") );
         connect(btn, SIGNAL(clicked()), this, SLOT(ignoreSelected()));
 
-        btn = dialogBtnBox->addButton(tr("Close"), QDialogButtonBox::AcceptRole);
+        btn = dialogBtnBox->addButton(tr("&Close"), QDialogButtonBox::AcceptRole);
         btn->setIcon( QIcon::fromTheme("window-close") );
         connect(btn, SIGNAL(clicked()), this, SLOT(closeSelected()));
     }
     else {
-        btn = dialogBtnBox->addButton(tr("Ignore"), QDialogButtonBox::ActionRole);
+        btn = dialogBtnBox->addButton(tr("&Ignore"), QDialogButtonBox::ActionRole);
         btn->setIcon( QIcon::fromTheme("window-close") );
         connect(btn, SIGNAL(clicked()), this, SLOT(ignoreSelected()));
 
-        btn = dialogBtnBox->addButton(tr("Save"), QDialogButtonBox::ActionRole);
+        btn = dialogBtnBox->addButton(tr("&Save"), QDialogButtonBox::ActionRole);
         btn->setIcon( QIcon::fromTheme("document-save") );
         connect(btn, SIGNAL(clicked()), this, SLOT(saveSelected()));
 
-        defaultBtn = btn = dialogBtnBox->addButton(tr("Do Not Quit"), QDialogButtonBox::RejectRole);
+        defaultBtn = btn = dialogBtnBox->addButton(tr("Do &Not Quit"), QDialogButtonBox::RejectRole);
         btn->setIcon( QIcon::fromTheme("window-close") );
         connect(btn, SIGNAL(clicked()), this, SLOT(reject()));
     }
 
-    QPushButton *selectAllBtn = new QPushButton(tr("Select All"));
+    QPushButton *selectAllBtn = new QPushButton(tr("Select &All"));
     connect(selectAllBtn, SIGNAL(clicked()), this, SLOT(selectAll()));
 
-    QPushButton *selectNoneBtn = new QPushButton(tr("Select None"));
+    QPushButton *selectNoneBtn = new QPushButton(tr("Select N&one"));
     connect(selectNoneBtn, SIGNAL(clicked()), this, SLOT(selectNone()));
 
     QLabel *iconLabel = new QLabel;
