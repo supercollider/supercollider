8761f37c6efdc4045b7ca66e27f0e56402d7cede
Tim Blechmann <tim@klingt.org>
2012-10-28 14:39:42 +0100

    scide: ensure visibility of help browser
    fixes #631
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>
    (cherry picked from commit 1b7a91a885186468e722f78d157a3fc6eb8d7c10)

--- editors/sc-ide/widgets/main_window.cpp
+++ editors/sc-ide/widgets/main_window.cpp
@@ -1302,6 +1302,7 @@ void MainWindow::openHelp()
 {
     mHelpBrowserDockable->browser()->goHome();
     mHelpBrowserDockable->show();
+    mHelpBrowserDockable->raise();
 }
 
 void MainWindow::dragEnterEvent( QDragEnterEvent * event )
