e31a6f14528a6bd649e3b16ed30847140f42071d
Tim Blechmann <tim@klingt.org>
2012-11-12 12:10:55 +0100

    supernova: aligned allocator - c++11 fix
    compile fix for gcc-4.8
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/supernova/utilities/malloc_aligned.hpp
+++ server/supernova/utilities/malloc_aligned.hpp
@@ -22,7 +22,8 @@
 #include <cstdlib>
 #include <cstring>
 
-#include <new> // for std::bad_alloc
+#include <new>     // for std::bad_alloc
+#include <utility> // for std::forward
 
 #ifdef __SSE2__
 #include <xmmintrin.h>
@@ -235,6 +236,14 @@ public:
     {
         ::new(p) T(val);
     }
+
+#if (__cplusplus >= 201103L)
+    template< class U, class... Args >
+    void construct(U * p, Args&& ... args)
+    {
+        ::new(p) T(std::forward<Args>(args)...);
+    }
+#endif
 
     void destroy(pointer p)
     {
