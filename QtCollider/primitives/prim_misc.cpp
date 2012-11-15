05d6484f550a26073d20db4391427d39c65a7055
Jakob Leben <jakob.leben@gmail.com>
2012-11-15 09:01:31 +0100

    qtcollider: fix slip of the tongue

--- QtCollider/primitives/prim_misc.cpp
+++ QtCollider/primitives/prim_misc.cpp
@@ -139,7 +139,7 @@ QC_LANG_PRIMITIVE( QFont_DefaultFamilyForStyle, 1, PyrSlot *r, PyrSlot *a, VMGlo
         family = "serif";
         break;
     case 2:
-        styleHint = QFont::Monospace;
+        styleHint = QFont::TypeWriter;
         family = "monospace";
         break;
     default:
