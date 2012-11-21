supercollider/supercollider

commit 852d146bafebc5aae8cfdc9e3deb9dd45dddbcfa

SC Class Library: Event - support SynthDef instances as instrument
at the moment, the following code does not work, since `instrument' is expected
to be a string or symbol.

a = SynthDef(\foo, {Out.ar(0, SinOsc.ar * -30.dbamp)}).add

(
    instrument: a
).play

with this patch, asDefName is used in order to resolve the synthdef name.

Signed-off-by: Tim Blechmann <tim@klingt.org>

SCClassLibrary/Common/Collections/Event.sc
@@ -253,7 +253,7 @@ Event : Environment {
 					var	synthLib, synthDesc, desc;
 						// if user specifies a msgFunc, prefer user's choice
 					if(~msgFunc.isNil) {
-						instrument = ~instrument = instrument.asSymbol;
+						instrument = ~instrument = instrument.asDefName;
 						synthLib = ~synthLib ?? { SynthDescLib.global };
 						synthDesc = desc = synthLib.at(instrument);
 						if (desc.notNil) {
@@ -266,6 +266,7 @@ Event : Environment {
 				},
 				synthDefName: { |instrument, variant, synthDesc|
 						// allow `nil to cancel a variant in a pattern
+					instrument = instrument.asDefName;
 					variant = variant.dereference;
 					if(variant.notNil and: { synthDesc.notNil and: { synthDesc.hasVariants } })
 						{ (instrument ++ "." ++ variant).asSymbol }

commit a09086bb69ea0cdbb979e8cd35266ee044146dfd

Fix Event getMsgFunc so that it correctly looks up the SynthDesc
The commit 852d146bafebc5aae8cfdc9e3deb9dd45dddbcfa introduced a bug by
replacing .asSymbol with .asDefName, but the latter returns a String,
while the SynthDescLib is indexed by symbols. This commit fixes it by
converting it back to a symbol if it was not already a symbol.

SCClassLibrary/Common/Collections/Event.sc
@@ -250,12 +250,16 @@ Event : Environment {
 					// assembles a synth control list from event values
 
 				getMsgFunc: { |instrument|
-					var	synthLib, synthDesc, desc;
+					var	synthLib, desc;
 						// if user specifies a msgFunc, prefer user's choice
 					if(~msgFunc.isNil) {
-						instrument = ~instrument = instrument.asDefName;
+						instrument = ~instrument = if(instrument.class===Symbol) {
+							instrument
+						} {
+							instrument.asDefName.asSymbol
+						};
 						synthLib = ~synthLib ?? { SynthDescLib.global };
-						synthDesc = desc = synthLib.at(instrument);
+						desc = synthLib.at(instrument);
 						if (desc.notNil) {
 							~hasGate = desc.hasGate;
 							~msgFunc = desc.msgFunc;