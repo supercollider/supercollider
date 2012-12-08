8d6d3ed725ff60542187d38e9569581aa10cfb2b
Jakob Leben <jakob.leben@gmail.com>
2012-12-08 09:15:19 +0100

    sclang: SCIpcClient - fix includes
    (cherry picked from commit 2b977e4d07f316498c21d6526e3a64b07dc627b8)

--- editors/sc-ide/primitives/sc_ipc_client.cpp
+++ editors/sc-ide/primitives/sc_ipc_client.cpp
@@ -18,7 +18,8 @@
  *    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
  */
 
-#include <QtNetwork/QLocalSocket>
+#include <QLocalSocket>
+#include <QDataStream>
 
 #include <cstdlib>
 
