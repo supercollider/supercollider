a20e586d50c88154406b4e2afee9e5c68756c9ff
Tim Blechmann <tim@klingt.org>
2012-12-16 13:37:18 +0100

    plugins: DiskIO - adapt header
    * copyright notice should go to the file header
    * changes should be logged in the git history
    
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/plugins/DiskIO_UGens.cpp
+++ server/plugins/DiskIO_UGens.cpp
@@ -1,25 +1,24 @@
 /*
 	SuperCollider real time audio synthesis system
-    Copyright (c) 2002 James McCartney. All rights reserved.
+	Copyright (c) 2002 James McCartney. All rights reserved.
+	Copyright (c) 2009 Marije Baalman
+	Copyright (c) 2008 Scott Wilson
+	Copyright (c) 2012 Tim Blechmann
 	http://www.audiosynth.com
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
-*/
-/*
-  Output of frame index for DiskOut by Marije Baalman (nescivi), 2009
-  VDiskIn by Scott Wilson, 2008
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
 #include "MsgFifo.h"
