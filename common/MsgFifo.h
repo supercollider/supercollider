e7537bf47117a9dbec4432133a5533c0f769d1f7
Tim Blechmann <tim@klingt.org>
2013-02-11 11:58:12 +0100

    common: MsgFifo - rewrite in a portable manner with c++11-style atomics
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- common/MsgFifo.h
+++ common/MsgFifo.h
@@ -1,36 +1,35 @@
 /*
 	SuperCollider real time audio synthesis system
-    Copyright (c) 2002 James McCartney. All rights reserved.
+	Copyright (c) 2002 James McCartney. All rights reserved.
 	http://www.audiosynth.com
 
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
+	Copyright (c) 2013 Tim Blechmann
 
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation; either version 2 of the License, or
+	(at your option) any later version.
 
-    You should have received a copy of the GNU General Public License
-    along with this program; if not, write to the Free Software
-    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 */
 
 #ifndef _MsgFifo_
 #define _MsgFifo_
 
-#ifdef __APPLE__
-# include <libkern/OSAtomic.h>
-#endif
-
-#ifdef _WIN32
-# include <winsock2.h>
-# include <windows.h>
-#endif
+#include <boost/atomic.hpp>
 
 /////////////////////////////////////////////////////////////////////
+
+namespace SCMsgFifo {
+
+using namespace boost;
 
 template <class MsgType, int N>
 class MsgFifo
@@ -40,78 +39,47 @@ public:
 			: mReadHead(0), mWriteHead(0), mFreeHead(0)
 	{}
 
-	void MakeEmpty() { mFreeHead = mReadHead = mWriteHead; }
-	bool IsEmpty() { return mReadHead == mWriteHead; }
-	bool HasData() { return mReadHead != mWriteHead; }
-	bool NeedsFree() { return mFreeHead != mReadHead; }
+	void MakeEmpty() {
+		int writeHead= mWriteHead.load();
+		mReadHead.store(writeHead, memory_order_relaxed);
+		mFreeHead.store(writeHead, memory_order_release);
+	}
+	bool IsEmpty() { return mReadHead.load(memory_order_relaxed) == mWriteHead.load(memory_order_relaxed); }
+	bool HasData() { return mReadHead.load(memory_order_relaxed) != mWriteHead.load(memory_order_relaxed); }
+	bool NeedsFree() { return mFreeHead.load(memory_order_relaxed) != mReadHead.load(memory_order_relaxed); }
 
 	bool Write(MsgType& data)
 	{
-		unsigned int next = NextPos(mWriteHead);
-		if (next == mFreeHead) return false; // fifo is full
+		unsigned int next = NextPos(mWriteHead.load(memory_order_relaxed));
+		if (next == mFreeHead.load(memory_order_relaxed)) return false; // fifo is full
 		mItems[next] = data;
-#ifdef __APPLE__
-		// we don't really need a compare and swap, but this happens to call
-		// the PowerPC memory barrier instruction lwsync.
-		OSAtomicCompareAndSwap32Barrier(mWriteHead, next, &mWriteHead);
-#elif defined(_WIN32)
-		InterlockedExchange(reinterpret_cast<volatile LONG*>(&mWriteHead),next);
-#else
-#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1) && ( !defined(__INTEL_COMPILER) || defined(__ia64) )
-		__sync_synchronize();
-#endif
-		mWriteHead = next;
-#endif
+
+		mWriteHead.store(next, memory_order_release);
+
 		return true;
 	}
 
 	void Perform() // get next and advance
 	{
 		while (HasData()) {
-			unsigned int next = NextPos(mReadHead);
+			unsigned int next = NextPos(mReadHead.load(memory_order_relaxed));
 			mItems[next].Perform();
-#ifdef __APPLE__
-			// we don't really need a compare and swap, but this happens to call
-			// the PowerPC memory barrier instruction lwsync.
-			OSAtomicCompareAndSwap32Barrier(mReadHead, next, &mReadHead);
-#elif defined(_WIN32)
-      InterlockedExchange(reinterpret_cast<volatile LONG*>(&mReadHead),next);
-#else
-#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1) && ( !defined(__INTEL_COMPILER) || defined(__ia64) )
-			__sync_synchronize();
-#endif
-			mReadHead = next;
-#endif
+			mReadHead.store(next, memory_order_release);
 		}
 	}
 	void Free() // reclaim messages
 	{
 		while (NeedsFree()) {
-			unsigned int next = NextPos(mFreeHead);
+			unsigned int next = NextPos(mFreeHead.load(memory_order_relaxed));
 			mItems[next].Free();
-#ifdef __APPLE__
-			// we don't really need a compare and swap, but this happens to call
-			// the PowerPC memory barrier instruction lwsync.
-			OSAtomicCompareAndSwap32Barrier(mFreeHead, next, &mFreeHead);
-#elif defined(_WIN32)
-			InterlockedExchange(reinterpret_cast<volatile LONG*>(&mFreeHead),next);
-#else
-#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1) && ( !defined(__INTEL_COMPILER) || defined(__ia64) )
-			__sync_synchronize();
-#endif
-			mFreeHead = next;
-#endif
+			mFreeHead.store(next, memory_order_release);
 		}
 	}
 
 private:
 	int NextPos(int inPos) { return (inPos + 1) & (N - 1); }
 
-#ifdef __APPLE__
-	int32_t mReadHead, mWriteHead, mFreeHead;
-#else
-	volatile int mReadHead, mWriteHead, mFreeHead;
-#endif
+	atomic<int> mReadHead, mWriteHead, mFreeHead;
 	MsgType mItems[N];
 };
 
@@ -122,67 +90,45 @@ class MsgFifoNoFree
 {
 public:
 	MsgFifoNoFree()
-			: mReadHead(0), mWriteHead(0)
-	{
-	}
+		: mReadHead(0), mWriteHead(0)
+	{}
 
-	void MakeEmpty() { mReadHead = mWriteHead; }
-	bool IsEmpty() { return mReadHead == mWriteHead; }
-	bool HasData() { return mReadHead != mWriteHead; }
+	void MakeEmpty() { mReadHead.store(mWriteHead.load()); }
+	bool IsEmpty() { return mReadHead.load(memory_order_relaxed) == mWriteHead.load(memory_order_relaxed); }
+	bool HasData() { return mReadHead.load(memory_order_relaxed) != mWriteHead.load(memory_order_relaxed); }
 
 	bool Write(MsgType& data)
 	{
-		unsigned int next = NextPos(mWriteHead);
-		if (next == mReadHead) return false; // fifo is full
+		unsigned int next = NextPos(mWriteHead.load(memory_order_relaxed));
+		if (next == mReadHead.load(memory_order_relaxed)) return false; // fifo is full
 		mItems[next] = data;
-#ifdef __APPLE__
-			// we don't really need a compare and swap, but this happens to call
-			// the PowerPC memory barrier instruction lwsync.
-			OSAtomicCompareAndSwap32Barrier(mWriteHead, next, &mWriteHead);
-#elif defined(_WIN32)
-			InterlockedExchange(reinterpret_cast<volatile LONG*>(&mWriteHead),next);
-#else
-#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1) && ( !defined(__INTEL_COMPILER) || defined(__ia64) )
-			__sync_synchronize();
-#endif
-			mWriteHead = next;
-#endif
+
+		mWriteHead.store(next, memory_order_release);
+
 		return true;
 	}
 
 	void Perform() // get next and advance
 	{
 		while (HasData()) {
-			unsigned int next = NextPos(mReadHead);
+			unsigned int next = NextPos(mReadHead.load(memory_order_relaxed));
 			mItems[next].Perform();
-#ifdef __APPLE__
-			// we don't really need a compare and swap, but this happens to call
-			// the PowerPC memory barrier instruction lwsync.
-			OSAtomicCompareAndSwap32Barrier(mReadHead, next, &mReadHead);
-#elif defined(_WIN32)
-			InterlockedExchange(reinterpret_cast<volatile LONG*>(&mReadHead),next);
-#else
-#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ >= 1) && ( !defined(__INTEL_COMPILER) || defined(__ia64) )
-			__sync_synchronize();
-#endif
-			mReadHead = next;
-#endif
+			mReadHead.store(next, memory_order_release);
 		}
 	}
 
 private:
 	int NextPos(int inPos) { return (inPos + 1) & (N - 1); }
-#ifdef __APPLE__
-	int32_t mReadHead, mWriteHead;
-#else
-	volatile int mReadHead, mWriteHead;
-#endif
+
+	atomic<int> mReadHead, mWriteHead;
 	MsgType mItems[N];
 };
 
+}
+
+using SCMsgFifo::MsgFifo;
+using SCMsgFifo::MsgFifoNoFree;
+
 /////////////////////////////////////////////////////////////////////
 
-
 #endif
-
-
