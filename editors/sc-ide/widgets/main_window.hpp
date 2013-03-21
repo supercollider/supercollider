ee0ae33a16783b419694a07fe6c68e2347355f1b
Jakob Leben <jakob.leben@gmail.com>
2013-03-21 12:55:08 +0100

    sc ide: main window - apply cursor blinking settings at startup

--- editors/sc-ide/widgets/main_window.hpp
+++ editors/sc-ide/widgets/main_window.hpp
@@ -202,6 +202,7 @@ private:
     void openSession( QString const & sessionName );
     bool checkFileExtension( const QString & fpath );
     void toggleInterpreterActions( bool enabled);
+    void applyCursorBlinkingSettings();
 
     Main *mMain;
 
