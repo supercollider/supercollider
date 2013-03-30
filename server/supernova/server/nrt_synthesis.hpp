fd209c1e4d4956bcddb79366c58655aef61621ed
Tim Blechmann <tim@klingt.org>
2013-03-30 10:50:03 +0100

    supernova: nrt synthesis - improve stats & avoid packet size limitation
    Signed-off-by: Tim Blechmann <tim@klingt.org>

--- server/supernova/server/nrt_synthesis.hpp
+++ server/supernova/server/nrt_synthesis.hpp
@@ -19,10 +19,12 @@
 #define SERVER_NRT_SYNTHESIS_HPP
 
 #include <cstdio>
+#include <chrono>
 #include <fstream>
 #include <string>
 
 #include <boost/integer/endian.hpp>
+#include <boost/format.hpp>
 
 #include "server_args.hpp"
 #include "server.hpp"
@@ -87,23 +89,34 @@ struct non_realtime_synthesis_engine
     void run(void)
     {
         using namespace std;
-        array<char, 16384> packet_buffer;
+        using namespace std::chrono;
 
-        printf("Starting non-rt synthesis");
+        const int reserved_packed_size = 16384;
+        std::vector <char> packet_vector(reserved_packed_size, 0);
+
+        printf("\nPerforming non-rt synthesis:\n");
         backend.activate_audio();
+
+        auto start_time = steady_clock::now();
 
         while (!command_stream.eof()) {
             boost::integer::big32_t packet_size;
             command_stream.read((char*)&packet_size, sizeof(packet_size));
 
             assert(packet_size > 0);
-            command_stream.read(packet_buffer.data(), packet_size);
 
-            time_tag bundle_time = instance->handle_bundle_nrt(packet_buffer.data(), packet_size);
+            const bool huge_packet = (packet_size >= reserved_packed_size);
+
+            if (huge_packet)
+                packet_vector.resize(packet_size);
+
+            command_stream.read(packet_vector.data(), packet_size);
+
+            time_tag bundle_time = instance->handle_bundle_nrt(packet_vector.data(), packet_size);
 
             size_t seconds = bundle_time.get_secs();
             size_t nano_seconds = bundle_time.get_nanoseconds();
-            printf("Next OSC bundle: %zu.%09zu\n", seconds, nano_seconds);
+            printf("  Next OSC bundle: %zu.%09zu\n", seconds, nano_seconds);
 
             while (instance->current_time() < bundle_time) {
                 if (instance->quit_requested())
@@ -114,20 +127,48 @@ struct non_realtime_synthesis_engine
                 else
                     backend.audio_fn_noinput(samples_per_block);
             }
+
+            if (huge_packet)
+                packet_vector.resize(16384);
         }
 
     done:
         backend.deactivate_audio();
-        printf("Non-rt synthesis finished\n");
+        auto end_time = steady_clock::now();
+        std::string elapsed_string = format_duration ( end_time - start_time );
+
+        printf("\nNon-rt synthesis finished in %s\n", elapsed_string.c_str());
 
         auto peaks = backend.get_peaks();
         printf("Peak summary:\n");
         for (size_t channel = 0; channel != peaks.size(); ++channel) {
             auto amplitude = peaks[channel];
-            printf("Channel %zu: %gdB\n", channel, sc_ampdb(amplitude));
+            printf("  Channel %zu: %gdB\n", channel, sc_ampdb(amplitude));
         }
     }
 
+    template <typename Duration>
+    static std::string format_duration(Duration const & duration)
+    {
+        using namespace boost;
+        using namespace std::chrono;
+
+        auto elapsed_hours = duration_cast<hours>(duration);
+        auto remain = duration - elapsed_hours;
+        auto elapsed_minutes = duration_cast<minutes>(remain);
+        remain -= elapsed_minutes;
+        auto elapsed_nanoseconds = duration_cast<nanoseconds>(remain);
+        double seconds = elapsed_nanoseconds.count() * 1e-9;
+
+        if (elapsed_hours.count())
+            return str( format("%|d|h %|d|m %|0.3f|s") % elapsed_hours.count() % elapsed_minutes.count() % seconds );
+        if (elapsed_minutes.count())
+            return str( format("%|d|m %|.3f|s")      % elapsed_minutes.count() % seconds );
+        else
+            return str( format("%|.3f|s") % seconds );
+    }
+
+private:
     sndfile_backend<non_rt_functor, float, true> backend;
     std::fstream command_stream;
     int samples_per_block;
