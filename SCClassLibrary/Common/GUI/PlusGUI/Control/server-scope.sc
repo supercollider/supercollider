supercollider/supercollider

commit f8c3ba6318301db86b1d21006de710e7e512daf2

class library: Server.scope - remove limitation to 16 channels
we also default to the number ouf output channels if we're scoping the
output busses.

fixes #769.

Signed-off-by: Tim Blechmann <tim@klingt.org>

SCClassLibrary/Common/GUI/PlusGUI/Control/server-scope.sc
@@ -1,7 +1,6 @@
 + Server {
 	scope { arg numChannels, index = 0, bufsize = 4096, zoom = 1, rate = \audio;
-		numChannels = (numChannels ? 2).min(16);
-		zoom = zoom ? 1.0;
+		numChannels = numChannels ?? { if (index == 0) { options.numOutputBusChannels } { 2 } };
 
 		if(scopeWindow.isNil) {
 			scopeWindow = Stethoscope(this, numChannels, index, bufsize, zoom, rate, nil,

SCClassLibrary/Common/GUI/osx/scide_scapp/SCStethoscope.sc
@@ -7,7 +7,10 @@ SCStethoscope {
 
 
 	*new { arg server, numChannels = 2, index, bufsize = 4096, zoom, rate, view, bufnum;
-		if(server.inProcess.not, { "scope works only with internal server".error; ^nil });		^super.newCopyArgs(server, numChannels, rate ? \audio).makeWindow(view)
+		numChannels = min(numChannels, 16);
+
+		if(server.inProcess.not, { "scope works only with internal server".error; ^nil });
+		^super.newCopyArgs(server, numChannels, rate ? \audio).makeWindow(view)
 		.index_(index ? 0).zoom_(zoom).allocBuffer(bufsize, bufnum).run;
 	}
 

commit 2b885352b70f654bff74827afbfba3af63eade31

sc class library: fix regression in Server:-scope
[fixes #825]

Apply default value for 'zoom' argument even if explicitly passed 'nil'.
There's an awful many places where this method is called in such way, so
rather take care of it in the method itself.

The regression was introduced in [f8c3ba6318301db86b1d21006de710e7e512daf2]

SCClassLibrary/Common/GUI/PlusGUI/Control/server-scope.sc
@@ -1,5 +1,5 @@
 + Server {
-	scope { arg numChannels, index = 0, bufsize = 4096, zoom = 1, rate = \audio;
+	scope { arg numChannels, index = 0, bufsize = 4096, zoom = (1), rate = \audio;
 		numChannels = numChannels ?? { if (index == 0) { options.numOutputBusChannels } { 2 } };
 
 		if(scopeWindow.isNil) {