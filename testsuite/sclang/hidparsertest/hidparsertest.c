37008aadf6a6fbbfd40c67a3c93d63414a6d2054
Marije Baalman <marije@nescivi.nl>
2013-09-12 15:19:27 +0200

    add copyright notices

--- testsuite/sclang/hidparsertest/hidparsertest.c
+++ testsuite/sclang/hidparsertest/hidparsertest.c
@@ -1,3 +1,27 @@
+/* hidapi_parser $ 
+ *
+ * Copyright (C) 2013, Marije Baalman <nescivi _at_ gmail.com>
+ * This work was funded by a crowd-funding initiative for SuperCollider's [1] HID implementation
+ * including a substantial donation from BEK, Bergen Center for Electronic Arts, Norway
+ * 
+ * [1] http://supercollider.sourceforge.net
+ * [2] http://www.bek.no
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <wchar.h>
