25dd087add27887e28a13d6c3e33bb4824d98178
Marije Baalman <marije@nescivi.nl>
2013-10-06 13:14:25 +0200

    lang : small fix to license text

--- lang/LangPrimSource/SC_HID_api.cpp
+++ lang/LangPrimSource/SC_HID_api.cpp
@@ -22,7 +22,6 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301  USA
 
-	Linux Input Device interface, 2004 <sk>
 */
 
 #include "SCBase.h"
