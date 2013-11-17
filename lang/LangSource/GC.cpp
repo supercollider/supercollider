a73b0a079a0413c5b7b1062e3311e086b6300fc3
Holger Ballweg <holger@music-nerds.net>
2013-11-17 18:37:36 +0000

    MacOS 10.9 SDK compat: remove all pre 10.7 compat

--- lang/LangSource/GC.cpp
+++ lang/LangSource/GC.cpp
@@ -797,10 +797,8 @@ bool PyrGC::SanityCheck2()
 	return mNumGrey == numgrey;
 }
 
-#if defined(SC_DARWIN) && defined(MACOS_SDK_PRE_10_7)
-	#include <CoreServices/../Frameworks/CarbonCore.framework/Headers/MacTypes.h>
-#elif defined(SC_DARWIN)
-	#include <MacTypes.h>	
+#ifdef SC_DARWIN
+	#include <MacTypes.h>  
 #endif
 
 bool PyrGC::SanityCheck()
