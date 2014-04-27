b662ce5fe95042ceea2dcb0f8d4ea0d1a17be8c1
Julian Rohrhuber <jrhb@users.sourceforge.net>
2014-04-27 23:07:38 +0200

    server: bounded controls: refactor whole parameter init, not only bounds checking

--- server/scsynth/SC_GraphDef.cpp
+++ server/scsynth/SC_GraphDef.cpp
@@ -251,21 +251,36 @@ static inline bool sortParamSpec(ParamSpec paramSpec1, ParamSpec paramSpec2)
 	return paramSpec1.mIndex < paramSpec2.mIndex;
 }
 
-static inline void calcParamSpecChannels(GraphDef* graphDef)
+inline static void calcParamSpecs(GraphDef* graphDef, char*& buffer)
 {
-	// calculate numChannels for each spec
-	// printf("\n\n**************\n");
-	uint32 nSpecs = graphDef->mNumParamSpecs;
-	std::sort(graphDef->mParamSpecs, graphDef->mParamSpecs + nSpecs, sortParamSpec);
-	for (uint32 i=0; i<(nSpecs - 1); ++i) {
-		ParamSpec *paramSpec = graphDef->mParamSpecs + i;
-		ParamSpec *nextParamSpec = paramSpec + 1;
-		paramSpec->mNumChannels = nextParamSpec->mIndex - paramSpec->mIndex;
-		// printf("%s: numChannels = %i\n", paramSpec->mName, paramSpec->mNumChannels);
+	if (graphDef->mNumParamSpecs) {
+		int hashTableSize = NEXTPOWEROFTWO(graphDef->mNumParamSpecs);
+		graphDef->mParamSpecTable = new ParamSpecTable(&gMalloc, hashTableSize, false);
+		graphDef->mParamSpecs = (ParamSpec*)malloc(graphDef->mNumParamSpecs * sizeof(ParamSpec));
+		uint32 nSpecs = graphDef->mNumParamSpecs;
+		for (uint32 i=0; i<nSpecs; ++i) {
+			ParamSpec *paramSpec = graphDef->mParamSpecs + i;
+			ParamSpec_Read(paramSpec, buffer);
+			graphDef->mParamSpecTable->Add(paramSpec);
+		}
+		// calculate numChannels for each spec
+		// printf("\n\n**************\n");
+		std::sort(graphDef->mParamSpecs, graphDef->mParamSpecs + nSpecs, sortParamSpec);
+		for (uint32 i=0; i<(nSpecs - 1); ++i) {
+			ParamSpec *paramSpec = graphDef->mParamSpecs + i;
+			ParamSpec *nextParamSpec = paramSpec + 1;
+			paramSpec->mNumChannels = nextParamSpec->mIndex - paramSpec->mIndex;
+			// printf("%s: numChannels = %i\n", paramSpec->mName, paramSpec->mNumChannels);
+		}
+		ParamSpec *paramSpec = graphDef->mParamSpecs + nSpecs - 1;
+		paramSpec->mNumChannels = graphDef->mNumControls - paramSpec->mIndex;
+		// printf("%s: numChannels = %i\n", paramSpec->mName, paramSpec->mNumChannels, paramSpec->mIndex);
+	} else {
+		// empty table to eliminate test in Graph_SetControl
+		graphDef->mParamSpecTable = new ParamSpecTable(&gMalloc, 4, false);
+		graphDef->mParamSpecs = 0;
 	}
-	ParamSpec *paramSpec = graphDef->mParamSpecs + nSpecs - 1;
-	paramSpec->mNumChannels = graphDef->mNumControls - paramSpec->mIndex;
-	// printf("%s: numChannels = %i\n", paramSpec->mName, paramSpec->mNumChannels, paramSpec->mIndex);
+
 }
 
 // ver 2
@@ -299,22 +314,7 @@ GraphDef* GraphDef_Read(World *inWorld, char*& buffer, GraphDef* inList, int32 i
 
 	graphDef->mNumParamSpecs = readInt32_be(buffer);
 
-	if (graphDef->mNumParamSpecs) {
-		int hashTableSize = NEXTPOWEROFTWO(graphDef->mNumParamSpecs);
-		graphDef->mParamSpecTable = new ParamSpecTable(&gMalloc, hashTableSize, false);
-		graphDef->mParamSpecs = (ParamSpec*)malloc(graphDef->mNumParamSpecs * sizeof(ParamSpec));
-		for (uint32 i=0; i<graphDef->mNumParamSpecs; ++i) {
-			ParamSpec *paramSpec = graphDef->mParamSpecs + i;
-			ParamSpec_Read(paramSpec, buffer);
-			graphDef->mParamSpecTable->Add(paramSpec);
-		}
-		calcParamSpecChannels(graphDef);
-
-	} else {
-		// empty table to eliminate test in Graph_SetControl
-		graphDef->mParamSpecTable = new ParamSpecTable(&gMalloc, 4, false);
-		graphDef->mParamSpecs = 0;
-	}
+	calcParamSpecs(graphDef, buffer);
 
 	graphDef->mNumWires = graphDef->mNumConstants;
 	graphDef->mNumUnitSpecs = readInt32_be(buffer);
@@ -409,23 +409,7 @@ GraphDef* GraphDef_ReadVer1(World *inWorld, char*& buffer, GraphDef* inList, int
 	}
 
 	graphDef->mNumParamSpecs = readInt16_be(buffer);
-	if (graphDef->mNumParamSpecs) {
-		int hashTableSize = NEXTPOWEROFTWO(graphDef->mNumParamSpecs);
-		graphDef->mParamSpecTable = new ParamSpecTable(&gMalloc, hashTableSize, false);
-		graphDef->mParamSpecs = (ParamSpec*)malloc(graphDef->mNumParamSpecs * sizeof(ParamSpec));
-		uint32 nSpecs = graphDef->mNumParamSpecs;
-		for (uint32 i=0; i<nSpecs; ++i) {
-			ParamSpec *paramSpec = graphDef->mParamSpecs + i;
-			ParamSpec_ReadVer1(paramSpec, buffer);
-			graphDef->mParamSpecTable->Add(paramSpec);
-		}
-		calcParamSpecChannels(graphDef);
-
-	} else {
-		// empty table to eliminate test in Graph_SetControl
-		graphDef->mParamSpecTable = new ParamSpecTable(&gMalloc, 4, false);
-		graphDef->mParamSpecs = 0;
-	}
+	calcParamSpecs(graphDef, buffer);
 
 	graphDef->mNumWires = graphDef->mNumConstants;
 	graphDef->mNumUnitSpecs = readInt16_be(buffer);
