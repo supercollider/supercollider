9ab8a29519d84b4c148e2c6b73f4ccc3fadf1be8
Marije Baalman <nescivi@gmail.com>
2014-09-03 12:30:26 +0200

    lang: HID: change to use method in hidapi library for setting output value

--- lang/LangPrimSource/SC_HID_api.cpp
+++ lang/LangPrimSource/SC_HID_api.cpp
@@ -912,8 +912,9 @@ int prHID_API_SetElementOutput( VMGlobals* g, int numArgsPushed ){
 			curelement = hid_get_next_output_element( curelement );
 		}
 		if ( thiselement != NULL ){
-			thiselement->value = value;
-			hid_send_output_report( devdesc, thiselement->report_id );
+			hid_element_set_output_value( devdesc, this_element, value );
+// 			thiselement->value = value;
+// 			hid_send_output_report( devdesc, thiselement->report_id );
 		}
 	}
 	return errNone;
