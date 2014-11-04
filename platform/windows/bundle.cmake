3cbd0c91b48f1ac9a66a3c2dd02041003bdb1880
Tim Blechmann <tim@klingt.org>
2014-11-04 17:31:28 +0100

    cmake: fix windows install script

--- platform/windows/bundle.cmake
+++ platform/windows/bundle.cmake
@@ -1,6 +1,9 @@
 include(BundleUtilities)
-fixup_bundle(
-    "${SC_INSTALL_PREFIX}/SuperCollider/sclang.exe"
+
+foreach(exe scide sclang scsynth)
+  fixup_bundle(
+    "${SC_INSTALL_PREFIX}/SuperCollider/${exe}.exe"
     ""
     "${SC_DEPENDENCY_DIRS}"
-)
+  )
+endforeach()
