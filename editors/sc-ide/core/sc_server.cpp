supercollider/supercollider

commit 35ddd7241b688c2b0df40fcc863c16841458e45b

IDE: Workaround to prevent UDP event feedback loop (qt5.3 bug)
After qt5.3, booting the server from the IDE would cause 150%+ cpu usage due to this QT bug:
https://bugreports.qt-project.org/browse/QTBUG-39080

After some fiddling last night, this workaround bypasses god-knows-what mechanisms in Qt socket notification to avoid the feedback. Given all the weirdness I saw in the Qt sockets implementation, it might be better to move this to boost long term, but this works for now. :)

editors/sc-ide/core/sc_server.cpp
@@ -50,11 +50,6 @@ ScServer::ScServer(ScProcess *scLang, Settings::Manager *settings, QObject *pare
     createActions(settings);
 
     mUdpSocket = new QUdpSocket(this);
-    for (int port = 57140; port != 57150; ++port) {
-        bool success = mUdpSocket->bind(port);
-        if (success)
-            break;
-    }
     startTimer(333);
 
     mRecordTimer.setInterval(1000);
@@ -64,6 +59,7 @@ ScServer::ScServer(ScProcess *scLang, Settings::Manager *settings, QObject *pare
             this, SLOT(onScLangStateChanged(QProcess::ProcessState)));
     connect(scLang, SIGNAL(response(QString,QString)),
             this, SLOT(onScLangReponse(QString,QString)));
+    connect(mUdpSocket, SIGNAL(readyRead()), this, SLOT(onReadyRead()));
 }
 
 void ScServer::createActions(Settings::Manager * settings)
@@ -449,38 +445,42 @@ void ScServer::handleRuningStateChangedMsg( const QString & data )
     emit runningStateChange( serverRunningState, qstrHostName, port );
 }
 
-void ScServer::timerEvent(QTimerEvent * event)
+void ScServer::onReadyRead()
 {
-    if (mUdpSocket->hasPendingDatagrams()) {
-        size_t datagramSize = mUdpSocket->pendingDatagramSize();
-        QByteArray array(datagramSize, 0);
-        mUdpSocket->readDatagram(array.data(), datagramSize);
+    if (mUdpSocket->isValid()) {
+        if (mUdpSocket->hasPendingDatagrams()) {
+            size_t datagramSize = mUdpSocket->pendingDatagramSize();
+            QByteArray array(datagramSize, 0);
+            mUdpSocket->readDatagram(array.data(), datagramSize);
 
-        if (!mPort)
-            return;
+            if (!mPort)
+                return;
 
-        if (array[0]) {
-            char *addr = array.data();
-            const char * data = OSCstrskip(array.data());
-            int size = datagramSize - (data - addr);
+            if (array[0]) {
+                char *addr = array.data();
+                const char * data = OSCstrskip(array.data());
+                int size = datagramSize - (data - addr);
 
-            if (strcmp(addr, "/status.reply") == 0) {
-                sc_msg_iter reply(size, data);
-                int	unused     = reply.geti();
-                int	ugenCount  = reply.geti();
-                int	synthCount = reply.geti();
-                int	groupCount = reply.geti();
-                int	defCount   = reply.geti();
-                float avgCPU   = reply.getf();
-                float peakCPU  = reply.getf();
-                double srNom   = reply.getd();
-                double srAct   = reply.getd();
-
-                emit updateServerStatus(ugenCount, synthCount, groupCount, defCount, avgCPU, peakCPU);
+                if (strcmp(addr, "/status.reply") == 0) {
+                    sc_msg_iter reply(size, data);
+                    int	unused     = reply.geti();
+                    int	ugenCount  = reply.geti();
+                    int	synthCount = reply.geti();
+                    int	groupCount = reply.geti();
+                    int	defCount   = reply.geti();
+                    float avgCPU   = reply.getf();
+                    float peakCPU  = reply.getf();
+                    double srNom   = reply.getd();
+                    double srAct   = reply.getd();
+                    
+                    emit updateServerStatus(ugenCount, synthCount, groupCount, defCount, avgCPU, peakCPU);
+                }
             }
         }
     }
+}
 
+void ScServer::timerEvent(QTimerEvent * event) {
     if (mPort) {
         small_scpacket packet;
         packet.BeginMsg();
@@ -489,7 +489,7 @@ void ScServer::timerEvent(QTimerEvent * event)
         packet.addtag(',');
         packet.EndMsg();
 
-        mUdpSocket->writeDatagram(packet.data(), packet.size(), mServerAddress, mPort);
+        mUdpSocket->write(packet.data(), packet.size());
     }
 }
 
@@ -498,11 +498,13 @@ void ScServer::onRunningStateChanged( bool running, QString const & hostName, in
     if (running) {
         mServerAddress = QHostAddress(hostName);
         mPort = port;
+        mUdpSocket->connectToHost(mServerAddress, mPort);
     } else {
         mServerAddress.clear();
         mPort = 0;
         mIsRecording = false;
         mRecordTimer.stop();
+        mUdpSocket->disconnectFromHost();
     }
 
     updateToggleRunningAction();

editors/sc-ide/core/sc_server.hpp
@@ -118,6 +118,7 @@ private slots:
     void sendMuted( bool muted );
     void sendVolume( float volume );
     void sendDumpingOSC( bool dumping );
+    void onReadyRead();
 
 protected:
     virtual void timerEvent(QTimerEvent * event);

commit 8587a1342faa133bf4f850336e3c20276a8cb4fa

scide: fix regression of sending status messages
fixes a regression introduced by 35ddd7241b688c2b0df40fcc863c16841458e45b

editors/sc-ide/core/sc_server.cpp
@@ -461,10 +461,9 @@ void ScServer::timerEvent(QTimerEvent * event)
         stream << osc::BeginMessage("status");
         stream << osc::MessageTerminator();
 
-        qint64 sentSize = mUdpSocket->writeDatagram(stream.Data(), stream.Size(),
-                                                    mServerAddress, mPort);
+        qint64 sentSize = mUdpSocket->write(stream.Data(), stream.Size());
         if (sentSize == -1)
-            qCritical("Failed to send server status request.");
+            qCritical() << "Failed to send server status request:" << mUdpSocket->errorString();
     }
 }
 