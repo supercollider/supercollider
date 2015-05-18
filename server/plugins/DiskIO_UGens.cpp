02ed48fbafec5f1208c9662e8d7719c64cf5ab17
Victor Bombi <sonoro@telefonica.net>
2015-05-18 14:40:56 +0200

    Revert "scsynth: allow method unload in plugings and use it in DiskIO_UGens"
    This reverts commit 87e26cd75c6c7d4cfba40985cb38b113a38a0821.

--- server/plugins/DiskIO_UGens.cpp
+++ server/plugins/DiskIO_UGens.cpp
@@ -636,10 +636,6 @@ void VDiskIn_next_rate1(VDiskIn *unit, int inNumSamples)
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////
-C_LINKAGE SC_API_EXPORT void unload(InterfaceTable *inTable)
-{
-	gDiskIO.~DiskIOThread();
-}
 
 PluginLoad(DiskIO)
 {
