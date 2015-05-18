01cddd4072ed4dd44ff91ab8e14563f85dcad32a
Victor Bombi <sonoro@telefonica.net>
2015-05-18 15:21:14 +0200

    supernova: prevent supernova crash on exit on Linux and Mac

--- server/supernova/server/server.cpp
+++ server/supernova/server/server.cpp
@@ -87,7 +87,8 @@ void nova_server::prepare_backend(void)
 
 nova_server::~nova_server(void)
 {
-    delete sc_factory;
+    //we should delete but get chrashes at the moment on linux and macosx
+    //delete sc_factory;
 #if defined(JACK_BACKEND) || defined(PORTAUDIO_BACKEND)
     deactivate_audio();
 #endif
