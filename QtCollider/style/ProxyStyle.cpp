6aa76e573cf2608566eda2ea7f85556c87cc0856
scztt <scott@artificia.org>
2015-06-05 13:14:39 -0700

    lang: Respect OS pref for showing scrollbars

--- QtCollider/style/ProxyStyle.cpp
+++ QtCollider/style/ProxyStyle.cpp
@@ -4,9 +4,27 @@
 #include <QStyleOptionSlider>
 #include <QPainter>
 
+#ifdef Q_OS_MAC
+  #include "../hacks/hacks_mac.hpp"
+#endif
+
 using namespace QtCollider;
 
+static bool AlwaysShowScrollbars() {
 #ifdef Q_OS_MAC
+  return QtCollider::Mac::AlwaysShowScrollbars();
+  
+#elif Q_OS_X11
+  return true;
+
+#elif Q_OS_WIN
+  return true;
+
+#else
+  return true;
+#endif
+};
+
 void ProxyStyle::drawComplexControl ( ComplexControl ctrl, const QStyleOptionComplex *opt,
                                       QPainter *p, const QWidget * w) const
 {
@@ -21,31 +39,28 @@ void ProxyStyle::drawComplexControl ( ComplexControl ctrl, const QStyleOptionCom
     opt2.styleObject = NULL;
 
     QProxyStyle::drawComplexControl( ctrl, &opt2, p, w );
-
+    return;
+  }
+  
+  if (ctrl == QStyle::CC_ScrollBar && AlwaysShowScrollbars()) {
+    const QStyleOptionSlider *optSlider = static_cast<const QStyleOptionSlider*>(opt);
+    QStyleOptionSlider opt2( *optSlider );
+    opt2.state = State_On;
+    QProxyStyle::drawComplexControl( ctrl, static_cast<const QStyleOptionComplex*>(&opt2), p, w );
     return;
   }
 
-	if (ctrl == QStyle::CC_ScrollBar) {
-		const QStyleOptionSlider *optSlider = static_cast<const QStyleOptionSlider*>(opt);
-		QStyleOptionSlider opt2( *optSlider );
-		opt2.state = State_On;
-		QProxyStyle::drawComplexControl( ctrl, static_cast<const QStyleOptionComplex*>(&opt2), p, w );
-
-		return;
-	}
-
   QProxyStyle::drawComplexControl( ctrl, opt, p, w );
 }
-#endif
 
 int ProxyStyle::styleHint ( StyleHint hint, const QStyleOption * option,
                             const QWidget * widget, QStyleHintReturn * returnData ) const
 {
   switch( hint ) {
-//    case QStyle::SH_Slider_AbsoluteSetButtons:
-//      return Qt::LeftButton;
-//    case QStyle::SH_Slider_PageSetButtons:
-//      return Qt::NoButton;
+    case QStyle::SH_Slider_AbsoluteSetButtons:
+      return Qt::LeftButton;
+    case QStyle::SH_Slider_PageSetButtons:
+      return Qt::NoButton;
     case QStyle::SH_ScrollBar_Transient:
       return 1;
     default:
