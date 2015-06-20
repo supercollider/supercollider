6e2f1799459c86159f81fd2c2ca8018162aa29a1
Lucas Cornelisse <lucas@neoboolean.com>
2015-06-20 16:13:59 -0700

    IPC - fix linux build

--- SCDoc/SCDoc.tab.hpp
+++ SCDoc/SCDoc.tab.hpp
@@ -1,8 +1,8 @@
-/* A Bison parser, made by GNU Bison 3.0.4.  */
+/* A Bison parser, made by GNU Bison 3.0.2.  */
 
 /* Bison interface for Yacc-like parsers in C
 
-   Copyright (C) 1984, 1989-1990, 2000-2015 Free Software Foundation, Inc.
+   Copyright (C) 1984, 1989-1990, 2000-2013 Free Software Foundation, Inc.
 
    This program is free software: you can redistribute it and/or modify
    it under the terms of the GNU General Public License as published by
@@ -103,10 +103,10 @@ extern int scdocdebug;
 
 /* Value type.  */
 #if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
-
+typedef union YYSTYPE YYSTYPE;
 union YYSTYPE
 {
-#line 55 "SCDoc.y" /* yacc.c:1909  */
+#line 56 "SCDoc.y" /* yacc.c:1909  */
 
     intptr_t i;
     const char *id;
@@ -115,8 +115,6 @@ union YYSTYPE
 
 #line 117 "SCDoc.tab.hpp" /* yacc.c:1909  */
 };
-
-typedef union YYSTYPE YYSTYPE;
 # define YYSTYPE_IS_TRIVIAL 1
 # define YYSTYPE_IS_DECLARED 1
 #endif
--- SCDoc/lex.scdoc.cpp
+++ SCDoc/lex.scdoc.cpp
@@ -28,7 +28,7 @@
 #define FLEX_SCANNER
 #define YY_FLEX_MAJOR_VERSION 2
 #define YY_FLEX_MINOR_VERSION 5
-#define YY_FLEX_SUBMINOR_VERSION 39
+#define YY_FLEX_SUBMINOR_VERSION 35
 #if YY_FLEX_SUBMINOR_VERSION > 0
 #define FLEX_BETA
 #endif
@@ -161,7 +161,15 @@ typedef unsigned int flex_uint32_t;
 
 /* Size of default input buffer. */
 #ifndef YY_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k.
+ * Moreover, YY_BUF_SIZE is 2*YY_READ_BUF_SIZE in the general case.
+ * Ditto for the __ia64__ case accordingly.
+ */
+#define YY_BUF_SIZE 32768
+#else
 #define YY_BUF_SIZE 16384
+#endif /* __ia64__ */
 #endif
 
 /* The state buf must be large enough to hold one state per character in the main buffer.
@@ -173,12 +181,7 @@ typedef unsigned int flex_uint32_t;
 typedef struct yy_buffer_state *YY_BUFFER_STATE;
 #endif
 
-#ifndef YY_TYPEDEF_YY_SIZE_T
-#define YY_TYPEDEF_YY_SIZE_T
-typedef size_t yy_size_t;
-#endif
-
-extern yy_size_t scdocleng;
+extern int scdocleng;
 
 extern FILE *scdocin, *scdocout;
 
@@ -200,13 +203,6 @@ extern FILE *scdocin, *scdocout;
                     if ( scdoctext[yyl] == '\n' )\
                         --scdoclineno;\
             }while(0)
-    #define YY_LINENO_REWIND_TO(dst) \
-            do {\
-                const char *p;\
-                for ( p = yy_cp-1; p >= (dst); --p)\
-                    if ( *p == '\n' )\
-                        --scdoclineno;\
-            }while(0)
     
 /* Return all but the first "n" matched characters back to the input stream. */
 #define yyless(n) \
@@ -223,6 +219,11 @@ extern FILE *scdocin, *scdocout;
 	while ( 0 )
 
 #define unput(c) yyunput( c, (yytext_ptr)  )
+
+#ifndef YY_TYPEDEF_YY_SIZE_T
+#define YY_TYPEDEF_YY_SIZE_T
+typedef size_t yy_size_t;
+#endif
 
 #ifndef YY_STRUCT_YY_BUFFER_STATE
 #define YY_STRUCT_YY_BUFFER_STATE
@@ -241,7 +242,7 @@ struct yy_buffer_state
 	/* Number of characters read into yy_ch_buf, not including EOB
 	 * characters.
 	 */
-	yy_size_t yy_n_chars;
+	int yy_n_chars;
 
 	/* Whether we "own" the buffer - i.e., we know we created it,
 	 * and can realloc() it to grow it, and should free() it to
@@ -311,8 +312,8 @@ static YY_BUFFER_STATE * yy_buffer_stack = 0; /**< Stack as an array. */
 
 /* yy_hold_char holds the character lost when scdoctext is formed. */
 static char yy_hold_char;
-static yy_size_t yy_n_chars;		/* number of characters read into yy_ch_buf */
-yy_size_t scdocleng;
+static int yy_n_chars;		/* number of characters read into yy_ch_buf */
+int scdocleng;
 
 /* Points to current character in buffer. */
 static char *yy_c_buf_p = (char *) 0;
@@ -340,7 +341,7 @@ static void scdoc_init_buffer (YY_BUFFER_STATE b,FILE *file  );
 
 YY_BUFFER_STATE scdoc_scan_buffer (char *base,yy_size_t size  );
 YY_BUFFER_STATE scdoc_scan_string (yyconst char *yy_str  );
-YY_BUFFER_STATE scdoc_scan_bytes (yyconst char *bytes,yy_size_t len  );
+YY_BUFFER_STATE scdoc_scan_bytes (yyconst char *bytes,int len  );
 
 void *scdocalloc (yy_size_t  );
 void *scdocrealloc (void *,yy_size_t  );
@@ -372,7 +373,7 @@ void scdocfree (void *  );
 
 /* Begin user sect3 */
 
-#define scdocwrap() 1
+#define scdocwrap(n) 1
 #define YY_SKIP_YYWRAP
 
 typedef unsigned char YY_CHAR;
@@ -1820,7 +1821,7 @@ static int method_caller;
 
 
 
-#line 1824 "lex.scdoc.cpp"
+#line 1825 "lex.scdoc.cpp"
 
 #define INITIAL 0
 #define verbatim 1
@@ -1866,7 +1867,7 @@ FILE *scdocget_out (void );
 
 void scdocset_out  (FILE * out_str  );
 
-yy_size_t scdocget_leng (void );
+int scdocget_leng (void );
 
 char *scdocget_text (void );
 
@@ -1908,7 +1909,12 @@ static int input (void );
 
 /* Amount of stuff to slurp up with each read. */
 #ifndef YY_READ_BUF_SIZE
+#ifdef __ia64__
+/* On IA-64, the buffer size is 16k, not 8k */
+#define YY_READ_BUF_SIZE 16384
+#else
 #define YY_READ_BUF_SIZE 8192
+#endif /* __ia64__ */
 #endif
 
 /* Copy whatever the last rule matched to the standard output. */
@@ -2009,6 +2015,23 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
     
+#line 45 "SCDoc.l"
+
+
+
+
+if (scdoc_start_token) {
+    int t = scdoc_start_token;
+    scdoc_start_token = 0;
+//    colnum = 1;
+    if(t==START_METADATA) BEGIN(eat);
+    return t;
+}
+
+
+
+#line 2034 "lex.scdoc.cpp"
+
 	if ( !(yy_init) )
 		{
 		(yy_init) = 1;
@@ -2035,24 +2058,6 @@ YY_DECL
 		scdoc_load_buffer_state( );
 		}
 
-	{
-#line 45 "SCDoc.l"
-
-
-
-
-if (scdoc_start_token) {
-    int t = scdoc_start_token;
-    scdoc_start_token = 0;
-//    colnum = 1;
-    if(t==START_METADATA) BEGIN(eat);
-    return t;
-}
-
-
-
-#line 2055 "lex.scdoc.cpp"
-
 	while ( 1 )		/* loops until end-of-file is reached */
 		{
 		yy_cp = (yy_c_buf_p);
@@ -2069,7 +2074,7 @@ if (scdoc_start_token) {
 yy_match:
 		do
 			{
-			register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)] ;
+			register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
 			if ( yy_accept[yy_current_state] )
 				{
 				(yy_last_accepting_state) = yy_current_state;
@@ -2095,7 +2100,7 @@ yy_find_action:
 
 		if ( yy_act != YY_END_OF_BUFFER && yy_rule_can_match_eol[yy_act] )
 			{
-			yy_size_t yyl;
+			int yyl;
 			for ( yyl = 0; yyl < scdocleng; ++yyl )
 				if ( scdoctext[yyl] == '\n' )
 					   
@@ -2651,7 +2656,7 @@ YY_RULE_SETUP
 #line 176 "SCDoc.l"
 ECHO;
 	YY_BREAK
-#line 2655 "lex.scdoc.cpp"
+#line 2660 "lex.scdoc.cpp"
 case YY_STATE_EOF(INITIAL):
 case YY_STATE_EOF(verbatim):
 case YY_STATE_EOF(verbatim2):
@@ -2790,7 +2795,6 @@ case YY_STATE_EOF(method):
 			"fatal flex scanner internal error--no action found" );
 	} /* end of action switch */
 		} /* end of scanning one token */
-	} /* end of user's declarations */
 } /* end of scdoclex */
 
 /* yy_get_next_buffer - try to read in a new buffer
@@ -2846,21 +2850,21 @@ static int yy_get_next_buffer (void)
 
 	else
 		{
-			yy_size_t num_to_read =
+			int num_to_read =
 			YY_CURRENT_BUFFER_LVALUE->yy_buf_size - number_to_move - 1;
 
 		while ( num_to_read <= 0 )
 			{ /* Not enough room in the buffer - grow it. */
 
 			/* just a shorter name for the current buffer */
-			YY_BUFFER_STATE b = YY_CURRENT_BUFFER_LVALUE;
+			YY_BUFFER_STATE b = YY_CURRENT_BUFFER;
 
 			int yy_c_buf_p_offset =
 				(int) ((yy_c_buf_p) - b->yy_ch_buf);
 
 			if ( b->yy_is_our_buffer )
 				{
-				yy_size_t new_size = b->yy_buf_size * 2;
+				int new_size = b->yy_buf_size * 2;
 
 				if ( new_size <= 0 )
 					b->yy_buf_size += b->yy_buf_size / 8;
@@ -2891,7 +2895,7 @@ static int yy_get_next_buffer (void)
 
 		/* Read in more data. */
 		YY_INPUT( (&YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[number_to_move]),
-			(yy_n_chars), num_to_read );
+			(yy_n_chars), (size_t) num_to_read );
 
 		YY_CURRENT_BUFFER_LVALUE->yy_n_chars = (yy_n_chars);
 		}
@@ -2986,7 +2990,7 @@ static int yy_get_next_buffer (void)
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
 	yy_is_jam = (yy_current_state == 1234);
 
-		return yy_is_jam ? 0 : yy_current_state;
+	return yy_is_jam ? 0 : yy_current_state;
 }
 
     static void yyunput (int c, register char * yy_bp )
@@ -3001,7 +3005,7 @@ static int yy_get_next_buffer (void)
 	if ( yy_cp < YY_CURRENT_BUFFER_LVALUE->yy_ch_buf + 2 )
 		{ /* need to shift things up to make room */
 		/* +2 for EOB chars. */
-		register yy_size_t number_to_move = (yy_n_chars) + 2;
+		register int number_to_move = (yy_n_chars) + 2;
 		register char *dest = &YY_CURRENT_BUFFER_LVALUE->yy_ch_buf[
 					YY_CURRENT_BUFFER_LVALUE->yy_buf_size + 2];
 		register char *source =
@@ -3054,7 +3058,7 @@ static int yy_get_next_buffer (void)
 
 		else
 			{ /* need more input */
-			yy_size_t offset = (yy_c_buf_p) - (yytext_ptr);
+			int offset = (yy_c_buf_p) - (yytext_ptr);
 			++(yy_c_buf_p);
 
 			switch ( yy_get_next_buffer(  ) )
@@ -3331,7 +3335,7 @@ void scdocpop_buffer_state (void)
  */
 static void scdocensure_buffer_stack (void)
 {
-	yy_size_t num_to_alloc;
+	int num_to_alloc;
     
 	if (!(yy_buffer_stack)) {
 
@@ -3428,12 +3432,12 @@ YY_BUFFER_STATE scdoc_scan_string (yyconst char * yystr )
  * 
  * @return the newly allocated buffer state object.
  */
-YY_BUFFER_STATE scdoc_scan_bytes  (yyconst char * yybytes, yy_size_t  _yybytes_len )
+YY_BUFFER_STATE scdoc_scan_bytes  (yyconst char * yybytes, int  _yybytes_len )
 {
 	YY_BUFFER_STATE b;
 	char *buf;
 	yy_size_t n;
-	yy_size_t i;
+	int i;
     
 	/* Get memory for full buffer, including space for trailing EOB's. */
 	n = _yybytes_len + 2;
@@ -3515,7 +3519,7 @@ FILE *scdocget_out  (void)
 /** Get the length of the current token.
  * 
  */
-yy_size_t scdocget_leng  (void)
+int scdocget_leng  (void)
 {
         return scdocleng;
 }
