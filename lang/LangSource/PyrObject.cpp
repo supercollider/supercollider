31aecdf8cfa5279149cf59e0ab673d1c25650c98
Tim Blechmann <tim@klingt.org>
2016-02-01 03:11:43 +0800

    sclang: mathod table sorting - fix heuristic to compute thread count and work distribution

--- lang/LangSource/PyrObject.cpp
+++ lang/LangSource/PyrObject.cpp
@@ -1186,8 +1186,10 @@ void buildBigMethodMatrix()
 	double t0 = elapsedTime();
 #endif
 
-	const int cpuCount = thread::hardware_concurrency();
-	boost::basic_thread_pool pool( std::min( cpuCount - 1, 1 ) );
+	const int hw_concurrency    = thread::hardware_concurrency();
+	const int cpuCount          = hw_concurrency > 0 ? hw_concurrency    : 1;
+	const int helperThreadCount = cpuCount       > 1 ? cpuCount - 1      : 1;
+	boost::basic_thread_pool pool( helperThreadCount );
 
 	// pyrmalloc:
 	// lifetime: kill after compile
