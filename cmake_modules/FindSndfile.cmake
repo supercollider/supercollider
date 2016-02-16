7d4c600b11da42ff3bc0bd86f9f1985dd40a80b0
bagong <rs@bagong.de>
2016-02-16 23:06:57 +0100

    Correct wrongly copied findSNDFILE

--- cmake_modules/FindSndfile.cmake
+++ cmake_modules/FindSndfile.cmake
@@ -1,80 +1,75 @@
-if(NOT READLINE_INCLUDE_DIR OR NOT READLINE_LIBRARY)
-    find_path(READLINE_INCLUDE_DIR readline/readline.h)
-    find_library(READLINE_LIBRARY NAMES readline)
+# - Find sndfile
+# Find the native sndfile includes and libraries
+#
+#  SNDFILE_INCLUDE_DIR - where to find sndfile.h, etc.
+#  SNDFILE_LIBRARIES   - List of libraries when using libsndfile.
+#  SNDFILE_FOUND       - True if libsndfile found.
+
+if(NO_LIBSNDFILE)
+  #if(NOT SNDFILE_FIND_QUIETLY)
+  #  message(STATUS "FindSndfile: sndfile deactivated (NO_LIBSNDFILE)")
+  #endif()
+  set(SNDFILE_FOUND False)
+  set(SNDFILE_INCLUDE_DIR "nowhere")  # for onceonly check above
+  set(SNDFILE_LIBRARIES "")
+  add_definitions("-DNO_LIBSNDFILE")
+elseif (SNDFILE_INCLUDE_DIR AND SNDFILE_LIBRARY)
+  set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
+  set(SNDFILE_FOUND TRUE)
+elseif (APPLE)
+  set(SNDFILE_FOUND TRUE)
+  set(SNDFILE_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../external_libraries/libsndfile/)
+  set(SNDFILE_LIBRARIES ${CMAKE_CURRENT_LIST_DIR}/../platform/mac/lib/scUBlibsndfile.a)
+  add_definitions("-isystem ${CMAKE_CURRENT_LIST_DIR}/../external_libraries/libsndfile")
+
+  # TODO on non-apple platforms, we need to be able to test for >=1018.
+  # (On apple it is known true, because we bundle a later version.)
+  add_definitions("-DLIBSNDFILE_1018")
+
+else()
+  find_path(SNDFILE_INCLUDE_DIR sndfile.h
+    PATHS /usr/local/include
+      /usr/include
+      /${MINGW_ARCH}/include
+      $ENV{WD}/../../${MINGW_ARCH}/include
+      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/include"
+      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/include"
+      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/include"
+  )
+  find_library(SNDFILE_LIBRARY
+    NAMES sndfile sndfile-1 libsndfile libsndfile-1
+    PATHS /usr/local/
+      /usr/lib
+      /${MINGW_ARCH}/bin
+      $ENV{WD}/../../${MINGW_ARCH}/bin
+      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/bin"
+      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/lib"
+      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/bin"
+      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/lib"
+      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/bin"
+      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/lib"
+  )
+  # used for Windows only
+  find_path(SNDFILE_LIBRARY_DIR
+    NAMES sndfile.dll sndfile-1.dll libsndfile.dll libsndfile-1.dll
+    PATHS /${MINGW_ARCH}/bin
+      $ENV{WD}/../../${MINGW_ARCH}/bin
+      "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/libsndfile/bin"
+      "$ENV{ProgramW6432}/Mega-Nerd/libsndfile/bin"
+      "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/bin"
+  )
+
+  # Handle the QUIETLY and REQUIRED arguments and set SNDFILE_FOUND to TRUE if
+  # all listed variables are TRUE.
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Sndfile DEFAULT_MSG
+    SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
+
+  if(SNDFILE_FOUND)
+    set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
+  else(SNDFILE_FOUND)
+    set(SNDFILE_LIBRARIES)
+  endif(SNDFILE_FOUND)
+
 endif()
-
-if(APPLE)
-    # look in homebrew paths
-    execute_process(COMMAND brew --prefix readline OUTPUT_VARIABLE READLINE_BREW_PREFIX)
-    if (READLINE_BREW_PREFIX)
-        string(STRIP ${READLINE_BREW_PREFIX} READLINE_BREW_PREFIX)
-        message(STATUS "Found a homebrew install of readline ${READLINE_BREW_PREFIX}")
-        set(READLINE_INCLUDE_DIR ${READLINE_BREW_PREFIX}/include)
-        set(READLINE_LIBRARY ${READLINE_BREW_PREFIX}/lib/libreadline.dylib)
-    endif()
-endif()
-
-if(WIN32)
-    find_path(READLINE_INCLUDE_DIR
-        NAMES readline/readline.h
-        PATHS /${MINGW_ARCH}/include
-              "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/readline/include"
-              $ENV{WD}/../../${MINGW_ARCH}/include
-    )
-    find_library(READLINE_LIBRARY
-        NAMES libreadline6 readline5 libreadline readline
-        PATHS /${MINGW_ARCH}/bin
-              $ENV{WD}/../../${MINGW_ARCH}/bin
-              "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/readline/bin"
-              "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/readline/lib"
-    )
-    find_path(READLINE_LIBRARY_DIR
-        NAMES libreadline6.dll readline5.dll libreadline.dll
-        PATHS /${MINGW_ARCH}/bin
-              $ENV{WD}/../../${MINGW_ARCH}/bin
-              "${CMAKE_SOURCE_DIR}/../${CMAKE_LIBRARY_ARCHITECTURE}/readline/bin"
-    )
-endif()
-
-if (READLINE_INCLUDE_DIR AND READLINE_LIBRARY)
-    set(READLINE_FOUND TRUE)
-endif()
-
-if (READLINE_INCLUDE_DIR AND EXISTS "${READLINE_INCLUDE_DIR}/readline/readline.h")
-  file(STRINGS "${READLINE_INCLUDE_DIR}/readline/readline.h"
-               READLINE_MAJOR_VERSION
-       REGEX "^#define RL_VERSION_MAJOR.*$")
-  file(STRINGS "${READLINE_INCLUDE_DIR}/readline/readline.h"
-               READLINE_MINOR_VERSION
-       REGEX "^#define RL_VERSION_MINOR.*$")
-
-  string(REGEX REPLACE "^.*RL_VERSION_MAJOR.*([0-9]+).*$"
-                       "\\1"
-                       READLINE_MAJOR_VERSION
-                       "${READLINE_MAJOR_VERSION}")
-  string(REGEX REPLACE "^.*RL_VERSION_MINOR.*([0-9]+).*$"
-                       "\\1"
-                       READLINE_MINOR_VERSION
-                       "${READLINE_MINOR_VERSION}")
-
-  if(READLINE_MAJOR_VERSION)
-    set(READLINE_VERSION "${READLINE_MAJOR_VERSION}.${READLINE_MINOR_VERSION}")
-  else()
-    set(READLINE_VERSION "(unknown)")
-  endif()
-endif ()
-
-if (READLINE_FOUND)
-  if (Readline_FIND_VERSION VERSION_GREATER READLINE_VERSION)
-    message(STATUS "Found version ${READLINE_VERSION} of GNU Readline at ${READLINE_LIBRARY}, but version ${Readline_FIND_VERSION} required")
-    unset(READLINE_FOUND)
-  else()
-    if (NOT READLINE_FIND_QUIETLY)
-      message(STATUS "Found GNU Readline version ${READLINE_VERSION}: ${READLINE_LIBRARY}")
-    endif ()
-  endif ()
-else ()
-  if (READLINE_FIND_REQUIRED)
-    message(FATAL_ERROR "Could not find GNU Readline")
-  endif ()
-endif ()
+mark_as_advanced(SNDFILE_INCLUDE_DIR SNDFILE_LIBRARY)
