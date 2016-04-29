e80c15cf81395e319e1f69750aff0be90f126874
Luis Lloret <luislloret@gmail.com>
2016-04-29 08:53:02 +0100

    removed diagnostics output

--- lang/LangSource/SC_TerminalClient.cpp
+++ lang/LangSource/SC_TerminalClient.cpp
@@ -528,13 +528,11 @@ void SC_TerminalClient::startInputRead()
 		else {
 			DWORD bytes_transferred;
 
-			postfl("Going to ReadFile()\n");
 			::ReadFile(GetStdHandle(STD_INPUT_HANDLE),
 					   inputBuffer.data(),
 					   inputBuffer.size(),
 					   &bytes_transferred,
 					   nullptr);
-			postfl("Done ReadFile()\n");
 			onInputRead(error, bytes_transferred);
 		}
 	});
@@ -633,13 +631,10 @@ void SC_TerminalClient::startInput()
 
 void SC_TerminalClient::endInput()
 {
-	postfl("endInput()\n");
 #ifdef _WIN32
 	::CancelIoEx(GetStdHandle(STD_INPUT_HANDLE), nullptr);
 #endif
-	postfl("endInput() 2\n");
 	mInputService.stop();
-	postfl("endInput() 3\n");
 	mStdIn.cancel();
 	postfl("main: waiting for input thread to join...\n");
 	mInputThread.join();
