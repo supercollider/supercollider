73cc72ee8b160ef3eecd658bd2e36c51ae7bcc24
Dan Stowell <danstowell@users.sourceforge.net>
2016-06-10 23:51:15 +0100

    Fix oscpack build fail on various architectures
    (amd64, i386, arm64, powerpc and ppc64el...)
    
    Upstreaming a debian patch authored by James Cowgill.
    See https://bugs.debian.org/824581 for information.
    The patch was new in debian supercollider package version 1:3.7.0_repack-3

--- external_libraries/oscpack_1_1_0/osc/OscTypes.h
+++ external_libraries/oscpack_1_1_0/osc/OscTypes.h
@@ -37,42 +37,17 @@
 #ifndef INCLUDED_OSCPACK_OSCTYPES_H
 #define INCLUDED_OSCPACK_OSCTYPES_H
 
+#include <stdint.h>
 
 namespace osc{
 
 // basic types
 
-#if defined(__BORLANDC__) || defined(_MSC_VER)
+typedef int64_t int64;
+typedef uint64_t uint64;
 
-typedef __int64 int64;
-typedef unsigned __int64 uint64;
-
-#elif defined(__x86_64__) || defined(_M_X64)
-
-typedef long int64;
-typedef unsigned long uint64;
-
-#else
-
-typedef long long int64;
-typedef unsigned long long uint64;
-
-#endif
-
-
-
-#if defined(__x86_64__) || defined(_M_X64)
-
-typedef signed int int32;
-typedef unsigned int uint32;
-
-#else
-
-typedef signed long int32;
-typedef unsigned long uint32;
-
-#endif
-
+typedef int32_t int32;
+typedef uint32_t uint32;
 
 enum ValueTypeSizes{
     OSC_SIZEOF_INT32 = 4,
