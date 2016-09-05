83586be6c091c2fc5b8e41a3de8832f22e5c0795
Yvan Volochine <yvan.volochine@gmail.com>
2016-09-05 15:02:00 +0200

    travis: test.sh - fix typo
    Signed-off-by: Yvan Volochine <yvan.volochine@gmail.com>

--- .travis/test.sh
+++ .travis/test.sh
@@ -6,7 +6,7 @@ else
 	export SCLANG=$TRAVIS_BUILD_DIR/BUILD/Install/SuperCollider/SuperCollider.app/Contents/MacOS/sclang
 fi
 
-if [[ $QT != true ]]; then ./fix-non-Qt.sh; fi
+if [[ $QT != true ]]; then $TRAVIS_BUILD_DIR/.travis/fix-non-Qt.sh; fi
 
 $TRAVIS_BUILD_DIR/testsuite/sclang/launch_test.py $SCLANG
 
