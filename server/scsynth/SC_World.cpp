316550ea1c70887abd00407a120b9e3dc2ff04ce
Giulio Moro <giuliomoro@yahoo.it>
2016-10-08 17:26:58 +0000

    Added sc_setDenormalFlags for systems with vfp (vector floating point) unit.

--- server/scsynth/SC_World.cpp
+++ server/scsynth/SC_World.cpp
@@ -181,6 +181,29 @@ void sc_SetDenormalFlags()
 	_MM_SET_FLUSH_ZERO_MODE(_MM_FLUSH_ZERO_ON);
 }
 
+#elif defined(__VFP_FP__)
+// vfp is the IEE-754-compliant fp unit on
+// the Cortex A8, along with the SIMD Neon unit
+// these function turns on "fast mode" by enabling
+// flushing denormals to zero on the VFP.
+void sc_SetDenormalFlags(){
+/* This code is from math-neon/math_runfast.c
+Copyright (c) 2015 Lachlan Tychsen-Smith (lachlan.ts@gmail.com)
+MIT License (MIT)
+ */
+	static const unsigned int x = 0x04086060;
+	static const unsigned int y = 0x03000000;
+	int r;
+	asm volatile (
+		"fmrx	%0, fpscr			\n\t"	//r0 = FPSCR
+		"and	%0, %0, %1			\n\t"	//r0 = r0 & 0x04086060
+		"orr	%0, %0, %2			\n\t"	//r0 = r0 | 0x03000000
+		"fmxr	fpscr, %0			\n\t"	//FPSCR = r0
+		: "=r"(r)
+		: "r"(x), "r"(y)
+	);
+}
+
 #else
 
 void sc_SetDenormalFlags()
