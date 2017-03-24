9aea83696adf3b3e5de9d0e85564ca984dec6b70
scztt <scott@artificia.org>
2017-03-23 21:07:17 -0700

    sclang: Default initialization.

--- lang/LangPrimSource/OSCData.cpp
+++ lang/LangPrimSource/OSCData.cpp
@@ -409,7 +409,7 @@ static int prNetAddr_Connect(VMGlobals *g, int numArgsPushed)
 
 static int prNetAddr_Disconnect(VMGlobals *g, int numArgsPushed)
 {
-	int err;
+	int err = errFailed;
 
 	PyrSlot* netAddrSlot = g->sp;
 	PyrObject* netAddrObj = slotRawObject(netAddrSlot);
