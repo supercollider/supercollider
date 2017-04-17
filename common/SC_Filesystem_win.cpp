ecce63dd52f1aec3170fefe15254e0e53b099e6d
Brian Heim <brianlheim@gmail.com>
2017-04-17 10:35:29 -0400

    SC_FS: add win source
    - Uses GetKnownFolderPath instead of GetFolderPath (see MSDN ref)
    - Copies glob code verbatim, will want to change later

--- common/SC_Filesystem_win.cpp
+++ common/SC_Filesystem_win.cpp
@@ -27,4 +27,250 @@
  */
 #ifdef _WIN32
 
+#include "SC_Filesystem.hpp"
+
+// stdlib
+#include <string>
+
+#define DEBUG_SCFS
+#ifdef DEBUG_SCFS
+#include <iostream>
+using std::cout;
+using std::endl;
+#endif
+
+// boost
+#include <boost/algorithm/string/predicate.hpp> // iequals
+
+// system includes
+#include <Shlobj.h> // SHGetKnownFolderPath
+
+using Path = SC_Filesystem::Path;
+using DirName = SC_Filesystem::DirName;
+using DirMap = SC_Filesystem::DirMap;
+
+//============ DIRECTORY NAMES =============//
+const char* gIdeName = "none"; // @TODO: move out
+const char* SUPERCOLLIDER_DIR_NAME = "SuperCollider";
+const char* EXTENSIONS_DIR_NAME = "Extensions";
+
+//============ PATH UTILITIES =============//
+
+Path SC_Filesystem::getDirectory(const DirName& dn)
+{
+#ifdef DEBUG_SCFS
+	cout << "SCFS::getDirectory: enter" << endl;
+#endif
+	const DirMap::const_iterator& it = mDirectoryMap.find(dn);
+	Path p;
+	if (it != mDirectoryMap.end()) {
+		p = it->second;
+	} else {
+		p = initDirectory(dn) ? mDirectoryMap.find(dn)->second : Path();
+	}
+#ifdef DEBUG_SCFS
+	cout << "\tgot " << p << endl;
+	cout << "SCFS::getDirectory: exit" << endl;
+#endif
+	return p;
+}
+
+Path SC_Filesystem::expandTilde(const Path& p)
+{
+	Path::const_iterator piter = p.begin();
+
+	if (piter != p.end() && *piter == "~") {
+		Path ret = getDirectory(DirName::UserHome);
+		while (++piter != p.end())
+			ret /= *piter;
+		return ret;
+	} else {
+		return p;
+	}
+}
+
+bool SC_Filesystem::shouldNotCompileDirectory(const Path& p) const
+{
+	const std::string& dirname = p.filename().string();
+	const std::string& idePath = std::string("scide_") + gIdeName;
+	return (boost::iequals(dirname, "help") ||
+			boost::iequals(dirname, "ignore") ||
+			dirname == ".svn" ||
+			dirname == ".git" ||
+			dirname == "_darcs" ||
+			dirname == idePath ||
+			isNonHostPlatformDirectory(dirname));
+}
+
+bool SC_Filesystem::isStandalone() { return false; }
+
+Path SC_Filesystem::resolveIfAlias(const Path& p, bool& isAlias) { isAlias = true; return p; }
+
+// Returns false if initialization failed
+bool SC_Filesystem::initDirectory(const DirName& dn)
+{
+#ifdef DEBUG_SCFS
+	cout << "SCFS::initDirectory: enter" << endl;
+	cout << "\tdn: " << (int)dn << endl;
+#endif
+	Path p;
+
+	switch (dn) {
+		case DirName::SystemAppSupport:
+			p = defaultSystemAppSupportDirectory();
+			break;
+
+		case DirName::SystemExtension:
+			p = defaultSystemExtensionDirectory();
+			break;
+
+		case DirName::UserHome:
+			p = defaultUserHomeDirectory();
+			break;
+
+		case DirName::UserAppSupport:
+			p = defaultUserAppSupportDirectory();
+			break;
+
+		case DirName::UserExtension:
+			p = defaultUserExtensionDirectory();
+			break;
+
+		case DirName::UserConfig:
+			p = defaultUserConfigDirectory();
+			break;
+
+		case DirName::Resource:
+			p = defaultResourceDirectory();
+			break;
+
+		default:
+#ifdef DEBUG_SCFS
+			cout << "\tdefault case" << endl;
+#endif
+			break;
+	}
+
+	if (!p.empty()) {
+#ifdef DEBUG_SCFS
+		cout << "\tsuccess: " << p << endl;
+#endif
+		mDirectoryMap[dn] = p;
+	} else {
+#ifdef DEBUG_SCFS
+		cout << "\tinit failed" << endl;
+#endif
+	}
+
+#ifdef DEBUG_SCFS
+	cout << "SCFS::initDirectory: exit" << endl;
+#endif
+	return !p.empty();
+}
+
+//============ GLOB UTILITIES =============//
+
+// @TODO: replace chars w paths
+struct SC_Filesystem::Glob
+{
+	HANDLE mHandle;
+	char mFolder[MAX_PATH];
+	WIN32_FIND_DATA mEntry;
+	char mEntryPath[MAX_PATH];
+	bool mAtEnd;
+};
+
+// @TODO: fix up
+SC_Filesystem::Glob* SC_Filesystem::makeGlob(const char* pattern)
+{
+	Glob* glob = new Glob;
+	char patternWin[1024];
+
+	strncpy(patternWin, pattern, 1024);
+	patternWin[1023] = 0;
+	win32_ReplaceCharInString(patternWin, 1024, '/', '\\'); // TODO: replace
+
+	win32_ExtractContainingFolder(glob->mFolder, patternWin, PATH_MAX); // TODO: replace
+
+	glob->mHandle = ::FindFirstFile(patternWin, &glob->mEntry);
+	if (glob->mHandle == INVALID_HANDLE_VALUE) {
+		delete glob;
+		return 0;
+	}
+
+	glob->mAtEnd = false;
+	return glob;
+}
+
+void SC_Filesystem::freeGlob(Glob* glob)
+{
+	::FindClose(glob->mHandle);
+	delete glob;
+}
+
+// @TODO: fix up
+Path SC_Filesystem::globNext(Glob* glob)
+{
+	if (glob->mAtEnd)
+		return Path();
+	strncpy(glob->mEntryPath, glob->mFolder, PATH_MAX);
+	glob->mEntryPath = (Path(glob->mEntryPath) / glob->mEntry.cFileName).c_str();
+	if (!::FindNextFile(glob->mHandle, &glob->mEntry))
+		glob->mAtEnd = true;
+	return Path(glob->mEntryPath);
+}
+
+//============= PRIVATE METHODS ==============//
+
+bool SC_Filesystem::isNonHostPlatformDirectory(const std::string& s)
+{
+	return s == "linux" || s == "osx" || s == "iphone";
+}
+
+Path SC_Filesystem::defaultSystemAppSupportDirectory()
+{
+	wchar_t* wptr;
+	const HRESULT hr = SHGetKnownFolderPath(FOLDERID_RoamingAppData, 0, nullptr, wptr);
+	return FAILED(hr) ? Path() : Path(wptr) / SUPERCOLLIDER_DIR_NAME;
+}
+
+Path SC_Filesystem::defaultSystemExtensionDirectory()
+{
+	const Path& p = defaultSystemAppSupportDirectory();
+	return p.empty() ? p : p / EXTENSIONS_DIR_NAME;
+}
+
+Path SC_Filesystem::defaultUserHomeDirectory()
+{
+	wchar_t* wptr;
+	const HRESULT hr = SHGetKnownFolderPath(FOLDERID_Profile, 0, nullptr, wptr);
+	return FAILED(hr) ? Path() : Path(wptr);
+}
+
+Path SC_Filesystem::defaultUserAppSupportDirectory()
+{
+	wchar_t* wptr;
+	const HRESULT hr = SHGetKnownFolderPath(FOLDERID_LocalAppData, 0, nullptr, wptr);
+	return FAILED(hr) ? Path() : Path(wptr) / SUPERCOLLIDER_DIR_NAME;
+}
+
+Path SC_Filesystem::defaultUserExtensionDirectory()
+{
+	const Path& p = defaultUserAppSupportDirectory();
+	return p.empty() ? p : p / EXTENSIONS_DIR_NAME;
+}
+
+Path SC_Filesystem::defaultUserConfigDirectory()
+{
+	return defaultUserAppSupportDirectory();
+}
+
+Path SC_Filesystem::defaultResourceDirectory()
+{
+	wchar_t buf[MAX_PATH];
+	// @TODO: error check? (none in original)
+	GetModuleFileName(nullptr, buf, MAX_PATH);
+	return Path(buf).parent_path();
+}
+
 #endif // _WIN32
