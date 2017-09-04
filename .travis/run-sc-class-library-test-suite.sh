8255c8a4475171b30f42d5ad85f0589a1eb44f7e
Brian Heim <brianlheim@gmail.com>
2017-09-04 14:13:05 -0400

    travis test script: use environment variables to pass configuration info
    Also add more echos for clarity

--- .travis/run-sc-class-library-test-suite.sh
+++ .travis/run-sc-class-library-test-suite.sh
@@ -5,16 +5,20 @@
 # as are tests for the UnitTesting Quark itself.
 
 echo "Attempting to configure test environment."
-if "$SCLANG" -i none "$TRAVIS_BUILD_DIR/.travis/configureTestEnvironment.scd"
+export SCLANG_CONF="$TRAVIS_BUILD_DIR/.travis/sclang_conf.yaml"
+"$SCLANG" -i none "$TRAVIS_BUILD_DIR/.travis/configureTestEnvironment.scd"
+
+echo "Testing for existence of $SCLANG_CONF"
+if [[ -e "$SCLANG_CONF" ]]
 then
 	echo "Configuration complete. Attempting to run unit tests now."
-	SCLANG_CONF="$TRAVIS_BUILD_DIR/.travis/sclang_conf.yaml"
 	echo "sclang configuration: "
 	cat "$SCLANG_CONF"
+	echo ""
 	"$SCLANG" -i none -l "$SCLANG_CONF" "$TRAVIS_BUILD_DIR/.travis/runAllUnitTests.scd"
 	exit $?
 else
-	echo "Error while configuring test environment."
+	echo "ERROR: $SCLANG_CONF not found. Test environment configuration failed."
 	exit 1
 fi
 
