50250a6ffe8f35ddffa983aee927e10ea7dc3a1f
Herman Torjussen <herman.torjussen@gmail.com>
2018-08-12 11:02:01 +0200

    docs: improve syntax highlighter

--- HelpSource/editor.js
+++ HelpSource/editor.js
@@ -1,24 +1,30 @@
 const init = () => {
     CodeMirror.defineSimpleMode('scd', {
         start: [
-            {regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: 'string'},
-            {regex: /^[\t\n\r \xA0]+/, token: 'whitespace'},
-            {regex: /^\\\w+/, token: 'attribute-name'},
-            {regex: /^'[^']+'/, token: 'attribute-name'},
-            {regex: /^\w+\:/, token: 'attribute-name'},
-            {regex: /^\$(\\)?./, token: 'attribute-name'},
-            {regex: /^~\w+/, token: 'env-var'},
-            {regex: /^(?:super|thisFunctionDef|thisFunction|thisMethod|thisProcess|thisThread|this)\b/, token: 'keyword'},
-            {regex: /^(?:true|false|nil|inf)\b/, token: 'atom'},
-            {regex: /^(?:var|classvar|const|arg)\b/, token: 'declaration'},
-            {regex: /^\b([A-Z][A-Za-z_0-9]+)\b/, token: 'type'},
-            {regex: /^\/(?:\/.*|\*(?:\/|\**[^*/])*(?:\*+\/?)?)/, token: 'comment'},
-            {regex: /^-?\d+r[\da-zA-Z]+(\.[\da-zA-Z]+)?/, token: 'literal'},
-            {regex: /^-?(?:(?:\d+(\.\d+)?)(?:e[+\-]?\d+)?(pi)?)|(?:pi\b)/, token: 'literal'},
-            {regex: /^[a-z_]\w*/i, token: 'plain'},
-            {regex: /^[-.,;#()\[\]{}]/, token: 'punctuation'}
+            { regex: /^\s+/, token: 'whitespace' },
+            { regex: /^(?:arg|classvar|const|super|this|var)\b/, token: 'keyword' },
+            { regex: /^(?:false|inf|nil|true|thisFunction|thisFunctionDef|thisMethod|thisProcess|thisThread|currentEnvironment|topEnvironment)\b/, token: 'built-in' },
+            { regex: /^\b\d+r[0-9a-zA-Z]*(\.[0-9A-Z]*)?/, token: 'number radix-float' },
+            { regex: /^\b((\d+(\.\d+)?([eE][-+]?\d+)?(pi)?)|pi)\b/, token: 'number float' },
+            { regex: /^\b0(x|X)(\d|[a-f]|[A-F])+/, token: 'number hex-int' },
+            { regex: /^\b[A-Za-z_]\w*\:/, token: 'symbol symbol-arg' },
+            { regex: /^[a-z]\w*/, token: 'text name' },
+            { regex: /^\b[A-Z]\w*/, token: 'class' },
+            { regex: /^\b_\w+/, token: 'primitive' },
+            { regex: /^\\\w*/, token: 'symbol' },
+            { regex: /^\$\\\?./, token: 'char' },
+            { regex: /^~\w+/, token: 'env-var' },
+            { regex: /^\/\/[^\r\n]*/, token: 'comment single-line-comment' },
+            { regex: /"(?:[^\\]|\\.)*?(?:"|$)/, token: 'string' },
+            { regex: /^[-.,;#()\[\]{}]/, token: 'text punctuation' },
+            { regex: /\/\*/, push: 'comment', token: 'comment multi-line-comment' },
+            { regex: /^[\+-\\*/&\\|\\^%<>=]+/, token: 'text operator' },
+        ],
+        comment: [
+            { regex: /\*\//, pop: true, token: 'comment multi-line-comment' },
+            { regex: /./, token: 'comment multi-line-comment' }
         ]
-    });
+    })
 
     let textareas = Array.from(document.querySelectorAll('textarea'))
     textareas.forEach(textarea => {
