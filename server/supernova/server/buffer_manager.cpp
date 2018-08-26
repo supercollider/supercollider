05cccc96027e886299e89d41802e775788825f88
sonoro1234 <sonoro@telefonica.net>
2018-08-26 10:08:48 +0200

    comment bad include to sndfile to let windows compile supernova

--- server/supernova/server/buffer_manager.cpp
+++ server/supernova/server/buffer_manager.cpp
@@ -16,7 +16,7 @@
 //  the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
 //  Boston, MA 02111-1307, USA.
 
-#include "sndfile.hh"
+//#include "sndfile.hh"
 
 #include "buffer_manager.hpp"
 #include "malloc_aligned.hpp"
