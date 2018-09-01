ab10444f7d53ac82cc7c033542d62fde44ae9a11
Brian Heim <brianlheim@gmail.com>
2018-08-31 21:34:27 -0500

    cmake_gen: IN (typo)

--- tools/cmake_gen/SuperColliderServerPlugin.cmake
+++ tools/cmake_gen/SuperColliderServerPlugin.cmake
@@ -12,7 +12,7 @@ function(sc_check_sc_path path)
     get_filename_component(full_path "${path}" ABSOLUTE BASE_DIR "${CMAKE_SOURCE_DIR}")
 
     # check main paths
-    if(NOT EXISTS "${full_path}/include/plugin_interface/SC_Plugin.h")
+    if(NOT EXISTS "${full_path}/include/plugin_interface/SC_PlugIn.h")
         set(msg_end "\nPlease set SC_PATH to the root folder of the SuperCollider project relative to the folder containing this CMakeLists.txt file")
         message(FATAL_ERROR "Could not find SuperCollider3 headers at '${full_path}'.${msg_end}")
     endif()
