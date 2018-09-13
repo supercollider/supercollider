dea45dedb38acb122e5f95aa7a5e2a223ae9d79e
Brian Heim <brianlheim@gmail.com>
2018-09-12 20:58:17 -0500

    travis: add no-webengine builds

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-cmake -G"Xcode" -DRULE_LAUNCH_COMPILE=ccache -DCMAKE_PREFIX_PATH=`brew --prefix qt5` -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 $TRAVIS_BUILD_DIR --debug-output
+cmake -G"Xcode" -DRULE_LAUNCH_COMPILE=ccache \
+    -DCMAKE_PREFIX_PATH=`brew --prefix qt5` \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 \
+    -DSC_USE_WEBENGINE=$SC_USE_WEBENGINE \
+    $TRAVIS_BUILD_DIR \
+    --debug-output
