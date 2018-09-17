f9400ed814bcaba96ca769bc5f45081e4510a8ba
Brian Heim <brianlheim@gmail.com>
2018-09-16 20:32:41 -0500

    travis: use 10.10 in builds
    first system supported by QtWebEngine

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -3,6 +3,6 @@
 cmake -G"Xcode" \
     -DRULE_LAUNCH_COMPILE=ccache \
     -DCMAKE_PREFIX_PATH=`brew --prefix qt5` \
-    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.8 \
+    -DCMAKE_OSX_DEPLOYMENT_TARGET=10.10 \
     -DSUPERNOVA=ON \
     $TRAVIS_BUILD_DIR --debug-output
