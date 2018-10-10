867d2de325f1e6bd34eb3212c3915650840b4f70
Brian Heim <brianlheim@gmail.com>
2018-10-09 21:19:10 -0500

    travis: add job for qt minus webengine

--- .travis/before-script-linux.sh
+++ .travis/before-script-linux.sh
@@ -2,8 +2,25 @@
 
 $TRAVIS_BUILD_DIR/.travis/lint.sh $TRAVIS_BUILD_DIR
 if [[ -n "$1" && "$1" == "--qt=true" ]]; then
-	source /opt/qt59/bin/qt59-env.sh
-	cmake -DSC_EL=OFF -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/BUILD/Install -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR --debug-output
+    source /opt/qt59/bin/qt59-env.sh
+    if [[ -n "$2" && "$2" == "--webengine=false" ]]; then
+        SC_USE_WEBENGINE=OFF
+    elif [[ -n "$2" && "$2" == "--webengine=true" ]]; then
+        SC_USE_WEBENGINE=ON
+    fi
+
+    cmake \
+        -DSC_EL=OFF \
+        -DSC_USE_WEBENGINE=$SC_USE_WEBENGINE \
+        -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/BUILD/Install \
+        -DCMAKE_BUILD_TYPE=Release \
+        $TRAVIS_BUILD_DIR --debug-output
 else
-	cmake -DSC_EL=OFF -DSC_QT=OFF -DSC_IDE=OFF -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/BUILD/Install -DCMAKE_BUILD_TYPE=Release $TRAVIS_BUILD_DIR --debug-output
+    cmake \
+        -DSC_EL=OFF \
+        -DSC_QT=OFF \
+        -DSC_IDE=OFF \
+        -DCMAKE_INSTALL_PREFIX:PATH=$TRAVIS_BUILD_DIR/BUILD/Install \
+        -DCMAKE_BUILD_TYPE=Release \
+        $TRAVIS_BUILD_DIR --debug-output
 fi
