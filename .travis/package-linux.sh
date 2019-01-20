ae57cebd7c58bee908ad16e70a9780f98c23ec18
Nathan Ho <nathan@snappizz.com>
2019-01-19 17:26:43 -0800

    Null commit for CI [skip appveyor]

--- .travis/package-linux.sh
+++ .travis/package-linux.sh
@@ -5,4 +5,4 @@ mkdir -p $HOME/uploads
 cd $TRAVIS_BUILD_DIR/package
 ./create_package.sh -v $VERSION_TO_BUILD
 ./create_package.sh -v $VERSION_TO_BUILD -l
-mv -v SuperCollider-*.tar.bz2 $HOME/uploads/
+# mv -v SuperCollider-*.tar.bz2 $HOME/uploads/
