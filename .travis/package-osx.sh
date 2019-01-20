e60fc393c7ecd3d571459ae4dc964e2d118a4af7
Nathan Ho <nathan@snappizz.com>
2019-01-19 17:51:24 -0800

    Build source tarballs on macOS [skip appveyor]

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -6,3 +6,8 @@ zip -q -r --symlinks $HOME/artifacts/SC-$TRAVIS_COMMIT.zip SuperCollider
 
 mkdir -p $HOME/uploads
 cp $HOME/artifacts/SC-$TRAVIS_COMMIT.zip $HOME/uploads/SuperCollider-$VERSION_TO_BUILD-macOS.zip
+
+cd $TRAVIS_BUILD_DIR/package
+./create_package.sh -v $VERSION_TO_BUILD
+./create_package.sh -v $VERSION_TO_BUILD -l
+mv -v SuperCollider-*.tar.bz2 $HOME/uploads/
