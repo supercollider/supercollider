4f106f3e1c8f489a600850fe7e2a1b3498ee95e4
Luke Nihlen <luke.nihlen@gmail.com>
2019-05-17 12:30:34 +0100

    some tinkering with parsing, broken

--- lang/LangSource/ReflectParseNode.cpp
+++ lang/LangSource/ReflectParseNode.cpp
@@ -12,13 +12,13 @@ const char* nodeNameLabels[] = { "class",
                                  "slot",
                                  "variableList",
                                  "variableDefinition",
-                                 "dictionary",
-                                 "dictionaryList",
+                                 "dynamicDictionary",
+                                 "dynamicList",
                                  "literalList",
                                  "literalDictionary",
-                                 "staticVariableList",
-                                 "instanceVariableList",
-                                 "poolVariableList",
+                                 "staticVariableList", // does not actually seem to ever be created
+                                 "instanceVariableList", // does not actually seem to ever be created
+                                 "poolVariableList", // does not actually seem to ever be created
                                  "argumentList",
                                  "slotDefinition",
                                  "literal",
