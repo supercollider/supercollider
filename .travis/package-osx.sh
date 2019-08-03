59d68c9c1be69ab65ad78e2217fb9dc6c9826bd7
Joshua Parmenter <josh@realizedsound.net>
2019-08-03 15:26:33 -0700

    update for OS X codesigning in Travis builds

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -2,6 +2,9 @@
 
 mkdir -p $HOME/artifacts
 
-cd $TRAVIS_BUILD_DIR/BUILD/Install
+cd $TRAVIS_BUILD_DIR/BUILD/Install/SuperColider
+xattr -cr SuperCollider.app
+codesign --deep --force --verify --verbose --sign "Mac Developer: josh@realizedsound.net" SuperCollider.app
+cd ..
 zip -q -r --symlinks $HOME/artifacts/SC-$TRAVIS_COMMIT.zip SuperCollider
 cp $HOME/artifacts/SC-$TRAVIS_COMMIT.zip $HOME/SuperCollider-$VERSION_TO_BUILD-macOS.zip
