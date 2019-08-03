19750da0a9bc10146fb27055971be8e336372bf9
Joshua Parmenter <josh@realizedsound.net>
2019-08-03 16:52:17 -0700

    path update for before-script to find the certs folder

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -11,7 +11,7 @@ security default-keychain -s ios-build.keychain
 security unlock-keychain -p travis ios-build.keychain
 
 # Add certificates to keychain and allow codesign to access them
-security import ./certs/distribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+security import ../certs/distribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
 
 security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
 
