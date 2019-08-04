1b4e133d6c48089793d928fed35bdb4298e32def
Joshua Parmenter <josh@realizedsound.net>
2019-08-03 22:12:13 -0700

    Try code sign with a different certificate type

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -4,7 +4,7 @@ mkdir -p $HOME/artifacts
 
 cd $TRAVIS_BUILD_DIR/BUILD/Install/SuperCollider
 xattr -cr SuperCollider.app
-codesign --deep --force --verify --verbose --sign "Mac Developer: josh@realizedsound.net" SuperCollider.app
+codesign --deep --force --verify --verbose --sign "Developer ID Application: Joshua Parmenter" SuperCollider.app
 cd ..
 zip -q -r --symlinks $HOME/artifacts/SC-$TRAVIS_COMMIT.zip SuperCollider
 cp $HOME/artifacts/SC-$TRAVIS_COMMIT.zip $HOME/SuperCollider-$VERSION_TO_BUILD-macOS.zip
