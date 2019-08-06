06a095cf0ae499c97c15bc81115a7ce9eeed0f9a
Joshua Parmenter <josh@realizedsound.net>
2019-08-06 16:05:17 -0700

    feature: update osx travis scripts for signing

--- .travis/before-script-osx.sh
+++ .travis/before-script-osx.sh
@@ -1,5 +1,20 @@
 #!/bin/sh
 
+
+# Create the keychain with a password
+security create-keychain -p travis ios-build.keychain
+
+# Make the custom keychain default, so xcodebuild will use it for signing
+security default-keychain -s ios-build.keychain
+
+# Unlock the keychain
+security unlock-keychain -p travis ios-build.keychain
+
+# Add certificates to keychain and allow codesign to access them
+security import ../certs/distribution.p12 -k ~/Library/Keychains/ios-build.keychain -P $KEY_PASSWORD -T /usr/bin/codesign
+
+security set-key-partition-list -S apple-tool:,apple: -s -k travis ios-build.keychain
+
 cmake -G"Xcode" \
     -DRULE_LAUNCH_COMPILE=ccache \
     -DCMAKE_PREFIX_PATH=`brew --prefix qt5` \
--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -2,6 +2,9 @@
 
 mkdir -p $HOME/artifacts
 
-cd $TRAVIS_BUILD_DIR/BUILD/Install
+cd $TRAVIS_BUILD_DIR/BUILD/Install/SuperCollider
+xattr -cr SuperCollider.app
+codesign --deep --force --verify --verbose --sign "Developer ID Application: Joshua Parmenter" SuperCollider.app
+cd ..
 zip -q -r --symlinks $HOME/artifacts/SC-$TRAVIS_COMMIT.zip SuperCollider
 cp $HOME/artifacts/SC-$TRAVIS_COMMIT.zip $HOME/SuperCollider-$VERSION_TO_BUILD-macOS.zip
