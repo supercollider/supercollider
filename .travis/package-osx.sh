0ac425f2f4b1601996f261f26595268c5cb9ac83
Joshua Parmenter <josh@realizedsound.net>
2019-08-06 16:37:15 -0700

    feature: OS X sign - add echo statements for the packaging script to debug travis work

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -3,8 +3,13 @@
 mkdir -p $HOME/artifacts
 
 cd $TRAVIS_BUILD_DIR/BUILD/Install/SuperCollider
+echo "Removing Finder cruft..."
 xattr -cr SuperCollider.app
+echo "Attempting codesigning"
 codesign --deep --force --verify --verbose --sign "Developer ID Application: Joshua Parmenter" SuperCollider.app
 cd ..
+echo "Packaging..."
 zip -q -r --symlinks $HOME/artifacts/SC-$TRAVIS_COMMIT.zip SuperCollider
+echo "Copying build!"
 cp $HOME/artifacts/SC-$TRAVIS_COMMIT.zip $HOME/SuperCollider-$VERSION_TO_BUILD-macOS.zip
+echo "Done!"
