9fc622d0c823a19031c9d3dc734e88b2dbe58970
Joshua Parmenter <josh@realizedsound.net>
2019-08-10 14:37:05 -0700

    feature: OSX-CODESIGH attempt signing and upload of build to apple

--- .travis/package-osx.sh
+++ .travis/package-osx.sh
@@ -7,7 +7,21 @@ echo "Removing Finder cruft..."
 xattr -cr SuperCollider.app
 echo "Attempting codesigning"
 security set-key-partition-list -S apple-tool:,apple: -s -k $SC_KEYCHAIN_PASSWORD supercollider.keychain
-codesign --deep --force --verify --verbose --sign "Developer ID Application: Joshua Parmenter" SuperCollider.app
+codesign --deep --force --verify --verbose --timestamp --sign "Developer ID Application: Joshua Parmenter" --options runtime SuperCollider.app
+codesign --deep --force --verify --verbose --timestamp --sign "Developer ID Application: Joshua Parmenter" --options runtime SuperCollider.app/Contents/Resources/scsynth 
+codesign --deep --force --verify --verbose --timestamp --sign "Developer ID Application: Joshua Parmenter" --options runtime SuperCollider.app/Contents/Resources/plugins/*
+#MUST be last!
+codesign --deep --force --verify --verbose --timestamp --sign "Developer ID Application: Joshua Parmenter" --options runtime SuperCollider.app/Contents/MacOS/SuperCollider 
+
+ditto -c -k --rsrc --keepParent SuperCollider.app SuperCollider.app.zip
+
+#parse the output of this commmand for the UUID for this norarization:
+#The RequestUUID is what will be needed in the following commands
+#No errors uploading 'SuperCollider.app.zip'.
+#RequestUUID = 07c23ea0-7b88-4912-a120-4d1613684a78
+xcrun altool --notarize-app -t osx -f SuperCollider.app.zip --primary-bundle-id net.sourceforge.supercollider \
+ -u $APPLE_USER_NAME -p $APPLE_USER_PASSWORD --output-format json 
+
 cd ..
 echo "Packaging..."
 zip -q -r --symlinks $HOME/artifacts/SC-$TRAVIS_COMMIT.zip SuperCollider
