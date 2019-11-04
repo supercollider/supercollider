7f74f5daa13b1c8c277da73926dcb2bdfb5df526
Patrick Dupuis <p.dupuis@live.com>
2019-11-03 22:34:57 -0500

    keep including /home/patrick/Quarks dir

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $TRAVIS_BUILD_DIR/testsuite/classlibary
+qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks --include $TRAVIS_BUILD_DIR/testsuite/classlibary
