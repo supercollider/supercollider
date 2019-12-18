c50ea99feb90f6adca3629ebf8a0779dbb5a782e
Brian Heim <brianlheim@gmail.com>
2019-12-18 08:53:23 -0600

    Travis: use travis_retry and force correct installation of dependencies

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -2,11 +2,11 @@
 
 export HOMEBREW_NO_ANALYTICS=1
 
-brew install libsndfile || true
-brew install portaudio || true
-brew install ccache || true
-brew install qt5 || true
-brew link qt5 --force
+travis_retry brew install libsndfile || exit 1
+travis_retry brew install portaudio || exit 2
+travis_retry brew install ccache || exit 3
+travis_retry brew install qt5 || exit 4
+travis_retry brew link qt5 --force || exit 5
 
 # according to https://docs.travis-ci.com/user/caching#ccache-cache
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
