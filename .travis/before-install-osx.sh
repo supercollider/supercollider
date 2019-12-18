3fd27e3b2bb05e50ab00c000f573b634f69e167e
Brian Heim <brianlheim@gmail.com>
2019-12-18 09:00:35 -0600

    Travis: retry entire install step on osx

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -2,11 +2,11 @@
 
 export HOMEBREW_NO_ANALYTICS=1
 
-travis_retry brew install libsndfile || exit 1
-travis_retry brew install portaudio || exit 2
-travis_retry brew install ccache || exit 3
-travis_retry brew install qt5 || exit 4
-travis_retry brew link qt5 --force || exit 5
+brew install libsndfile || exit 1
+brew install portaudio || exit 2
+brew install ccache || exit 3
+brew install qt5 || exit 4
+brew link qt5 --force || exit 5
 
 # according to https://docs.travis-ci.com/user/caching#ccache-cache
 export PATH="/usr/local/opt/ccache/libexec:$PATH"
