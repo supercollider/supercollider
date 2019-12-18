cfb4b7856bb04ce3c5598beb7198118ac739ec55
Brian Heim <brianlheim@gmail.com>
2019-12-18 09:05:39 -0600

    Travis: just try installing libsndfile twice

--- .travis/before-install-osx.sh
+++ .travis/before-install-osx.sh
@@ -2,7 +2,7 @@
 
 export HOMEBREW_NO_ANALYTICS=1
 
-brew install libsndfile || exit 1
+brew install libsndfile || brew install libsndfile || exit 1
 brew install portaudio || exit 2
 brew install ccache || exit 3
 brew install qt5 || exit 4
