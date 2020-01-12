85cdcec35935e5f9416788decc2327ca13421f14
patrickdupuis <p.dupuis@live.com>
2020-01-11 20:16:43 -0500

    rm git clone ComonTests quark

--- .travis/qpm-prep.sh
+++ .travis/qpm-prep.sh
@@ -5,8 +5,6 @@ sudo pip2 install git+https://github.com/supercollider/qpm.git
 
 mkdir $HOME/Quarks && cd $HOME/Quarks
 git clone --depth=1 https://github.com/supercollider-quarks/API
-git clone --depth=1 https://github.com/supercollider-quarks/CommonTests
-git clone --depth=1 https://github.com/supercollider-quarks/CommonTestsGUI
 cd $TRAVIS_BUILD_DIR/BUILD
 
 cp ../travis_test_run_proto.json ./travis_test_run.json
