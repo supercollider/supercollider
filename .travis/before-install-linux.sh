186a7ec3b01efe3c3a66e20eaa64dd39213b4ee6
patrickdupuis <p.dupuis@live.com>
2020-01-11 20:21:41 -0500

    install JACK1

--- .travis/before-install-linux.sh
+++ .travis/before-install-linux.sh
@@ -21,7 +21,8 @@ npm install -g lintspaces-cli
 sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
 sudo add-apt-repository --yes ppa:beineri/opt-qt591-trusty
 sudo apt-get update
-sudo apt-get install --yes build-essential gcc-4.9 g++-4.9 cmake pkg-config libjack-jackd2-dev libsndfile1-dev libasound2-dev libavahi-client-dev libreadline6-dev libfftw3-dev libicu-dev libxt-dev libudev-dev
+sudo apt-get install --yes build-essential gcc-4.9 g++-4.9 cmake pkg-config libsndfile1-dev libasound2-dev libavahi-client-dev libreadline6-dev libfftw3-dev libicu-dev libxt-dev libudev-dev
+sudo apt-get install --yes --no-install-recommends libjack-dev jackd1
 if [[ -n "$1" && "$1" == "--qt=true" ]]; then
 	sudo apt-get install --yes libgl1-mesa-dev qt59base qt59location qt59declarative qt59tools qt59webengine qt59webchannel qt59xmlpatterns qt59svg qt59websockets
 fi
