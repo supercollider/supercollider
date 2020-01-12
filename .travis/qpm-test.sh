c96ca4dfb067ef2698fba1279e39e1cb958f7929
patrickdupuis <p.dupuis@live.com>
2020-01-11 20:45:22 -0500

    qpm test.run include testsuite

--- .travis/qpm-test.sh
+++ .travis/qpm-test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks
+qpm test.run -l $TRAVIS_BUILD_DIR/BUILD/travis_test_run.json --path $SCLANG --include $HOME/Quarks $TRAVIS_BUILD_DIR/testsuite/classlibrary
