bc56a6edfd56a23011ecd1b7519d06c8b10c5969
elgiano <elgiano@gmail.com>
2020-04-03 11:26:25 +0200

    AA_EnableHighDpiScaling flags

--- editors/sc-ide/core/main_function.cpp
+++ editors/sc-ide/core/main_function.cpp
@@ -33,6 +33,7 @@
 using namespace ScIDE;
 
 int main(int argc, char* argv[]) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
     QApplication app(argc, argv);
 
     QStringList arguments(QApplication::arguments());
