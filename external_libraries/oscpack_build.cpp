e48ffe66a64e031bd5ff47aeca86cc8e6703fd50
Tristan Cacqueray <tdecacqu@redhat.com>
2020-05-31 18:33:17 +0000

    Check for value instead of definition

--- external_libraries/oscpack_build.cpp
+++ external_libraries/oscpack_build.cpp
@@ -1,8 +1,8 @@
 #include <boost/predef.h>
 
-#ifdef BOOST_ENDIAN_LITTLE_BYTE
+#if BOOST_ENDIAN_LITTLE_BYTE
 #define OSC_HOST_LITTLE_ENDIAN
-#elif defined(BOOST_ENDIAN_BIG_BYTE)
+#elif BOOST_ENDIAN_BIG_BYTE
 #define OSC_HOST_BIG_ENDIAN
 #else
 #error please define endianness
