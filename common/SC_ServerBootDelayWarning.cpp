1c343ceb40403c44e2d04414dca97297ab98f0ea
Geoffroy Montel <geoffroy.montel@radiofrance.com>
2020-06-01 10:13:29 +0200

    various changes after Brian's code review

--- common/SC_ServerBootDelayWarning.cpp
+++ common/SC_ServerBootDelayWarning.cpp
@@ -1,5 +1,3 @@
-// Add a warning when Windows Defender delays scsynth boot by 60+ seconds
-// cf. github issue #4368
 #include "SC_ServerBootDelayWarning.h"
 
 #ifdef _WIN32
@@ -10,38 +8,37 @@
 #    include <condition_variable>
 
 static bool g_isServerBooted = false;
-static std::mutex g_scsynthBootWarningMutex;
-static std::condition_variable g_scsynthBootWarningConditionVariable;
-static std::thread* g_bootWarningThread;
+static std::mutex g_serverBootWarningMutex;
+static std::condition_variable g_serverBootWarningConditionVariable;
+static std::thread g_bootWarningThread;
 
 static void displayWarningIfServerHasNotBooted() {
-    std::unique_lock<std::mutex> lock(g_scsynthBootWarningMutex);
+    std::unique_lock<std::mutex> lock(g_serverBootWarningMutex);
 
-    if (!g_scsynthBootWarningConditionVariable.wait_for(lock, std::chrono::seconds(10),
-                                                        [] { return g_isServerBooted; })) {
+    if (!g_serverBootWarningConditionVariable.wait_for(lock, std::chrono::seconds(10),
+                                                       [] { return g_isServerBooted; })) {
         std::cout << "Server: possible boot delay." << std::endl;
         std::cout << "On some Windows-based machines, Windows Defender sometimes ";
         std::cout << "delays server boot by one minute." << std::endl;
         std::cout << "You can add scsynth.exe and/or supernova.exe *processes* to ";
-        std::cout << "Windows Defender exclusion list to disable this check. It's safe." << std::endl;
+        std::cout << "Windows Defender exclusion list to avoid this delay. It's safe." << std::endl;
     }
 }
 #endif //_WIN32
 
 void startServerBootDelayWarningTimer() {
 #ifdef _WIN32
-    g_bootWarningThread = new std::thread(displayWarningIfServerHasNotBooted);
+    g_bootWarningThread = std::thread(displayWarningIfServerHasNotBooted);
 #endif //_WIN32
 }
 
 void stopServerBootDelayWarningTimer() {
 #ifdef _WIN32
     {
-        const std::lock_guard<std::mutex> lock(g_scsynthBootWarningMutex);
+        const std::lock_guard<std::mutex> lock(g_serverBootWarningMutex);
         g_isServerBooted = true;
     }
-    g_scsynthBootWarningConditionVariable.notify_all();
-    g_bootWarningThread->join();
-    delete g_bootWarningThread;
+    g_serverBootWarningConditionVariable.notify_all();
+    g_bootWarningThread.join();
 #endif //_WIN32
 }
--- common/SC_ServerBootDelayWarning.h
+++ common/SC_ServerBootDelayWarning.h
@@ -1,2 +1,14 @@
+// Add a warning when Windows Defender delays scsynth boot by 60+ seconds
+// cf. github issue #4368
+
+#pragma once
+
+// On Windows, starts a timer that will display a warning message
+// after 10 seconds if the server has not booted,
+// which may be due to Windows Defender.
+// On other platforms, it does not do anything.
+// Must be called at the start of the server boot sequence.
 void startServerBootDelayWarningTimer();
+
+// Must be called at the end of the server boot sequence.
 void stopServerBootDelayWarningTimer();
