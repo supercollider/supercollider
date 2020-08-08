4d168bbfee4557e0fb6b76f5a9c3d1cc84d5d9be
Brian Heim <brianlheim@gmail.com>
2020-08-08 11:54:12 -0500

    make_release.py: add version cmd-line option, add more URLs

--- tools/release/make_release.py
+++ tools/release/make_release.py
@@ -3,6 +3,7 @@
 # This script provides an automated checklist for running through all the steps required for a release.
 
 from collections import deque
+import sys
 
 import utilities
 
@@ -49,7 +50,7 @@ class YesNoStageWithUrl(YesNoStage):
         return super().do()
 
 
-def main():
+def main(version: str):
     prompts = [
         "Is the repo clean?",
         "If this is a minor release, have you made the release branch?",
@@ -59,7 +60,7 @@ def main():
         "Have all the deprecations been either removed or deferred to a later release?\n      Deprecations are removed on a case-by-case basis with each minor (3.x) release.\n      Corresponding UGen and primitive code should also be removed.\n      Be careful when deprecating UGens and be considerate of alternate clients!",
         "Have all the removed deprecations been documented in the changelog?",
         # depends on branch name
-        # https://github.com/supercollider/supercollider/blob/Version-3.11.1-rc1/README.md#platform-support
+        # https://github.com/supercollider/supercollider/blob/3.11/README.md#platform-support
         "Have you reviewed the platform support information in the main README.md for accuracy?",
 
         "Have you updated SCVersion.txt?",
@@ -68,6 +69,8 @@ def main():
 
         "Have you made sure the schelp file 'News in 3.x' is up to date with the changelog by running the conversion script?", # XXX where is the script?
         "Have you made sure HelpSource/Help.schelp points to the latest 'News in 3.x' schelp file?",
+        # depends on branch name
+        # https://github.com/supercollider/supercollider/blob/develop/CHANGELOG.md#change-log
         "If this is a proper release, have you updated the release history in README.md?",
         "If this is a proper release, have you merged the current release branch into master with git merge --no-ff?",
         "Have you tagged the release?",
@@ -75,24 +78,16 @@ def main():
         ["Have you created a release on GitHub?", "https://github.com/supercollider/supercollider/releases/new"],
         "Have you run ./package/create_source_tarball.sh -v <version> (where version is the version tag, e.g. Version-3.11.0) to create a source tarball (including submodules)?",
         "Have you optionally run the script with -s <email-or-keyid> (where email-or-keyid is a valid PGP key id of the release manager) to also create a detached PGP signature for the source tarball?",
-        # depends on release tag
-        # https://github.com/supercollider/supercollider/releases/edit/Version-3.11.1-rc1
-        "Have you uploaded source tarball (and optionally detached PGP signature)?",
-        # depends on release tag
-        # https://github.com/supercollider/supercollider/releases/tag/Version-3.11.1-rc1
-        "Are builds for macOS, Linux, and Windows uploaded from CI?",
+        ["Have you uploaded source tarball (and optionally detached PGP signature)?", f"https://github.com/supercollider/supercollider/releases/edit/Version-{version}"],
+        ["Are builds for macOS, Linux, and Windows uploaded from CI?",f"https://github.com/supercollider/supercollider/releases/tag/Version-{version}"],
         "Have you codesigned and notarized a macOS app bundle?",
-        # depends on release tag
-        # https://github.com/supercollider/supercollider/releases/edit/Version-3.11.1-rc1
-        "Have you uploaded the signed macOS app bundle?",
-        # depends on release tag
-        # https://github.com/supercollider/supercollider/releases/tag/Version-3.11.1-rc1
-        "Have you made sure to note known-to-work platform versions and any changes in platform support on the Github release page?",
+        ["Have you uploaded the signed macOS app bundle?", f"https://github.com/supercollider/supercollider/releases/edit/Version-{version}"],
+        ["Have you made sure to note known-to-work platform versions and any changes in platform support on the Github release page?", f"https://github.com/supercollider/supercollider/releases/tag/Version-{version}"],
         ["If it is a full release, did you update the website download page?", "https://supercollider.github.io/download"],
 
         "Did you do the same for sc3-plugins?", # XXX review what does this mean
 
-        ["Did you update the sc3-plugins page?", "https://github.com/supercollider/sc3-plugins/tree/master/website"],
+        ["Did you update the sc3-plugins page?", "https://supercollider.github.io/sc3-plugins/"],
         ["If it's a proper release, did you update the Wikipedia page?", "https://en.wikipedia.org/wiki/SuperCollider"],
 
         "Have you created the text with an abbreviated changelog for announcing?", # XXX review what does this mean
@@ -103,7 +98,7 @@ def main():
         # open slack client?
         "Did you announce on Slack #general?",
         # maybe remove? or indicate optional?
-        "Did you announce on Facebook group?",
+        ["Did you announce on Facebook group?", "https://www.facebook.com/groups/2222754532"],
         # maybe remove? or indicate optional?
         "Did you announce on Reddit (/r/supercollider)?",
 
@@ -129,4 +124,8 @@ def main():
             break
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: make_release.py <version>")
+        sys.exit(1)
+
+    main(sys.argv[1])
