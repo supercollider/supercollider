d674ed7543858f3afd7fe0b2b1af041d5cdef61d
Brian Heim <brianlheim@gmail.com>
2020-10-03 17:35:34 -0500

    travis: manually codesign app bundle after building

--- .travis/script-osx.sh
+++ .travis/script-osx.sh
@@ -10,6 +10,19 @@ cmake --build $TRAVIS_BUILD_DIR/BUILD --config Release --target install | tee $B
 CMAKE_EXIT=$?
 set +o pipefail
 
+# Manually codesign. Without this, there will be present but invalid code signatures on SuperCollider.app and
+# SuperCollider.app/Contents/MacOS/SuperCollider.
+CODESIGN='/usr/bin/codesign --sign - --force --verbose=4'
+# select executables, *.app's, dylibs, and frameworks
+find SuperCollider.app \( \( -type f -a -perm -1 \) -o \( -name '*.app' \) -o \( -name '*.dylib' \) -o \
+    \( -name '*.framework' \) \) -exec $CODESIGN {} \;
+$CODESIGN SuperCollider.app/Contents/Frameworks/QtWebEngineCore.framework
+$CODESIGN SuperCollider.app/Contents/MacOS/SuperCollider
+$CODESIGN SuperCollider.app
+
+# If any signing fails, then we will be unable to sign SuperCollider.app
+/usr/bin/codesign --verify --verbose=4 SuperCollider.app || exit 1
+
 if [[ $CMAKE_EXIT != 0 ]]; then
     echo "Build failed. Log:"
     cat $BUILD_LOG
