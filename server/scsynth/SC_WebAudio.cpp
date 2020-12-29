0feab05455c0770278ea44b68331b5b39454f18c
Hanns Holger Rutz <contact@sciss.de>
2020-12-29 22:25:06 +0100

    driver setup ok from web audio point of view

--- server/scsynth/SC_WebAudio.cpp
+++ server/scsynth/SC_WebAudio.cpp
@@ -33,6 +33,7 @@
 #include <stdlib.h>
 #include <emscripten.h>
 #include <emscripten/val.h>
+#include <emscripten/bind.h>
 
 using namespace emscripten;
 
@@ -49,8 +50,10 @@ extern "C" void EMSCRIPTEN_KEEPALIVE print_error_to_console(intptr_t pointer) {
 }
 
 class SC_WebAudioDriver : public SC_AudioDriver {
-    val mContext    = val::undefined();
-    val mProcessor  = val::undefined();
+    int mInputChannelCount, mOutputChannelCount, mBufSize;
+    double mSampleRate;
+    
+    float *mBufInPtr, *mBufOutPtr;
 
 protected:
     virtual bool DriverSetup(int* outNumSamplesPerCallback, double* outSampleRate);
@@ -61,22 +64,52 @@ public:
     SC_WebAudioDriver(struct World* inWorld);
     virtual ~SC_WebAudioDriver();
 
+    virtual void WebAudioInitBuffers(
+        uintptr_t bufInPtr  , int inputChannelCount, 
+        uintptr_t bufOutPtr , int outputChannelCount, int bufSize, double sampleRate);
+
+    virtual void WebAudioProcess();
+
     int WebAudioCallback(const void* input, void* output, unsigned long frameCount);
+
+    static SC_WebAudioDriver* instance;
 };
+
+SC_WebAudioDriver* SC_WebAudioDriver::instance = NULL;
+
+extern "C" SC_WebAudioDriver* audio_driver() {
+    if (SC_WebAudioDriver::instance == NULL) {
+        throw std::runtime_error("SC_WebAudioDriver instance has not yet been created\n");
+    }
+    return SC_WebAudioDriver::instance;
+}
+
+EMSCRIPTEN_BINDINGS(Audio_Driver) {
+    emscripten::class_<SC_WebAudioDriver>("SC_WebAudioDriver")
+        .function("WebAudioProcess"     , &SC_WebAudioDriver::WebAudioProcess)
+        .function("WebAudioInitBuffers" , &SC_WebAudioDriver::WebAudioInitBuffers, emscripten::allow_raw_pointers())
+        ;
+    emscripten::function("audio_driver", &audio_driver, emscripten::allow_raw_pointers());
+}
 
 SC_AudioDriver* SC_NewAudioDriver(struct World* inWorld) { 
     // set up exception error printing function
     EM_ASM({
         window.onerror = function(message, url, line, column, e) {
-            if (typeof e != 'number') return;
-            var pointer = e;
-            Module.ccall('print_error_to_console', 'number', ['number'], [pointer]);
+            if (typeof e == 'number') { // e is a pointer to std:runtime_error
+                Module.ccall('print_error_to_console', 'number', ['number'], [e]);
+            }
         }
     });
     return new SC_WebAudioDriver(inWorld); 
 }
 
-SC_WebAudioDriver::SC_WebAudioDriver(struct World* inWorld): SC_AudioDriver(inWorld) {}
+SC_WebAudioDriver::SC_WebAudioDriver(struct World* inWorld): SC_AudioDriver(inWorld) {
+    if (SC_WebAudioDriver::instance != NULL) {
+        throw std::runtime_error("SC_WebAudioDriver can only have one instance\n");
+    }
+    SC_WebAudioDriver::instance = this;
+}
 
 SC_WebAudioDriver::~SC_WebAudioDriver() {
 }
@@ -100,45 +133,84 @@ int SC_WebAudioDriver::WebAudioCallback(const void* input, void* output, unsigne
     return 0;
 }
 
-// TODO: check mPreferredHardwareBufferFrameSize, mPreferredSampleRate
+void SC_WebAudioDriver::WebAudioInitBuffers(
+    uintptr_t bufInPtr  , int inputChannelCount, 
+    uintptr_t bufOutPtr , int outputChannelCount, int bufSize, double sampleRate) {
+
+    printf("SC_WebAudio: WebAudioInitBuffers.\n");
+    // cf. https://stackoverflow.com/questions/20355880/#27364643
+    this->mBufInPtr             = reinterpret_cast<float*>(bufInPtr );
+    this->mBufOutPtr            = reinterpret_cast<float*>(bufOutPtr);
+    this->mInputChannelCount    = inputChannelCount;
+    this->mOutputChannelCount   = outputChannelCount;
+    this->mBufSize              = bufSize;
+    this->mSampleRate           = sampleRate;
+}
+
+void SC_WebAudioDriver::WebAudioProcess() {
+
+}
+
 bool SC_WebAudioDriver::DriverSetup(int* outNumSamples, double* outSampleRate) {
     scprintf("SC_WebAudio: DriverSetup.\n");
 
-    // this uses the experimental emscripten Embind 'val' transliteration,
-    // see https://emscripten.org/docs/api_reference/val.h.html
-    val AudioContext = val::global("AudioContext");
-    if (!AudioContext.as<bool>()) {
-        AudioContext = val::global("webkitAudioContext");
-        if (!AudioContext.as<bool>()) {
-            scprintf("SC_WebAudioDriver: could not get hold of AudioContext\n");
-            return false;
+    int res = EM_ASM_INT({
+        var AudioContext = window.AudioContext || window.webkitAudioContext;
+        if (!AudioContext) return -1;
+        if (!Module.audioDriver) Module.audioDriver = {};
+        var ad              = Module.audioDriver;
+        var opt             = {};
+        // $1 is mPreferredSampleRate;
+        if ($1) opt.sampleRate = $1;
+        ad.context          = new AudioContext(opt);
+        ad.inChanCount      = 0;
+        ad.outChanCount     = 2;
+        // $0 is mPreferredHardwareBufferFrameSize;
+        // note: using zero buffer size gives us default buffer size;
+        ad.proc             = ad.context.createScriptProcessor($0, ad.inChanCount, ad.outChanCount);
+        ad.bufSize          = ad.proc.bufferSize;
+        ad.sampleRate       = ad.context.sampleRate;
+        var bytesPerChan    = ad.bufSize * Float32Array.BYTES_PER_ELEMENT;
+        var numInBytes      = ad.inChanCount  * bytesPerChan;
+        var numOutBytes     = ad.outChanCount * bytesPerChan;
+        ad.bufInPtr         = Module._malloc(numInBytes );
+        ad.bufOutPtr        = Module._malloc(numOutBytes);
+        ad.floatBufIn       = [];
+        ad.floatBufOut      = [];
+        for (var ch = 0; ch < ad.inChanCount; ch++) {
+            ad.floatBufIn [ch] = new Float32Array(Module.HEAPU8.buffer, ad.bufInPtr  + (ch * bytesPerChan), ad.bufSize);
         }
-    }
+        for (var ch = 0; ch < ad.outChanCount; ch++) {
+            ad.floatBufOut[ch] = new Float32Array(Module.HEAPU8.buffer, ad.bufOutPtr + (ch * bytesPerChan), ad.bufSize);
+        }
+        var self            = Module.audio_driver();
+        self.WebAudioInitBuffers(ad.bufInPtr, ad.inChanCount, ad.bufOutPtr, ad.outChanCount, ad.bufSize, ad.sampleRate);
+        ad.proc.onaudioprocess = function(e) {
+            self.process();
+            var bOut    = e.outputBuffer;
+            var bProc   = ad.floatBufOut;
+            var numCh   = Math.min(bOut.numberOfChannels, ad.outChanCount);
+            var bufSize = ad.bufSize;
+            for (var ch = 0; ch < numCh; ch++) {
+                bOut.copyToChannel(bProc[ch], ch, 0);
+            }
+        };
+        return 0;
+    }, mPreferredHardwareBufferFrameSize, mPreferredSampleRate);
 
-    val context = AudioContext.new_();
-    mContext    = context;
-    double sr   = context["sampleRate"].as<double>();
-    // is there a better way to obtain the default buffer size?
-    val proc    = context.call<val>("createScriptProcessor", 0, 0, 2);
-    mProcessor  = proc;
-    int bufSize = proc["bufferSize"].as<int>();
+    if (res != 0) return false;
 
-    *outNumSamples = bufSize;
-    *outSampleRate = sr;
+    *outNumSamples = mBufSize;
+    *outSampleRate = mSampleRate;
     return true;
 }
 
 bool SC_WebAudioDriver::DriverStart() {
     scprintf("SC_WebAudio: DriverStart.\n");
-
-    mProcessor.call<void>("connect", mContext["destination"]);
     return true;
 }
 
 bool SC_WebAudioDriver::DriverStop() {
     scprintf("SC_WebAudio: DriverStop.\n");
-
-    mProcessor.call<void>("disconnect", mContext["destination"]);
-    mContext.call<void>("close");
     return true;
 }
