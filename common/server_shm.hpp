881b0049f3d28d341b64712bea2eec2f606d195d
Marcin Pączkowski <dyfeer@gmail.com>
2021-03-29 14:04:29 -0700

    Update server_shm.hpp

--- common/server_shm.hpp
+++ common/server_shm.hpp
@@ -28,40 +28,6 @@
 #include <boost/interprocess/managed_shared_memory.hpp>
 #include <boost/interprocess/containers/vector.hpp>
 
-// this overrides the location of boost's shared memory, fixing an issue where missing logs
-// prevented the server from booting, see https://github.com/supercollider/supercollider/issues/2409
-#ifdef BOOST_INTERPROCESS_SHARED_DIR_FUNC
-#    include "SC_Filesystem.hpp"
-#    include <boost/filesystem/operations.hpp>
-#    include <iostream>
-#    include "SC_Codecvt.hpp"
-namespace boost { namespace interprocess { namespace ipcdetail {
-inline void get_shared_dir(std::string& shared_dir) {
-    // this solution still fails if the shm file already exists and sc has no permissions to overwrite it - e.g. if it
-    // was created by another user
-    // the shm file is deleted after scsynth exits when run from scide, but NOT when
-    // run in the command prompt and stopped with ctrl+c
-    // I don't know what's the reason for not cleaning up that file in
-    // the latter situation
-    get_shared_dir_root(shared_dir); // this is C:\ProgramData\boost_interprocess
-    shared_dir += "/SuperCollider";
-
-    // user location alternative - doesn't work for users with non-ascii characters
-    // auto dir = (SC_Filesystem::instance().getDirectory(SC_Filesystem::DirName::UserConfig) /
-    // "BoostInterprocessSharedMemory"); shared_dir = SC_Codecvt::path_to_utf8_str(dir);
-    // boost::filesystem::create_directories(dir); // note boost path here - this works for non-ascii path
-    // however, shared_dir is still incorrect and while directory creation succeedes, shm creations fails later on
-
-    std::cout << shared_dir << std::endl; // debug
-    try {
-        boost::filesystem::create_directories(shared_dir);
-    } catch (const boost::filesystem::filesystem_error& error) {
-        throw std::runtime_error("Cannot create a directory for shared memory at " + shared_dir
-                                 + "\nReason: " + error.what());
-    }
-}
-}}}
-#endif
 
 namespace detail_server_shm {
 
