0c7eb29b0d88c12fd69aa294b6b17f2eef0ab242
Marcin Pączkowski <dyfeer@gmail.com>
2022-09-11 12:59:42 -0700

    Update x64-osx-release-supercollider.cmake

--- vcpkg/triplets/x64-osx-release-supercollider.cmake
+++ vcpkg/triplets/x64-osx-release-supercollider.cmake
@@ -1,7 +1,7 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
-if(PORT MATCHES "libsndfile|fftw|readline") # only specific libraries are build as shared
+if(PORT MATCHES "libsndfile|fftw3|readline") # only specific libraries are build as shared
     set(VCPKG_LIBRARY_LINKAGE dynamic)
 endif()
 
