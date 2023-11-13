f65c194b999965aa5300da9a6daa035d564531b4
Marcin Pączkowski <dyfeer@gmail.com>
2023-11-13 02:03:14 -0800

    [qt6] fix QFontDatabase deprecations

--- editors/sc-ide/widgets/settings/editor_page.cpp
+++ editors/sc-ide/widgets/settings/editor_page.cpp
@@ -37,7 +37,9 @@ namespace ScIDE { namespace Settings {
 
 EditorPage::EditorPage(QWidget* parent):
     QWidget(parent),
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     fontDatabase(new QFontDatabase),
+#endif()
     ui(new Ui::EditorConfigPage) {
     ui->setupUi(this);
 
@@ -67,7 +69,9 @@ EditorPage::EditorPage(QWidget* parent):
 
 EditorPage::~EditorPage() {
     delete ui;
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     delete fontDatabase;
+#endif
     qDeleteAll(mThemes);
 }
 
@@ -204,9 +208,17 @@ void EditorPage::loadThemeFormats(Theme& theme) {
 
 void EditorPage::populateFontList(bool onlyMonospaced) {
     ui->fontCombo->clear();
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     QStringList fontFamilies = fontDatabase->families();
+#else
+    QStringList fontFamilies = QFontDatabase::families();
+#endif
     foreach (QString family, fontFamilies) {
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
         if (onlyMonospaced && !fontDatabase->isFixedPitch(family))
+#else
+        if (onlyMonospaced && !QFontDatabase::isFixedPitch(family))
+#endif
             continue;
 
         ui->fontCombo->addItem(family);
