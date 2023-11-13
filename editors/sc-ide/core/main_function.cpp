08c9c80f434d6396c5ad1e64a13bd343e51f72a1
Marcin Pączkowski <dyfeer@gmail.com>
2023-11-13 02:03:34 -0800

    [qt6] fix QLibraryInfo deprecation

--- editors/sc-ide/core/main_function.cpp
+++ editors/sc-ide/core/main_function.cpp
@@ -71,7 +71,11 @@ int main(int argc, char* argv[]) {
 
     // Set up translations
     QTranslator qtTranslator;
+#if (QT_VERSION < QT_VERSION_CHECK(6, 0, 0))
     qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::location(QLibraryInfo::TranslationsPath));
+#else
+    qtTranslator.load("qt_" + QLocale::system().name(), QLibraryInfo::path(QLibraryInfo::TranslationsPath));
+#endif
     app.installTranslator(&qtTranslator);
 
     QString ideTranslationPath = standardDirectory(ScResourceDir) + "/translations";
