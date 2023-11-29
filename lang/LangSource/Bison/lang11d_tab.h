c995ce9df113d7ba9d2f716065c4dbbdc9a9b6a8
Dennis Scheiba <dennis.scheiba@gmx.de>
2023-11-29 23:24:56 +0100

    ignore bison for clang-format

--- lang/LangSource/Bison/lang11d_tab.h
+++ lang/LangSource/Bison/lang11d_tab.h
@@ -36,10 +36,10 @@
    private implementation details that can be changed or removed.  */
 
 #ifndef YY_YY_LANG11D_TAB_H_INCLUDED
-#define YY_YY_LANG11D_TAB_H_INCLUDED
+# define YY_YY_LANG11D_TAB_H_INCLUDED
 /* Debug traces.  */
 #ifndef YYDEBUG
-#    define YYDEBUG 0
+# define YYDEBUG 0
 #endif
 #if YYDEBUG
 extern int yydebug;
@@ -47,57 +47,58 @@ extern int yydebug;
 
 /* Token kinds.  */
 #ifndef YYTOKENTYPE
-#    define YYTOKENTYPE
-enum yytokentype {
+# define YYTOKENTYPE
+  enum yytokentype
+  {
     YYEMPTY = -2,
-    YYEOF = 0, /* "end of file"  */
-    YYerror = 256, /* error  */
-    YYUNDEF = 257, /* "invalid token"  */
-    NAME = 258, /* NAME  */
-    INTEGER = 259, /* INTEGER  */
-    SC_FLOAT = 260, /* SC_FLOAT  */
-    ACCIDENTAL = 261, /* ACCIDENTAL  */
-    SYMBOL = 262, /* SYMBOL  */
-    STRING = 263, /* STRING  */
-    ASCII = 264, /* ASCII  */
-    PRIMITIVENAME = 265, /* PRIMITIVENAME  */
-    CLASSNAME = 266, /* CLASSNAME  */
-    CURRYARG = 267, /* CURRYARG  */
-    VAR = 268, /* VAR  */
-    ARG = 269, /* ARG  */
-    CLASSVAR = 270, /* CLASSVAR  */
-    SC_CONST = 271, /* SC_CONST  */
-    NILOBJ = 272, /* NILOBJ  */
-    TRUEOBJ = 273, /* TRUEOBJ  */
-    FALSEOBJ = 274, /* FALSEOBJ  */
-    PSEUDOVAR = 275, /* PSEUDOVAR  */
-    ELLIPSIS = 276, /* ELLIPSIS  */
-    DOTDOT = 277, /* DOTDOT  */
-    PIE = 278, /* PIE  */
-    BEGINCLOSEDFUNC = 279, /* BEGINCLOSEDFUNC  */
-    BADTOKEN = 280, /* BADTOKEN  */
-    INTERPRET = 281, /* INTERPRET  */
-    BEGINGENERATOR = 282, /* BEGINGENERATOR  */
-    LEFTARROW = 283, /* LEFTARROW  */
-    WHILE = 284, /* WHILE  */
-    BINOP = 285, /* BINOP  */
-    KEYBINOP = 286, /* KEYBINOP  */
-    READWRITEVAR = 287, /* READWRITEVAR  */
-    UMINUS = 288 /* UMINUS  */
-};
-typedef enum yytokentype yytoken_kind_t;
+    YYEOF = 0,                     /* "end of file"  */
+    YYerror = 256,                 /* error  */
+    YYUNDEF = 257,                 /* "invalid token"  */
+    NAME = 258,                    /* NAME  */
+    INTEGER = 259,                 /* INTEGER  */
+    SC_FLOAT = 260,                /* SC_FLOAT  */
+    ACCIDENTAL = 261,              /* ACCIDENTAL  */
+    SYMBOL = 262,                  /* SYMBOL  */
+    STRING = 263,                  /* STRING  */
+    ASCII = 264,                   /* ASCII  */
+    PRIMITIVENAME = 265,           /* PRIMITIVENAME  */
+    CLASSNAME = 266,               /* CLASSNAME  */
+    CURRYARG = 267,                /* CURRYARG  */
+    VAR = 268,                     /* VAR  */
+    ARG = 269,                     /* ARG  */
+    CLASSVAR = 270,                /* CLASSVAR  */
+    SC_CONST = 271,                /* SC_CONST  */
+    NILOBJ = 272,                  /* NILOBJ  */
+    TRUEOBJ = 273,                 /* TRUEOBJ  */
+    FALSEOBJ = 274,                /* FALSEOBJ  */
+    PSEUDOVAR = 275,               /* PSEUDOVAR  */
+    ELLIPSIS = 276,                /* ELLIPSIS  */
+    DOTDOT = 277,                  /* DOTDOT  */
+    PIE = 278,                     /* PIE  */
+    BEGINCLOSEDFUNC = 279,         /* BEGINCLOSEDFUNC  */
+    BADTOKEN = 280,                /* BADTOKEN  */
+    INTERPRET = 281,               /* INTERPRET  */
+    BEGINGENERATOR = 282,          /* BEGINGENERATOR  */
+    LEFTARROW = 283,               /* LEFTARROW  */
+    WHILE = 284,                   /* WHILE  */
+    BINOP = 285,                   /* BINOP  */
+    KEYBINOP = 286,                /* KEYBINOP  */
+    READWRITEVAR = 287,            /* READWRITEVAR  */
+    UMINUS = 288                   /* UMINUS  */
+  };
+  typedef enum yytokentype yytoken_kind_t;
 #endif
 
 /* Value type.  */
-#if !defined YYSTYPE && !defined YYSTYPE_IS_DECLARED
+#if ! defined YYSTYPE && ! defined YYSTYPE_IS_DECLARED
 typedef int YYSTYPE;
-#    define YYSTYPE_IS_TRIVIAL 1
-#    define YYSTYPE_IS_DECLARED 1
+# define YYSTYPE_IS_TRIVIAL 1
+# define YYSTYPE_IS_DECLARED 1
 #endif
 
 
 extern YYSTYPE yylval;
 
-int yyparse(void);
+int yyparse (void);
 
 #endif /* !YY_YY_LANG11D_TAB_H_INCLUDED  */
