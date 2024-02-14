320fa2d7fafa296d09c065dfed60f8c82d1152fc
JordanHendersonMusic <j.henderson.music@outloook.com>
2024-02-14 17:05:08 +0000

    actually run clang format
    ...hopefully, clion doesn't want to use the config provided

--- lang/LangPrimSource/PyrStringPrim.cpp
+++ lang/LangPrimSource/PyrStringPrim.cpp
@@ -565,13 +565,15 @@ int prString_Getenv(struct VMGlobals* g, int /* numArgsPushed */) {
 
 #ifdef _WIN32
     std::string buf;
-    static constexpr DWORD initial_size{1024};
+    static constexpr DWORD initial_size { 1024 };
     buf.reserve(static_cast<std::size_t>(initial_size));
     const int return_size1 = GetEnvironmentVariable(this_str.c_str(), buf.data(), inital_size);
 
     char* value = [&]() -> char* {
-        if (return_size1 == 0) return nullptr; // not present
-        else if (return_size1 <= initial_size) return buf.data(); // success
+        if (return_size1 == 0)
+            return nullptr; // not present
+        else if (return_size1 <= initial_size)
+            return buf.data(); // success
         else { // buffer too small, try again
             buf.reserve(return_size1);
             const int return_size2 = GetEnvironmentVariable(this_str.c_str(), buf.data(), return_size1);
