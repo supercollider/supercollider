b15e9676b6505a87fbabe0ed70c517d1026d0da3
Dennis Scheiba <git@dennis-scheiba.com>
2025-03-26 17:33:25 +0100

    improve naming conventions further

--- lang/LangSource/SC_CLIOptions.hpp
+++ lang/LangSource/SC_CLIOptions.hpp
@@ -9,7 +9,7 @@ namespace SC_CLI {
 class CLIOptions {
 public:
     CLIOptions() {};
-    bool parse(int argc, char* argv[], SC_TerminalClient::Options& terminal_options, int& exit_code);
+    bool parse(int argc, char* argv[], SC_TerminalClient::Options& terminalOptions, int& exit_code);
 
     string mInputFile;
     vector<string> mScArgs;
@@ -20,12 +20,12 @@ private:
     bool mPrintVersion = false;
 
     po::options_description buildGenericDescription();
-    po::options_description buildTerminalDescription(SC_TerminalClient::Options& terminal_options);
+    po::options_description buildTerminalDescription(SC_TerminalClient::Options& terminalOptions);
     static po::options_description buildLanguageConfigDescription();
 
     static void parseLanguageConfig(po::variables_map& vm);
 
     static int parseMemoryString(std::string input);
-    static std::string convertMemoryInteger(int mem_size);
+    static std::string convertMemoryInteger(int memSize);
 };
 }
