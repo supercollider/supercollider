3bd28d4949143a825335920f5adb0144aecebb6f
Christof Ressi <christof.ressi@gmx.at>
2025-04-22 13:49:41 +0200

    SC_ServerBootDelayWarning: turn free function into RAII class
    This makes sure that the timer thread will be joined automatically when the object goes out of scope.

--- common/SC_ServerBootDelayWarning.h
+++ common/SC_ServerBootDelayWarning.h
@@ -3,12 +3,38 @@
 
 #pragma once
 
-// On Windows, starts a timer that will display a warning message
-// after 10 seconds if the server has not booted,
-// which may be due to Windows Defender.
-// On other platforms, it does not do anything.
-// Must be called at the start of the server boot sequence.
-void startServerBootDelayWarningTimer();
+#ifdef _WIN32
+#    include <condition_variable>
+#    include <thread>
+#    include <mutex>
 
-// Must be called at the end of the server boot sequence.
-void stopServerBootDelayWarningTimer();
+class ServerBootDelayWarningTimer {
+public:
+    // Automatically stops the timer thread if necessary.
+    ~ServerBootDelayWarningTimer();
+
+    // Starts a timer that will display a warning message after
+    // 10 seconds if the server has not booted, which may be due
+    // to Windows Defender.
+    // Must be called at the start of the server boot sequence.
+    void start();
+    // Call this at the end of the server boot sequence.
+    void stop();
+
+private:
+    std::mutex mMutex;
+    std::condition_variable mCondVar;
+    std::thread mThread;
+    bool mIsServerBooted = false;
+};
+
+#else
+
+// Does nothing on non-Windows platforms
+class ServerBootDelayWarningTimer {
+public:
+    void start() {}
+    void stop() {}
+};
+
+#endif // _WIN32
