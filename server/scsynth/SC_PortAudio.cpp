6e7735977dd1bb93f8818188f441c9be680b937f
prko <pyoungryang.ko@googlemail.com>
2025-05-27 06:34:26 +0900

    restore \n

--- server/scsynth/SC_PortAudio.cpp
+++ server/scsynth/SC_PortAudio.cpp
@@ -272,7 +272,7 @@ bool SC_PortAudioDriver::DriverSetup(int* outNumSamples, double* outSampleRate) 
     fprintf(stdout, "\nDevice options:\n");
     for (int i = 0; i < numDevices; i++) {
         pdi = Pa_GetDeviceInfo(i);
-        fprintf(stdout, "%d: %s\u2028   (%d ins, %d outs)\n", i, GetPaDeviceName(i).c_str(), pdi->maxInputChannels,
+        fprintf(stdout, "%d: %s\n   (%d ins, %d outs)\n", i, GetPaDeviceName(i).c_str(), pdi->maxInputChannels,
                 pdi->maxOutputChannels);
     }
 
