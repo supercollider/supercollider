970e3e31acebb3f8670be0bcf94d77d162f19162
Dennis Scheiba <git@dennis-scheiba.com>
2026-01-05 21:29:17 +0100

    change API of Link UGen

--- SCClassLibrary/Common/Audio/LinkUGen.sc
+++ SCClassLibrary/Common/Audio/LinkUGen.sc
@@ -1,16 +1,81 @@
-LinkUGen : MultiOutUGen {
-	*ar { |setTempo, newTempo, setPlaying, playingValue, quantum=1.0, hardwareBlockSize=512|
-	    var delay = hardwareBlockSize / SampleRate.ir;
-		^this.multiNew('audio', setTempo, newTempo, setPlaying, playingValue, quantum, delay);
+LinkUGen : UGen {
+	classvar <enabled;
+	classvar <>server;
+
+	*initClass {
+		Class.initClassTree(Server);
+		enabled = false;
+		server = Server.default;
 	}
 
-    *kr { |setTempo, newTempo, setPlaying, playingValue, quantum=1.0, hardwareBlockSize=512|
-        var delay = hardwareBlockSize / SampleRate.ir;
-        ^this.multiNew('control', setTempo, newTempo, setPlaying, playingValue, quantum, delay);
-    }
+	*enable {
+		if(server.hasBooted.not, {
+			"Server % is not running - can not enable LinkUGen".format(server).warn;
+			^this;
+		});
+		if(enabled, {
+			"LinkUGen is already enabled".warn;
+			^this;
+		});
+		{FreeSelf.kr(LinkEnabler.kr())}.play(server);
+		enabled = true;
+	}
 
-	init {arg ... theInputs;
-		inputs = theInputs;
-		^this.initOutputs(2, rate);
+	*disable {
+		if(server.hasBooted.not, {
+			// this should not happen...
+			"Server % is not running - can not disable LinkUGen".format(server).warn;
+			^this;
+		});
+		if(enabled.not, {
+			"LinkUGen is already disabled".warn;
+			^this;
+		});
+		{FreeSelf.kr(LinkDisabler.kr)}.play(server);
+		enabled = false;
+	}
+
+	*enabled_ {|newEnabled|
+		newEnabled = newEnabled.asBoolean;
+		if(newEnabled, {
+			LinkUGen.enable;
+		}, {
+			LinkUGen.disable;
+		});
+	}
+
+	*setTempo {|bpm|
+		{
+			LinkTempo.kr(1.0, bpm);
+			FreeSelf.kr(1.0);
+		}.play(server);
+	}
+
+	*kr {|quantum=4|
+		^this.new1('control', quantum);
+	}
+}
+
+LinkEnabler : UGen {
+	*kr {
+		^this.new1('control');
+	}
+}
+
+LinkDisabler : UGen {
+	*kr {
+		^this.new1('control');
+	}
+}
+
+LinkTempo : UGen {
+	*kr {|change=0.0, bpm=60.0|
+		^this.new1('control', change, bpm);
+	}
+}
+
+LinkJump : UGen {
+	*kr {|trigger=0.0, beat=0.0, quantum=4.0, force=0.0|
+		^this.new1('control', trigger, beat, quantum, force);
 	}
 }
--- server/plugins/LinkUGen.cpp
+++ server/plugins/LinkUGen.cpp
@@ -6,115 +6,159 @@
 
 static InterfaceTable* ft;
 
-class LinkUGen : public SCUnit {
+/*! we only have one global link instance per server.
+ *
+ * @todo: respect hardware buffer size which delays the output
+ *  of the signal - this should normally be respected but currently
+ *  we do not have access to this value through the Plugin API.
+ */
+static ableton::Link* LINK_CLOCK = nullptr;
+
+/*!
+ * @class LinkControl
+ * @brief Enables or disables a Link clock, which needs to be deferred to
+ * a non-RT thread.
+ */
+template <bool IsEnabler> class LinkControl : public SCUnit {
 public:
-    LinkUGen(): mLink(120.0) {
-        // this is not rt safe :p
-        mLink.enable(true);
-
-        mMSecPerTick = std::chrono::microseconds(static_cast<int>(10e6 / sampleRate()));
-
-        // how do we obtain the passed 'control' or 'audio' parameter in the plugin api?
-        if (isControlRateIn(0)) {
-            mCalcFunc = make_calc_function<LinkUGen, &LinkUGen::next_k>();
-            // prime the pipe
-            next_k(1);
-        } else {
-            mCalcFunc = make_calc_function<LinkUGen, &LinkUGen::next>();
-            next(1);
+    LinkControl() {
+        mCalcFunc = make_calc_function<LinkControl, &LinkControl::next_k>();
+        if (IsEnabler ? !LINK_CLOCK->isEnabled() : LINK_CLOCK->isEnabled()) {
+            ft->fDoAsynchronousCommand(mWorld, nullptr, nullptr, nullptr, &LinkControl::set_clock_status, nullptr,
+                                       nullptr, &LinkControl::do_nothing, 0, nullptr);
         }
-    }
-    ~LinkUGen() {
-        mLink.enable(false);
-        // do we need to destroy mLink here ;?
     }
 
 private:
-    // how to allocate this not during construction?
-    ableton::Link mLink;
-    std::chrono::microseconds mMSecPerTick = std::chrono::microseconds::zero();
-
+    /*!
+     * Returns 1.0 the moment the clock has been enabled or disabled.
+     * This signal can therefore be used to free the synth where
+     * we enable the clock.
+     */
     void next_k(int numSamples) {
-        auto sessionState = mLink.captureAudioSessionState();
-
-        const float setTempo = in0(0);
-        const float newTempo = in0(1);
-        const float setIsPlaying = in0(2);
-        const float isPlayingVal = in0(3);
-        const float quantum = in0(4);
-        const float delay = in0(5);
-
-        bool tempoSet = false;
-        bool playSet = false;
-
-        const std::chrono::microseconds hardwareOffset = std::chrono::microseconds(static_cast<int>(10e6 * delay));
-
-        // should we calculate from the beginning or from middle of the block (size)?
-        const auto time = mLink.clock().micros() + hardwareOffset;
-
-        if (setTempo > 0.0f) {
-            tempoSet = true;
-            sessionState.setTempo(newTempo, time);
+        float* out = mOutBuf[0];
+        if (IsEnabler) {
+            *out = LINK_CLOCK->isEnabled() ? 1.0f : 0.0f;
+        } else {
+            *out = LINK_CLOCK->isEnabled() ? 0.0f : 1.0f;
         }
-        if (setIsPlaying > 0.0f) {
-            playSet = true;
-            sessionState.setIsPlaying(isPlayingVal, time);
+    }
+    /*! Not thread safe - therefore runs in a NRT thread in stage 2 */
+    static bool set_clock_status(World* inWorld, void* cmdData) {
+        LINK_CLOCK->enable(IsEnabler);
+        if (IsEnabler) {
+            Print("Enabled Link clock\n");
+        } else {
+            Print("Disabled Link clock\n");
         }
-
-        if (playSet || tempoSet) {
-            mLink.commitAudioSessionState(sessionState);
-        }
-
-        // phase
-        out0(0) = sessionState.phaseAtTime(time, quantum);
-        // tempo
-        out0(1) = sessionState.tempo();
+        return false;
     }
 
-    void next(int numSamples) {
-        auto sessionState = mLink.captureAudioSessionState();
+    /*! necessary b/c async commands require a cleanup function */
+    static void do_nothing(World* inWorld, void* cmdData) {}
+};
 
-        float* phase = out(0);
-        float* tempo = out(1);
+/*!
+ * @class LinkTempo
+ * @brief Allows to get and set the tempo of the Link clock of the server.
+ * Tempo will only happen if in0 > 0.0f - be nice to others!
+ * If no Link clock is enabled on the server it will return -1.0f.
+ */
+class LinkTempo : public SCUnit {
+public:
+    LinkTempo() { mCalcFunc = make_calc_function<LinkTempo, &LinkTempo::next_k>(); }
 
-        const float* setTempo = in(0);
-        const float* newTempo = in(1);
-        const float* setIsPlaying = in(2);
-        const float* isPlayingVal = in(3);
-        const float* quantum = in(4);
-        const float* delay = in(5);
-
-        bool tempoSet = false;
-        bool playSet = false;
-
-        const std::chrono::microseconds hardwareOffset = std::chrono::microseconds(static_cast<int>(10e6 * delay[0]));
-
-        for (int i = 0; i < numSamples; i++) {
-            // i * mSecPerTick could be pre-calculated once - this seems to be computational intensive for some reason?
-            const auto time = mLink.clock().micros() + (i * mMSecPerTick) + hardwareOffset;
-
-            if (setTempo[i] > 0.0f && !tempoSet) {
-                // only set once in a block? sample accurate though
-                tempoSet = true;
-                sessionState.setTempo(newTempo[i], time);
+private:
+    bool mWarned = false;
+    void next_k(int numSamples) {
+        float* out = mOutBuf[0];
+        if (LINK_CLOCK->isEnabled()) {
+            auto state = LINK_CLOCK->captureAudioSessionState();
+            if (in0(0) > 0.0f) {
+                state.setTempo(in0(1), LINK_CLOCK->clock().micros());
+                LINK_CLOCK->commitAudioSessionState(state);
             }
-            if (setIsPlaying[i] > 0.0f && !playSet) {
-                // only set once in a block? sample accurate though
-                playSet = true;
-                sessionState.setIsPlaying(isPlayingVal[i], time);
+            *out = static_cast<float>(state.tempo());
+        } else {
+            if (!mWarned) {
+                Print("Error: Link clock has not been enabled, can not access tempo\n");
+                mWarned = true;
             }
-
-            phase[i] = sessionState.phaseAtTime(time, quantum[i]);
-            tempo[i] = sessionState.tempo();
+            *out = -1.0f;
         }
+    }
+};
 
-        if (playSet || tempoSet) {
-            mLink.commitAudioSessionState(sessionState);
+/*!
+ * @class LinkPhase
+ * @brief Returns the phase of the clock for a given quantum of range [0, quantum).
+ * If no clock is enabled on the server, it will return -1.0f.
+ */
+class LinkPhase : public SCUnit {
+public:
+    LinkPhase() { mCalcFunc = make_calc_function<LinkPhase, &LinkPhase::next_k>(); }
+
+private:
+    bool mWarned = false;
+
+    void next_k(int numSamples) {
+        float* out = mOutBuf[0];
+        if (LINK_CLOCK->isEnabled()) {
+            auto state = LINK_CLOCK->captureAudioSessionState();
+            auto phase = state.phaseAtTime(LINK_CLOCK->clock().micros(), in0(0));
+            *out = static_cast<float>(phase);
+        } else {
+            if (!mWarned) {
+                Print("Error: Link clock has not been enabled, can not get phase\n");
+                mWarned = true;
+            }
+            *out = -1.0f;
+        }
+    }
+};
+
+/*!
+ * @class LinkJump
+ * @brief Requests or forces a jump of all Link clients to a specific position within a given quantum.
+ */
+class LinkJump : public SCUnit {
+public:
+    LinkJump() { mCalcFunc = make_calc_function<LinkJump, &LinkJump::next_k>(); }
+
+private:
+    bool mWarned = false;
+
+    void next_k(int numSamples) {
+        float* out = mOutBuf[0];
+        if (LINK_CLOCK->isEnabled()) {
+            if (in0(0) > 0.0f) {
+                auto state = LINK_CLOCK->captureAudioSessionState();
+                if (in0(3) >= 0.0f) {
+                    state.forceBeatAtTime(in0(1), LINK_CLOCK->clock().micros(), in0(2));
+                } else {
+                    state.requestBeatAtTime(in0(1), LINK_CLOCK->clock().micros(), in0(2));
+                }
+                LINK_CLOCK->commitAudioSessionState(state);
+            }
+            *out = 0.0f;
+        } else {
+            if (!mWarned) {
+                Print("Error: Link clock has not been enabled, can not set phase\n");
+                mWarned = true;
+            }
+            *out = -1.0f;
         }
     }
 };
 
 PluginLoad(LinkUGen) {
     ft = inTable;
-    registerUnit<LinkUGen>(ft, "LinkUGen", false);
+    LINK_CLOCK = new ableton::Link(60.0f);
+    registerUnit<LinkControl<true>>(ft, "LinkEnabler", false);
+    registerUnit<LinkControl<false>>(ft, "LinkDisabler", false);
+    registerUnit<LinkTempo>(ft, "LinkTempo", false);
+    registerUnit<LinkPhase>(ft, "LinkUGen", false);
+    registerUnit<LinkJump>(ft, "LinkJump", false);
 }
+
+PluginUnload(LinkUGen) { delete LINK_CLOCK; }
