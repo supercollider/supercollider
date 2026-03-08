8f8219553038c579ede2b87fcae3c78765786047
Christof Ressi <christof.ressi@gmx.at>
2026-03-08 18:27:14 +0100

    replace BOOST_REQUIRE with BOOST_TEST

--- testsuite/server/scsynth/readwrite_test.cpp
+++ testsuite/server/scsynth/readwrite_test.cpp
@@ -12,14 +12,14 @@ BOOST_AUTO_TEST_CASE(readInt8_test) {
     const char* end = buffer + 1;
 
     int8 b = readInt8(ptr, end);
-    BOOST_REQUIRE(a == b);
+    BOOST_TEST(a == b);
 
     int8 c = 0;
     try {
         // too little data, so readInt8 should throw
         c = readInt8(ptr, ptr);
     } catch (...) {}
-    BOOST_REQUIRE(c == 0);
+    BOOST_TEST(c == 0);
 }
 
 BOOST_AUTO_TEST_CASE(readUInt8_test) {
@@ -30,14 +30,14 @@ BOOST_AUTO_TEST_CASE(readUInt8_test) {
     const char* end = buffer + 1;
 
     uint8 b = readUInt8(ptr, end);
-    BOOST_REQUIRE(a == b);
+    BOOST_TEST(a == b);
 
     uint8 c = 0;
     try {
         // too little data, so readUInt8 should throw
         c = readUInt8(ptr, ptr);
     } catch (...) {}
-    BOOST_REQUIRE(c == 0);
+    BOOST_TEST(c == 0);
 }
 
 BOOST_AUTO_TEST_CASE(readInt16_be_test) {
@@ -53,14 +53,14 @@ BOOST_AUTO_TEST_CASE(readInt16_be_test) {
     const char* end = buffer + 2;
 
     int16 b = readInt16_be(ptr, end);
-    BOOST_REQUIRE(a == b);
+    BOOST_TEST(a == b);
 
     int16 c = 0;
     try {
         // too little data, so readInt16_be should throw
         c = readInt16_be(ptr, ptr + 1);
     } catch (...) {}
-    BOOST_REQUIRE(c == 0);
+    BOOST_TEST(c == 0);
 }
 
 BOOST_AUTO_TEST_CASE(readInt32_be_test) {
@@ -78,14 +78,14 @@ BOOST_AUTO_TEST_CASE(readInt32_be_test) {
     const char* end = buffer + 4;
 
     int32 b = readInt32_be(ptr, end);
-    BOOST_REQUIRE(a == b);
+    BOOST_TEST(a == b);
 
     int32 c = 0;
     try {
         // too little data, so readInt32_be should throw
         c = readInt32_be(ptr, ptr + 3);
     } catch (...) {}
-    BOOST_REQUIRE(c == 0);
+    BOOST_TEST(c == 0);
 }
 
 BOOST_AUTO_TEST_CASE(readFloat_be_test) {
@@ -103,12 +103,12 @@ BOOST_AUTO_TEST_CASE(readFloat_be_test) {
     const char* end = buffer + 4;
 
     float b = readFloat_be(ptr, end);
-    BOOST_REQUIRE(a == b);
+    BOOST_TEST(a == b);
 
     float c = 0;
     try {
         // too little data, so readFloat_be should throw
         c = readFloat_be(ptr, ptr + 3);
     } catch (...) {}
-    BOOST_REQUIRE(c == 0);
+    BOOST_TEST(c == 0);
 }
