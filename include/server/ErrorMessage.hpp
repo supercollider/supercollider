3fcb5ad50238574cbf7a9cf04713de7e5b0c312f
Christof Ressi <christof.ressi@gmx.at>
2026-04-24 11:52:01 +0200

    fix SC version in apiVersionMismatch()

--- include/server/ErrorMessage.hpp
+++ include/server/ErrorMessage.hpp
@@ -72,7 +72,7 @@ std::string apiVersionMismatch(std::string const& utf8Filename, int const expect
 
     // both 1 and 2 were introduced in 3.6
     static map<int, string> const scVersionForAPIVersion = {
-        { 1, "3.6.0" }, { 2, "3.6.0" }, { 3, "3.9.0" }, { 4, "3.14" }
+        { 1, "3.6.0" }, { 2, "3.6.0" }, { 3, "3.9.0" }, { 4, "3.15" }
     };
 
     stringstream message;
