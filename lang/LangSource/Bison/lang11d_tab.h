8cb885c35b523456c02adfc60508e3c6c91209c4
JordanHendersonMusic <j.henderson.music@outlook.com>
2026-05-02 11:07:10 +0100

    sclang: remove unused rules in grammar

--- lang/LangSource/Bison/lang11d_tab.h
+++ lang/LangSource/Bison/lang11d_tab.h
@@ -35,8 +35,8 @@
    especially those whose name start with YY_ or yy_.  They are
    private implementation details that can be changed or removed.  */
 
-#ifndef YY_YY_LANG11D_TAB_H_INCLUDED
-# define YY_YY_LANG11D_TAB_H_INCLUDED
+#ifndef YY_YY_LANG_LANGSOURCE_BISON_LANG11D_TAB_H_INCLUDED
+# define YY_YY_LANG_LANGSOURCE_BISON_LANG11D_TAB_H_INCLUDED
 /* Debug traces.  */
 #ifndef YYDEBUG
 # define YYDEBUG 0
@@ -126,4 +126,4 @@ extern YYLTYPE yylloc;
 int yyparse (void);
 
 
-#endif /* !YY_YY_LANG11D_TAB_H_INCLUDED  */
+#endif /* !YY_YY_LANG_LANGSOURCE_BISON_LANG11D_TAB_H_INCLUDED  */
