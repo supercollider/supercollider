dec8759fd9f34c40160c134706d177c6c6203824
JordanHendersonMusic <j.henderson.music@outlook.com>
2026-05-02 10:47:43 +0100

    build: check for bison errors when changing bison file as pre commit

--- lang/LangSource/Bison/make_parser.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-bison --defines=lang11d_tab.h --output=lang11d_tab.cpp lang11d
-
---
+++ tools/generate_parser_bison.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+bison -Werror --defines=lang/LangSource/Bison/lang11d_tab.h --output=lang/LangSource/Bison/lang11d_tab.cpp lang/LangSource/Bison/lang11d
