732b90e7d8963782816d3b93ff778075176daf47
sadguitarius <justbecuz@gmail.com>
2026-05-25 11:27:37 -0700

    fix raw string data access

--- langutils/sc_lexer/standalone/main.cpp
+++ langutils/sc_lexer/standalone/main.cpp
@@ -23,9 +23,9 @@ template <typename Stream> void print(Stream& s, SourceCodeRange r) {
 std::string utf8_encode(const std::wstring& wstr) {
     if (wstr.empty())
         return std::string();
-    int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], static_cast<int>(wstr.size()), NULL, 0, NULL, NULL);
+    int size_needed = WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), NULL, 0, NULL, NULL);
     std::string strTo(static_cast<size_t>(size_needed), 0);
-    WideCharToMultiByte(CP_UTF8, 0, &wstr[0], static_cast<int>(wstr.size()), &strTo[0], size_needed, NULL, NULL);
+    WideCharToMultiByte(CP_UTF8, 0, wstr.data(), static_cast<int>(wstr.size()), strTo.data(), size_needed, NULL, NULL);
     return strTo;
 }
 
